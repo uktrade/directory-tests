e8666015ebf638836bc9cbd5ea7d03ee65c0acc9
Rich Tier <rikatee@gmail.com>
2016-11-01 17:02:16 +0000

    Add sso webserver to docker-compose-local

--- docker/create_envs.sh
+++ docker/create_envs.sh
@@ -4,4 +4,6 @@ python ./docker/env_writer.py \
     ./docker/env.json \
     ./docker/directory-api/env.json \
     ./docker/directory-api/env-postgres.json \
+    ./docker/directory-sso/env.json \
+    ./docker/directory-sso/env-postgres.json \
     ./docker/directory-ui/env.json
