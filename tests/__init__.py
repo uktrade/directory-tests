6bfe312b171c27572cb705b619930ebc0e96ccf0
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2017-05-17 13:39:40 +0100

    sso_id should be an integer

--- tests/__init__.py
+++ tests/__init__.py
@@ -78,7 +78,7 @@ users = {
         'username': os.environ['SSO_USER_USERNAME'],
         'password': os.environ['SSO_USER_PASSWORD'],
         'token': os.environ['SSO_USER_TOKEN'],
-        'sso_id': os.environ['SSO_USER_SSO_ID']
+        'sso_id': int(os.environ['SSO_USER_SSO_ID'])
     },
     'unverified': {
         'token': os.environ['SSO_UNVERIFIED_USER_TOKEN']
