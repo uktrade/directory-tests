57e52836d172c19e9667068c42b8d4d17c43267b
kowalcj0 <janusz@justyoyo.com>
2017-06-12 19:04:12 +0100

    add csrfmiddlewaretoken prop to Actor

--- tests/functional/features/ScenarioData.py
+++ tests/functional/features/ScenarioData.py
@@ -7,7 +7,7 @@ from collections import namedtuple
 
 
 ScenarioData = namedtuple('ScenarioData', ['actors', 'unregistered_companies'])
-Actor = namedtuple('Actor', ['alias'])
+Actor = namedtuple('Actor', ['alias', 'csrfmiddlewaretoken'])
 UnregisteredCompany = namedtuple('UnregisteredCompany', ['alias', 'title',
                                                          'number', 'details'])
 
--- tests/functional/features/steps/fab_given_impl.py
+++ tests/functional/features/steps/fab_given_impl.py
@@ -11,5 +11,5 @@ def unauthenticated_supplier(context, supplier_alias):
     :param supplier_alias: alias of the Actor used within the scenario's scope
     :type supplier_alias: str
     """
-    actor = Actor(alias=supplier_alias)
+    actor = Actor(alias=supplier_alias, csrfmiddlewaretoken=None)
     context.add_actor(actor)
