7817df28ac16366f6e5e2b4a8ea49d13c8b4ad58
kowalcj0 <janusz@justyoyo.com>
2017-06-12 16:27:20 +0100

    rename step impl to describe actual action

--- tests/functional/features/steps/fab_when_def.py
+++ tests/functional/features/steps/fab_when_def.py
@@ -7,5 +7,5 @@ from tests.functional.features.steps.fab_when_impl import select_random_company
 
 @when("the supplier randomly selects an active company without a profile "
       "identified by an alias '{alias}'")
-def step_impl(context, alias):
+def when_random_company_is_selected(context, alias):
     select_random_company(context, alias)
