6006bc76e3810d2de6a00a53b4034def69c13cbb
kowalcj0 <janusz@justyoyo.com>
2017-06-16 13:05:35 +0100

    make request with consisten logging

--- tests/functional/features/utils.py
+++ tests/functional/features/utils.py
@@ -3,6 +3,9 @@
 
 import logging
 import os
+from enum import Enum
+
+import requests
 
 
 def get_file_log_handler(log_formatter,
@@ -72,3 +75,94 @@ class Method(Enum):
 
     def __eq__(self, y):
         return self.value == y.value
+
+
+def make_request(method: Method, url, *, session=None, params=None,
+                 headers=None, cookies=None, data=None, files=None,
+                 allow_redirects=True, trim_response_content=True):
+    """Make a desired HTTP request using optional parameters, headers and data.
+
+    NOTE:
+    If you want to send a POST/PUT/PATCH request as "multipart/form-data;"
+    rather than a default "application/x-www-form-urlencoded",
+    then provide `data` as `files`.
+
+    :param method: HTTP method, e.g.: GET, POST, PUT etc
+    :type  method: tests.functional.features.utils.Method
+    :param url: URL that request will be made against
+    :type  url: str
+    :param session: (optional) an instance of requests Session
+    :type session: requests.Session
+    :param params: (optional) query parameters
+    :type  params: dict
+    :param headers: (optional) extra request headers. Will not be persisted
+                    across requests, even if using a session.
+    :type  headers: dict
+    :param cookies: (optional) extra request cookies. Will not be persisted
+                    across requests, even if using a session.
+    :type  cookies: dict
+    :param data: (optional) data to send
+    :type  data: dict
+    :param files: (optional)
+    :type  files: dict with a file. For more details please refer to:
+                  http://docs.python-requests.org/en/master/user/quickstart/#post-a-multipart-encoded-file
+    :param allow_redirects: Follow or do not follow redirects
+    :type  allow_redirects: bool
+    :param trim_response_content: decide whether you want to log only first 150
+                                  characters of response content.
+                                  Defaults to True.
+    :type  trim_response_content: bool
+    :return: a response object
+    :rtype: requests.Response
+    """
+    assert url is not None, "Please provide the URL"
+
+    req = session or requests
+
+    if method == Method.DELETE:
+        res = req.delete(url=url, params=params, headers=headers,
+                         cookies=cookies, allow_redirects=allow_redirects)
+    elif method == Method.GET:
+        res = req.get(url=url, params=params, headers=headers, cookies=cookies,
+                      allow_redirects=allow_redirects)
+    elif method == Method.HEAD:
+        res = req.head(url=url, params=params, headers=headers, cookies=cookies,
+                       allow_redirects=allow_redirects)
+    elif method == Method.OPTIONS:
+        res = req.options(url=url, params=params, headers=headers,
+                          cookies=cookies, allow_redirects=allow_redirects)
+    elif method == Method.PATCH:
+        res = req.patch(url=url, params=params, headers=headers, cookies=cookies,
+                        data=data, files=files, allow_redirects=allow_redirects)
+    elif method == Method.POST:
+        res = req.post(url=url, params=params, headers=headers, cookies=cookies,
+                       data=data, files=files, allow_redirects=allow_redirects)
+    elif method == Method.PUT:
+        res = req.put(url=url, params=params, headers=headers, cookies=cookies,
+                      data=data, files=files, allow_redirects=allow_redirects)
+    else:
+        raise KeyError("Unrecognized Method: {}".format(method.name))
+
+    if not allow_redirects:
+        logging.debug("REQ Follow redirects: disabled")
+
+    logging.debug("REQ URL: {}".format(res.request.url))
+    logging.debug("REQ Headers: {}".format(res.request.headers))
+    if cookies:
+        logging.debug("REQ Cookies: {}".format(cookies))
+    if data:
+        logging.debug("REQ Data: {}".format(res.request.body))
+    if files:
+        logging.debug("REQ Files: {}".format(res.request.body))
+    logging.debug("RSP Status: {} {}".format(res.status_code, res.reason))
+    logging.debug("RSP URL: {}".format(res.url))
+    logging.debug("RSP Headers: {}".format(res.headers))
+    logging.debug("RSP Cookies: {}".format(res.cookies))
+    if res.content:
+        trim_response_content = len(res.content) > 150
+        if trim_response_content:
+            logging.debug("RSP Trimmed Content: {}".format(res.content[0:150]))
+        else:
+            logging.debug("RSP Content: {}".format(res.content))
+
+    return res
