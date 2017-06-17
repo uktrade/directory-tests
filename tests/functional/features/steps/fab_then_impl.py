cdbd6a66bec5181f8abcbc453c30238cdb253bd1
kowalcj0 <janusz@justyoyo.com>
2017-06-17 12:18:15 +0100

    split into smaller functions

--- tests/functional/features/steps/fab_then_impl.py
+++ tests/functional/features/steps/fab_then_impl.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 """FAB Given step definitions."""
-import email
 import logging
 
-from boto.s3 import connect_to_region
-from boto.s3.connection import OrdinaryCallingFormat
 from retrying import retry
 
-from tests.functional.features.settings import S3_ACCESS_KEY_ID
-from tests.functional.features.settings import S3_SECRET_ACCESS_KEY
-from tests.functional.features.settings import S3_BUCKET
-from tests.functional.features.settings import S3_REGION
-from tests.functional.features.utils import \
-    extract_plain_text_payload
 from tests.functional.features.utils import extract_email_confirmation_link
+from tests.functional.features.utils import find_confirmation_email_msg
+from tests.functional.features.utils import get_s3_bucket
 
 
 def sso_account_should_be_created(context, alias):
@@ -58,38 +51,7 @@ def should_get_verification_email(context, alias, subject):
     :type  subject: str
     """
     actor = context.get_actor(alias)
-    title = title or ("Your great.gov.uk account: Please Confirm Your E-mail "
-                      "Address")
-    conn = connect_to_region(region_name=S3_REGION,
-                             aws_access_key_id=S3_ACCESS_KEY_ID,
-                             aws_secret_access_key=S3_SECRET_ACCESS_KEY,
-                             is_secure=True,
-                             calling_format=OrdinaryCallingFormat())
-    bucket = conn.get_bucket(S3_BUCKET)
-    found = False
-    for key in bucket.list():
-        if key.key != "AMAZON_SES_SETUP_NOTIFICATION":
-            logging.debug("Processing email file: %s", key.key)
-            try:
-                msg_contents = key.get_contents_as_string().decode("utf-8")
-                msg = email.message_from_string(msg_contents)
-                if msg['To'] == actor.email:
-                    logging.debug("Found an email addressed at: %s",
-                                  msg['To'])
-                    if msg['Subject'] == title:
-                        logging.debug("Found email confirmation message "
-                                      "entitled: %s", title)
-                        payload = extract_plain_text_payload(msg)
-                        link = extract_email_confirmation_link(payload)
-                        context.set_actor_email_confirmation_link(alias, link)
-                        found = True
-                logging.debug("Deleting message %s", key.key)
-                bucket.delete_key(key.key)
-                logging.debug("Successfully deleted message %s from S3",
-                              key.key)
-            except Exception as ex:
-                logging.error("Something went wrong when getting an email msg "
-                              "from S3: %s", ex)
-
-    assert found, ("Could not find email confirmation message for {}"
-                   .format(actor.email))
+    bucket = get_s3_bucket()
+    payload = find_confirmation_email_msg(bucket, actor, subject)
+    link = extract_email_confirmation_link(payload)
+    context.set_actor_email_confirmation_link(alias, link)
