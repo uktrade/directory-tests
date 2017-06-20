ecb4171c240d911e000ab960813a7f25963765d2
kowalcj0 <janusz@justyoyo.com>
2017-06-20 16:39:53 +0100

    extract DB details from env variables

--- tests/functional/features/settings.py
+++ tests/functional/features/settings.py
@@ -1,4 +1,5 @@
 import os
+from urllib import parse as urlparse
 
 # These variables specify URLs to Services under test
 DIRECTORY_API_URL = os.environ["DIRECTORY_API_URL"]
@@ -14,3 +15,17 @@ S3_ACCESS_KEY_ID = os.environ["S3_ACCESS_KEY_ID"]
 S3_SECRET_ACCESS_KEY = os.environ["S3_SECRET_ACCESS_KEY"]
 S3_BUCKET = os.environ["S3_BUCKET"]
 S3_REGION = os.environ["S3_REGION"]
+
+FAB_DB_URL = urlparse.urlparse(os.environ['FAB_DATABASE_URL'])
+FAB_DB_NAME = FAB_DB_URL.path[1:]
+FAB_DB_USER = FAB_DB_URL.username
+FAB_DB_PASSWORD = FAB_DB_URL.password
+FAB_DB_HOST = FAB_DB_URL.hostname
+FAB_DB_PORT = FAB_DB_URL.port
+
+SSO_DB_URL = urlparse.urlparse(os.environ['SSO_DATABASE_URL'])
+SSO_DB_NAME = SSO_DB_URL.path[1:]
+SSO_DB_USER = SSO_DB_URL.username
+SSO_DB_PASSWORD = SSO_DB_URL.password
+SSO_DB_HOST = SSO_DB_URL.hostname
+SSO_DB_PORT = SSO_DB_URL.port
