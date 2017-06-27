c6250f14cfc3dd4e86709735a0fb95ca6738b76e
kowalcj0 <janusz@justyoyo.com>
2017-06-27 16:25:58 +0100

    renamed funcs with the same name

--- tests/functional/features/steps/fab_given_def.py
+++ tests/functional/features/steps/fab_given_def.py
@@ -29,14 +29,14 @@ def given_supplier_created_sso_account_for_company(context, supplier_alias,
 
 @given('"{alias}" received the email verification message with the email '
        'confirmation link')
-def then_supplier_should_receive_verification_email(context, alias):
+def given_supplier_received_verification_email(context, alias):
     subject = EMAIL_VERIFICATION_MSG_SUBJECT
     should_get_verification_email(context, alias, subject)
 
 
 @given('"{supplier_alias}" confirmed her email address')
 @given('"{supplier_alias}" confirmed his email address')
-def step_impl(context, supplier_alias):
+def given_supplier_confirmed_email_address(context, supplier_alias):
     confirm_email_address(context, supplier_alias)
 
 
