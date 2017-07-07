bc5158be6b80eaf0963c73ae33e558ad5f582a4a
kowalcj0 <kowalczykjanusz@gmail.com>
2017-07-07 16:14:38 +0100

    list company's detais that can be changed or checked

---
+++ tests/functional/features/pages/common.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Common data used across the functional tests"""
+
+DETAILS = {
+    "TITLE": "business name",
+    "KEYWORDS": "keywords",
+    "WEBSITE": "website",
+    "SIZE": "number of employees",
+    "SECTOR": "sector of interest",
+    "RECIPIENT": "letters recipient full name"
+}
