7fe4518df6ca81ff9bb43b2abd72180286c32bbe
kowalcj0 <kowalczykjanusz@gmail.com>
2017-07-10 12:04:35 +0100

    added fields to store links to online profiles

--- tests/functional/features/context_utils.py
+++ tests/functional/features/context_utils.py
@@ -24,7 +24,7 @@ Company = namedtuple(
     [
         'alias', 'title', 'number', 'address_details', 'summary', 'description',
         'website', 'keywords', 'no_employees', 'sector', 'letter_recipient',
-        'companies_house_details'
+        'companies_house_details', 'facebook', 'linkedin', 'twitter'
     ]
 )
 # Set all fields to None by default.
