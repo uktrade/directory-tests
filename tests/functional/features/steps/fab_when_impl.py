3bd141f2635f55a37aea41e639597b4626681cc2
kowalcj0 <kowalczykjanusz@gmail.com>
2017-07-13 12:06:56 +0100

    be more defensive about opening the file

--- tests/functional/features/steps/fab_when_impl.py
+++ tests/functional/features/steps/fab_when_impl.py
@@ -1290,7 +1290,8 @@ def prof_upload_unsupported_file_as_logo(context, supplier_alias, file):
     data = {"csrfmiddlewaretoken": actor.csrfmiddlewaretoken,
             "supplier_company_profile_logo_edit_view-current_step": "logo",
             }
-    files = {"logo-logo": open(filename, "rb")}
+    with open(filename, "rb") as picture:
+        files = {"logo-logo": picture}
     response = make_request(Method.POST, url, session=session, headers=headers,
                             data=data, files=files,
                             allow_redirects=False, context=context)
