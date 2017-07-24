05a2b867232d6d4fc5ba6d034f6b7c26914840d0
kowalcj0 <kowalczykjanusz@gmail.com>
2017-07-24 11:11:59 +0100

    update selected Page Objects

--- tests/functional/features/pages/fab_ui_build_profile_sector.py
+++ tests/functional/features/pages/fab_ui_build_profile_sector.py
@@ -11,9 +11,11 @@ from tests.settings import SECTORS
 
 URL = get_absolute_url("ui-buyer:company-edit")
 EXPECTED_STRINGS = [
-    "Your company sector", "Basic", "Industries", "Address", "Confirm",
-    "What sector is your company interested in working in?",
-    "< Back to previous step", "Next"
+    "Your company sector", "Basic", "Industry and exporting", "Address",
+    "Confirm",
+    "What industry is your company in?",
+    "< Back to previous step", "Next", "Select the countries you would like to export to:",
+    "China", "Germany", "India", "Japan", "United States", "Other", "Enter 3 maximum"
 ] + SECTORS
 
 
@@ -27,11 +29,13 @@ def should_be_here(response: Response):
                   "Choose Your company sector")
 
 
-def submit(actor: Actor, sector: str) -> Response:
+def submit(actor: Actor, sector: str, country: str, other: str) -> Response:
     """Submit Build your profile - Choose your sector form.
 
     :param actor: a namedtuple with Actor details
     :param sector: Industry Sector in which company is interested in
+    :param country: country code of the country you're exporting to
+    :param other: list of other countries your company is exporting to
     :return: response object
     """
     headers = {"Referer": URL}
@@ -39,6 +43,8 @@ def submit(actor: Actor, sector: str) -> Response:
         "csrfmiddlewaretoken": actor.csrfmiddlewaretoken,
         "supplier_company_profile_edit_view-current_step": "classification",
         "classification-sectors": sector,
+        "classification-export_destinations": country,
+        "classification-export_destinations_other": other
     }
     response = make_request(
         Method.POST, URL, session=actor.session, headers=headers, data=data)
