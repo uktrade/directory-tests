01a3617a9a7b3187f9e71e29b85885a47d641d43
kowalcj0 <kowalczykjanusz@gmail.com>
2017-07-24 11:33:48 +0100

    add more expected strings

--- tests/functional/features/pages/fab_ui_edit_sector.py
+++ tests/functional/features/pages/fab_ui_edit_sector.py
@@ -11,8 +11,9 @@ from tests.settings import SECTORS, COUNTRIES
 URL = get_absolute_url("ui-buyer:company-edit")
 EXPECTED_STRINGS = [
     "Your company sector",
-    "What sector is your company interested in working in?",
-    "Back to previous step"
+    "What industry is your company in?",
+    "Back to previous step", "Select the countries you would like to export to:",
+    "China", "Germany", "India", "Japan", "United States", "Other", "Save"
 ] + SECTORS
 
 
