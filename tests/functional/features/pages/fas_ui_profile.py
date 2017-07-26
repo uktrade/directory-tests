05719662228fe80cbfec1fe44ad136e58a0e4215
kowalcj0 <kowalczykjanusz@gmail.com>
2017-07-26 12:55:22 +0100

    PO: assert that details are visible on FAS profile page

--- tests/functional/features/pages/fas_ui_profile.py
+++ tests/functional/features/pages/fas_ui_profile.py
@@ -2,11 +2,14 @@
 """FAB - Edit Company's Directory Profile page"""
 import logging
 
+from behave.model import Table
 from requests import Response, Session
 
 from tests import get_absolute_url
 from tests.functional.features.context_utils import Company
+from tests.functional.features.pages.common import DETAILS
 from tests.functional.features.utils import Method, check_response, make_request
+from tests.settings import SECTORS_WITH_LABELS
 
 URL = get_absolute_url("ui-supplier:suppliers")
 EXPECTED_STRINGS = [
@@ -71,3 +74,40 @@ def should_see_case_studies(case_studies: dict, response: Response):
     for case in case_studies:
         assert case_studies[case].title in content
         assert case_studies[case].description in content
+
+
+def should_see_details(
+        company: Company, response: Response, table_of_details: Table):
+    """Supplier should see all expected Company details of FAS profile page.
+
+    :param company: a namedtuple with Company details
+    :param response: a response object
+    :param table_of_details: a table of expected company details
+    """
+    visible_details = [row["detail"] for row in table_of_details]
+    content = response.content.decode("utf-8")
+
+    title = DETAILS["TITLE"] in visible_details
+    keywords = DETAILS["KEYWORDS"] in visible_details
+    website = DETAILS["WEBSITE"] in visible_details
+    size = DETAILS["SIZE"] in visible_details
+    sector = DETAILS["SECTOR"] in visible_details
+
+    if title:
+        assert company.title in content
+    if keywords:
+        for keyword in company.keywords.split(", "):
+            assert keyword.strip() in content
+    if website:
+        assert company.website in content
+    if size:
+        if company.no_employees == "10001+":
+            assert "10,001+" in content
+        elif company.no_employees == "1001-10000":
+            assert "1,001-10,000" in content
+        elif company.no_employees == "501-1000":
+            assert "501-1,000" in content
+        else:
+            assert company.no_employees in content
+    if sector:
+        assert SECTORS_WITH_LABELS[company.sector] in content
