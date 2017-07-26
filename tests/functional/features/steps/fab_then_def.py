bf9d271f987c0883cc4707f697da5a52cb113fc9
kowalcj0 <kowalczykjanusz@gmail.com>
2017-07-26 11:42:07 +0100

    use updated steps & don't call POs directly

--- tests/functional/features/steps/fab_then_def.py
+++ tests/functional/features/steps/fab_then_def.py
@@ -2,10 +2,15 @@
 """FAB Given step definitions."""
 from behave import then
 
-from tests.functional.features.pages import fab_ui_profile, fas_ui_profile
 from tests.functional.features.steps.fab_then_impl import (
     bp_should_be_prompted_to_build_your_profile,
+    fab_no_links_to_online_profiles_are_visible,
+    fab_profile_is_verified,
     fab_should_see_all_case_studies,
+    fab_should_see_company_details,
+    fab_should_see_online_profiles,
+    fas_check_profiles,
+    fas_no_links_to_online_profiles_are_visible,
     fas_should_be_on_profile_page,
     fas_should_see_all_case_studies,
     fas_should_see_logo_picture,
@@ -13,25 +18,24 @@ from tests.functional.features.steps.fab_then_impl import (
     prof_should_be_on_profile_page,
     prof_should_be_told_about_invalid_links,
     prof_should_be_told_about_missing_description,
-    prof_should_be_told_that_company_is_published,
     prof_should_see_logo_picture,
+    profile_supplier_should_be_on_landing_page,
     reg_should_get_verification_email,
     reg_sso_account_should_be_created,
     reg_supplier_has_to_verify_email_first,
     reg_supplier_is_not_appropriate_for_fab,
-    sso_should_be_on_landing_page,
     sso_should_be_signed_in_to_sso_account
 )
 
 
 @then('"{alias}" should be told about the verification email')
 def then_sso_account_was_created(context, alias):
-    reg_sso_account_should_be_created(context, alias)
+    reg_sso_account_should_be_created(context.response, alias)
 
 
 @then('"{alias}" should receive an email verification msg entitled "{subject}"')
 def then_supplier_should_receive_verification_email(context, alias, subject):
-    reg_should_get_verification_email(context, alias, subject)
+    reg_should_get_verification_email(context, alias)
 
 
 @then('"{supplier_alias}" should be prompted to Build and improve your '
@@ -43,19 +47,20 @@ def then_supplier_should_be_prompted_to_build_your_profile(
 
 @then('"{supplier_alias}" should be on edit Company\'s Directory Profile page')
 def then_supplier_should_be_on_profile_page(context, supplier_alias):
-    prof_should_be_on_profile_page(context, supplier_alias)
+    prof_should_be_on_profile_page(context.response, supplier_alias)
 
 
 @then('"{supplier_alias}" should be told that her company has no description')
 def then_supplier_should_be_told_about_missing_description(
         context, supplier_alias):
-    prof_should_be_told_about_missing_description(context, supplier_alias)
+    response = context.response
+    prof_should_be_told_about_missing_description(response, supplier_alias)
 
 
 @then('"{supplier_alias}" should be told that her company is published')
 def then_supplier_should_be_told_that_profile_is_published(
         context, supplier_alias):
-    prof_should_be_told_that_company_is_published(context, supplier_alias)
+    fab_profile_is_verified(context, supplier_alias)
 
 
 @then('"{supplier_alias}" should be on FAS Directory Profile page of company '
@@ -80,7 +85,7 @@ def then_supplier_has_to_verify_email_first(context, supplier_alias):
 @then('"{supplier_alias}" should be on Welcome to your great.gov.uk profile '
       'page')
 def then_supplier_should_be_on_profile_landing_page(context, supplier_alias):
-    sso_should_be_on_landing_page(context, supplier_alias)
+    profile_supplier_should_be_on_landing_page(context, supplier_alias)
 
 
 @then('"{supplier_alias}" should be signed in to SSO/great.gov.uk account')
@@ -91,20 +96,19 @@ def then_supplier_should_be_signed_in_to_sso_account(context, supplier_alias):
 @then('"{supplier_alias}" should see new details on FAB Company\'s Directory '
       'Profile page')
 def then_supplier_should_see_new_details(context, supplier_alias):
-    fab_ui_profile.should_see_details(context, supplier_alias, context.table)
+    fab_should_see_company_details(context, supplier_alias)
 
 
 @then('"{supplier_alias}" should see links to all online profiles on FAB '
       'Company\'s Directory Profile page')
 def then_supplier_should_see_online_profiles_on_fab(context, supplier_alias):
-    fab_ui_profile.should_see_online_profiles(context, supplier_alias)
+    fab_should_see_online_profiles(context, supplier_alias)
 
 
 @then('"{supplier_alias}" should see links to all online profiles on FAS '
       'Company\'s Directory Profile page')
 def then_supplier_should_see_online_profiles_on_fas(context, supplier_alias):
-    fas_ui_profile.go_to(context, supplier_alias)
-    fas_ui_profile.should_see_online_profiles(context, supplier_alias)
+    fas_check_profiles(context, supplier_alias)
 
 
 @then('"{supplier_alias}" should be told to provide valid links to all online '
@@ -116,13 +120,13 @@ def then_supplier_should_be_told_to_use_valid_links(context, supplier_alias):
 @then('"{supplier_alias}" should not see any links to online profiles on FAB '
       'Company\'s Directory Profile page')
 def then_no_online_profiles_are_visible_on_fab(context, supplier_alias):
-    fab_ui_profile.should_not_see_online_profiles(context, supplier_alias)
+    fab_no_links_to_online_profiles_are_visible(context, supplier_alias)
 
 
 @then('"{supplier_alias}" should not see any links to online profiles on FAS '
       'Company\'s Directory Profile page')
 def then_no_online_profiles_are_visible_on_fas(context, supplier_alias):
-    fas_ui_profile.should_not_see_online_profiles(context, supplier_alias)
+    fas_no_links_to_online_profiles_are_visible(context, supplier_alias)
 
 
 @then('"{supplier_alias}" should see all case studies on the FAB Company\'s '
