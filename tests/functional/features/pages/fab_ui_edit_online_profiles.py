079fed41d96bda62cf0075ed0ae64612981342e4
kowalcj0 <kowalczykjanusz@gmail.com>
2017-07-26 11:38:22 +0100

    remove context & redundant code, use explicit types

--- tests/functional/features/pages/fab_ui_edit_online_profiles.py
+++ tests/functional/features/pages/fab_ui_edit_online_profiles.py
@@ -4,11 +4,10 @@ import logging
 import random
 
 from faker import Factory
+from requests import Response, Session
 
 from tests import get_absolute_url
-from tests.functional.features.pages.utils import (
-    extract_and_set_csrf_middleware_token
-)
+from tests.functional.features.context_utils import Actor, Company
 from tests.functional.features.utils import Method, check_response, make_request
 
 URL = get_absolute_url("ui-buyer:company-edit-social-media")
@@ -25,53 +24,47 @@ EXPECTED_STRINGS = [
 FAKE = Factory.create()
 
 
-def should_be_here(response):
+def should_be_here(response: Response):
     check_response(response, 200, body_contains=EXPECTED_STRINGS)
     logging.debug("Supplier is on the Edit Online Profiles page")
 
 
-def go_to(context, supplier_alias):
+def go_to(session: Session) -> Response:
     """Go to "Edit Company's Online Profiles" page.
 
     This requires:
      * Supplier to be logged in
 
-    :param context: behave `context` object
-    :param supplier_alias: alias of the Actor used in the scope of the scenario
+    :param session: Supplier session object
+    :return: response object
     """
-    actor = context.get_actor(supplier_alias)
-    session = actor.session
-
     headers = {"Referer": get_absolute_url("ui-buyer:company-profile")}
-    response = make_request(Method.GET, URL, session=session, headers=headers,
-                            allow_redirects=False, context=context)
-
+    response = make_request(Method.GET, URL, session=session, headers=headers)
     should_be_here(response)
-    extract_and_set_csrf_middleware_token(context, response, supplier_alias)
-    logging.debug("%s is on the Edit Company's Online Profiles page",
-                  supplier_alias)
+    return response
 
 
 def update_profiles(
-        context, supplier_alias, *, facebook=True, linkedin=True, twitter=True,
-        specific_facebook=None, specific_linkedin=None, specific_twitter=None):
+        actor: Actor, company: Company, *, facebook=True, linkedin=True,
+        twitter=True, specific_facebook=None, specific_linkedin=None,
+        specific_twitter=None) -> (Response, Company):
     """Change Company's Sector of Interest.
 
     NOTE:
     If any of `specific_*` arguments is set to an empty string, then this will
     remove existing link.
 
-    :param context: behave `context` object
-    :param supplier_alias: alias of the Actor used in the scope of the scenario
+    :param actor: a namedtuple with Actor details
+    :param company: a namedtuple with Company details
     :param facebook: change Facebook URL if True, or use the current one if False
     :param linkedin: change LinkedIn URL if True, or use the current one if False
     :param twitter: change Twitter URL if True, or use the current one if False
     :param specific_facebook: use specific Facebook URL
     :param specific_linkedin: use specific LinkedIn URL
     :param specific_twitter: use specific Twitter URL
+    :return: a tuple consisting of a Response object & a namedtuple with Company
+             details used in the update request.
     """
-    actor = context.get_actor(supplier_alias)
-    company = context.get_company(actor.company_alias)
     session = actor.session
     token = actor.csrfmiddlewaretoken
 
@@ -95,96 +88,54 @@ def update_profiles(
         new_tw = company.twitter
 
     headers = {"Referer": URL}
-    data = {"csrfmiddlewaretoken": token,
-            "company_social_links_edit_view-current_step": "social",
-            "social-facebook_url": new_fb,
-            "social-linkedin_url": new_li,
-            "social-twitter_url": new_tw
-            }
+    data = {
+        "csrfmiddlewaretoken": token,
+        "company_social_links_edit_view-current_step": "social",
+        "social-facebook_url": new_fb,
+        "social-linkedin_url": new_li,
+        "social-twitter_url": new_tw
+    }
 
-    make_request(Method.POST, URL, session=session, headers=headers, data=data,
-                 allow_redirects=True, context=context)
+    new_details = Company(facebook=new_fb, linkedin=new_li, twitter=new_tw)
 
-    context.set_company_details(
-        company.alias, facebook=new_fb, linkedin=new_li, twitter=new_tw)
-    logging.debug(
-        "%s set Company's Online Profile links to: Facebook=%s, LinkedId=%s, "
-        "Twitter=%s", supplier_alias, new_fb, new_li, new_tw)
+    response = make_request(
+        Method.POST, URL, session=session, headers=headers, data=data)
 
-
-def update_profiles_w_invalid_urls(
-        context, supplier_alias, *, facebook=True, linkedin=True, twitter=True,
-        specific_facebook=None, specific_linkedin=None, specific_twitter=None):
-    """Try to change Company's Online profiled using invalid URLs.
-
-    :param context: behave `context` object
-    :param supplier_alias: alias of the Actor used in the scope of the scenario
-    :param facebook: change Facebook URL if True, or use the current one if False
-    :param linkedin: change LinkedIn URL if True, or use the current one if False
-    :param twitter: change Twitter URL if True, or use the current one if False
-    :param specific_facebook: use specific Facebook URL
-    :param specific_linkedin: use specific LinkedIn URL
-    :param specific_twitter: use specific Twitter URL
-    """
-    actor = context.get_actor(supplier_alias)
-    company = context.get_company(actor.company_alias)
-    session = actor.session
-    token = actor.csrfmiddlewaretoken
-
-    fake = "http://{}".format(FAKE.domain_name())
-    new_facebook = specific_facebook or fake if facebook else company.facebook
-    new_linkedin = specific_linkedin or fake if linkedin else company.linkedin
-    new_twitter = specific_twitter or fake if twitter else company.twitter
-
-    headers = {"Referer": URL}
-    data = {"csrfmiddlewaretoken": token,
-            "supplier_company_social_links_edit_view-current_step": "social",
-            "social-facebook_url": new_facebook,
-            "social-linkedin_url": new_linkedin,
-            "social-twitter_url": new_twitter
-            }
-
-    response = make_request(Method.POST, URL, session=session, headers=headers,
-                            data=data, allow_redirects=True, context=context)
-    # check whether we're still on the same page
-    should_be_here(response)
+    return response, new_details
 
 
-def should_see_errors(context, supplier_alias, *, facebook=True, linkedin=True,
-                      twitter=True):
+def should_see_errors(
+        response: Response, *, facebook=True, linkedin=True, twitter=True):
     """Check if all required errors are visible.
 
-    :param context: behave `context` object
-    :param supplier_alias: alias of the Actor used in the scope of the scenario
+    :param response: a Response object
     :param facebook: check for a Facebook URL error if True, or not if False
     :param linkedin: check for a LinkedIn URL error if True, or not if False
     :param twitter: check for a Twitter URL error if True, or not if False
     """
-    content = context.response.content.decode("utf-8")
+    content = response.content.decode("utf-8")
     if facebook:
         assert "Please provide a link to Facebook." in content
     if linkedin:
         assert "Please provide a link to LinkedIn." in content
     if twitter:
         assert "Please provide a link to Twitter." in content
-    logging.debug(
-        "%s was not able to set Company's Online Profile links using invalid "
-        "URLs to: %s %s %s", supplier_alias, "Facebook" if facebook else "",
-        "LinkedIn" if linkedin else "", "Twitter" if twitter else "")
 
 
 def remove_links(
-        context, supplier_alias, *, facebook=False, linkedin=False,
-        twitter=False):
+        actor: Actor, company: Company, *, facebook=False, linkedin=False,
+        twitter=False) -> Response:
     """Remove links to all existing Online Profiles.
 
-    :param context: behave `context` object
-    :param supplier_alias: alias of the Actor used in the scope of the scenario
+    :param actor: a namedtuple with Actor details
+    :param company: a namedtuple with Company details
     :param facebook: remove link to Facebook profile if True, or not if False
     :param linkedin: remove link to LinkedIn profile if True, or not if False
     :param twitter: remove link to Twitter profile if True, or not if False
+    :return: a Response object
     """
-    update_profiles(
-        context, supplier_alias, facebook=facebook, linkedin=linkedin,
+    response, _ = update_profiles(
+        actor, company, facebook=facebook, linkedin=linkedin,
         twitter=twitter, specific_facebook="", specific_linkedin="",
         specific_twitter="")
+    return response
