c4584e5ff97240cc863fce1d198ef8474ba454a0
kowalcj0 <kowalczykjanusz@gmail.com>
2017-08-09 19:51:47 +0100

    move logging to console & file to separate functions

--- tests/functional/features/utils.py
+++ tests/functional/features/utils.py
@@ -8,9 +8,12 @@ import sys
 import traceback
 from contextlib import contextmanager
 from enum import Enum
+from pprint import pprint
 
 import requests
+import requests.exceptions as requests_base_exception
 from behave.runner import Context
+from requests import Session
 from requests.models import Response
 from retrying import retry
 from scrapy.selector import Selector
@@ -68,10 +71,131 @@ class Method(Enum):
         return self.value == y.value
 
 
-def make_request(method: Method, url, *, session=None, params=None,
-                 headers=None, cookies=None, data=None, files=None,
-                 allow_redirects=True, trim_response_content=True,
-                 context=None):
+def print_response(response: Response, *, trim: bool = True):
+    """
+
+    :param response:
+    :param trim:
+    :return:
+    """
+    request = response.request
+    trim_offset = 1024  # define the length of logged response content
+
+    if response.history:
+        blue("REQ was redirected")
+        for r in response.history:
+            blue("Intermediate REQ: %s %s" % (r.request.method, r.url))
+            blue("Intermediate REQ Headers:")
+            pprint(r.request.headers)
+            if r.request.body:
+                blue("Intermediate REQ Body (trimmed):")
+                print(r.request.body[0:trim_offset])
+            else:
+                blue("Intermediate REQ had no body")
+            blue("Intermediate RESP: %d %s" % (r.status_code, r.reason))
+            blue("Intermediate RESP Headers:")
+            pprint(r.headers)
+            if r.content:
+                content = r.content.decode("utf-8")
+                if trim:
+                    blue("Intermediate RESP Content (trimmed):")
+                    print(content[0:trim_offset])
+                else:
+                    blue("Intermediate RESP Content:")
+                    print(content)
+
+        blue("Final destination: %s %s -> %d %s" % (
+            request.method, request.url, response.status_code, response.url))
+    else:
+        green("REQ URL: %s %s" % (request.method, request.url))
+        green("REQ Headers:")
+        pprint(request.headers)
+        if request.headers.get('Set-Cookie'):
+            green("REQ Cookies:")
+            pprint(request.headers.get('Set-Cookie'))
+        if request.body:
+            if trim:
+                green("REQ Body (trimmed):")
+                print(request.body[0:trim_offset])
+            else:
+                green("REQ Body:")
+                print(request.body)
+        else:
+            green("REQ had no body")
+        green("RSP Status: %s %s" % (response.status_code, response.reason))
+        green("RSP URL: %s" % response.url)
+        green("RSP Headers:")
+        pprint(response.headers)
+        green("RSP Cookies:")
+        pprint(response.cookies)
+
+    if response.content:
+        content = response.content.decode("utf-8")
+        if trim:
+            red("RSP Content (trimmed):")
+            print(content[0:trim_offset])
+        else:
+            red("RSP Content:")
+            print(content)
+
+
+def log_response(response: Response, *, trim: bool = True):
+    request = response.request
+    trim_offset = 1024  # define the length of logged response content
+
+    if response.history:
+        logging.debug("REQ was redirected")
+        for r in response.history:
+            logging.debug("Intermediate REQ: %s %s", r.request.method, r.url)
+            logging.debug("Intermediate REQ Headers: %s", r.request.headers)
+            if r.request.body:
+                logging.debug(
+                    "Intermediate REQ Body (trimmed): %s",
+                    r.request.body[0:trim_offset])
+            else:
+                logging.debug("Intermediate REQ had no body")
+            logging.debug("Intermediate RESP: %d %s", r.status_code, r.reason)
+            logging.debug("Intermediate RESP Headers: %s", r.headers)
+            if r.content:
+                content = r.content.decode("utf-8")
+                if trim:
+                    logging.debug(
+                        "Intermediate RESP Content: %s", content[0:trim_offset])
+                else:
+                    logging.debug("Intermediate RSP Content: %s", content)
+        logging.debug(
+            "Final destination: %s %s -> %d %s", request.method, request.url,
+            response.status_code, response.url)
+    else:
+        logging.debug("REQ URL: %s %s", request.method, request.url)
+        logging.debug("REQ Headers:", request.headers)
+
+        if request.headers.get('Set-Cookie'):
+            logging.debug("REQ Cookies:", request.headers.get('Set-Cookie'))
+
+        if request.body:
+            logging.debug("REQ Body (trimmed): %s", request.body[0:trim_offset])
+        else:
+            logging.debug("REQ had no body")
+
+        logging.debug("RSP Status: %s %s", response.status_code, response.reason)
+        logging.debug("RSP URL: %s", response.url)
+        logging.debug("RSP Headers: %s", response.headers)
+        logging.debug("RSP Cookies: %s", response.cookies)
+
+    if response.content:
+        content = response.content.decode("utf-8")
+        if trim:
+            logging.debug("RSP Content (trimmed): %s", content[0:trim_offset])
+        else:
+            logging.debug("RSP Content: %s", content)
+
+
+def make_request(
+        method: Method, url: str, *, session: Session = None,
+        params: dict = None, headers: dict = None, cookies: dict = None,
+        data: dict = None, files: dict = None,
+        allow_redirects: bool = True) -> Response:
     """Make a desired HTTP request using optional parameters, headers and data.
 
     NOTE:
@@ -80,118 +204,58 @@ def make_request(method: Method, url, *, session=None, params=None,
     then provide `data` as `files`.
 
     :param method: HTTP method, e.g.: GET, POST, PUT etc
-    :type  method: tests.functional.features.utils.Method
     :param url: URL that request will be made against
-    :type  url: str
     :param session: (optional) an instance of requests Session
-    :type session: requests.Session
     :param params: (optional) query parameters
-    :type  params: dict
     :param headers: (optional) extra request headers. Will not be persisted
                     across requests, even if using a session.
-    :type  headers: dict
     :param cookies: (optional) extra request cookies. Will not be persisted
                     across requests, even if using a session.
-    :type  cookies: dict
     :param data: (optional) data to send
-    :type  data: dict
-    :param files: (optional)
-    :type  files: dict with a file. For more details please refer to:
+    :param files: (optional) a dict with a file. For more details please refer to:
                   http://docs.python-requests.org/en/master/user/quickstart/#post-a-multipart-encoded-file
     :param allow_redirects: Follow or do not follow redirects
-    :type  allow_redirects: bool
-    :param trim_response_content: decide whether you want to log only first 150
-                                  characters of response content.
-                                  Defaults to True.
-    :type  trim_response_content: bool
-    :param context: (optional) Behave's context object. If provided then this
-                    Will store the response in `context.response`
     :return: a response object
-    :rtype: requests.Response
     """
     with assertion_msg("Can't make a request without a valid URL!"):
         assert url is not None
 
     req = session or requests
-    trim_offset = 150  # define the length of logged response content
 
     connect_timeout = 3.05
     read_timeout = 60
-    request_kwargs = dict(url=url, params=params, headers=headers,
-                          cookies=cookies, data=data, files=files,
-                          allow_redirects=allow_redirects,
-                          timeout=(connect_timeout, read_timeout))
-
-    if method == Method.DELETE:
-        res = req.delete(**request_kwargs)
-    elif method == Method.GET:
-        res = req.get(**request_kwargs)
-    elif method == Method.HEAD:
-        res = req.head(**request_kwargs)
-    elif method == Method.OPTIONS:
-        res = req.options(**request_kwargs)
-    elif method == Method.PATCH:
-        res = req.patch(**request_kwargs)
-    elif method == Method.POST:
-        res = req.post(**request_kwargs)
-    elif method == Method.PUT:
-        res = req.put(**request_kwargs)
-    else:
-        raise KeyError("Unrecognized Method: %s", method.name)
+    request_kwargs = dict(
+        url=url, params=params, headers=headers, cookies=cookies, data=data,
+        files=files, allow_redirects=allow_redirects,
+        timeout=(connect_timeout, read_timeout))
 
     if not allow_redirects:
-        logging.debug("REQ Follow redirects: disabled")
+        msg = "REQ Follow redirects: disabled"
+        blue(msg)
+        logging.debug(msg)
 
-    logging.debug("REQ URL: %s %s", method, res.request.url)
-    logging.debug("REQ Headers: %s", res.request.headers)
-    if cookies:
-        logging.debug("REQ Cookies: %s", cookies)
-    if data:
-        if files:
-            if res.request.body:
-                logging.debug("REQ Body (trimmed): %s",
-                              res.request.body[0:trim_offset])
-            else:
-                logging.debug("REQ has no body just files")
+    try:
+        if method == Method.DELETE:
+            res = req.delete(**request_kwargs)
+        elif method == Method.GET:
+            res = req.get(**request_kwargs)
+        elif method == Method.HEAD:
+            res = req.head(**request_kwargs)
+        elif method == Method.OPTIONS:
+            res = req.options(**request_kwargs)
+        elif method == Method.PATCH:
+            res = req.patch(**request_kwargs)
+        elif method == Method.POST:
+            res = req.post(**request_kwargs)
+        elif method == Method.PUT:
+            res = req.put(**request_kwargs)
         else:
-            logging.debug("REQ Data: %s", res.request.body)
-    logging.debug("RSP Status: %s %s", res.status_code, res.reason)
-    logging.debug("RSP URL: %s", res.url)
-    logging.debug("RSP Headers: %s", res.headers)
-    logging.debug("RSP Cookies: %s", res.cookies)
-    if res.history:
-        logging.debug("REQ was redirected")
-        for resp in res.history:
-            logging.debug("Intermediate REQ: %s %s", resp.request.method, resp.url)
-            logging.debug("Intermediate REQ Headers: %s", resp.request.headers)
-            if files:
-                logging.debug("Intermediate REQ Body (trimmed): %s",
-                              resp.request.body[0:trim_offset])
-            else:
-                logging.debug("Intermediate REQ Body: %s", resp.request.body)
-            logging.debug("Intermediate RESP: %d %s", resp.status_code, resp.reason)
-            logging.debug("Intermediate RESP Headers: %s", resp.headers)
-            logging.debug("Intermediate RESP Content: %s",
-                          resp.content[0:trim_offset] or None)
-        logging.debug("Final destination: %s %s -> %d %s",
-                      res.request.method, res.request.url, res.status_code,
-                      res.url)
-    else:
-        logging.debug("REQ was not redirected")
-    if res.content:
-        if trim_response_content:
-            if len(res.content) > trim_offset:
-                logging.debug("RSP Trimmed Content: %s",
-                              res.content[0:trim_offset])
-            else:
-                logging.debug("RSP Content: %s", res.content)
-        else:
-            logging.debug("RSP Content: %s", res.content)
+            raise KeyError("Unrecognized Method: %s", method.name)
+    except requests_base_exception.HTTPError as ex:
+        print_response(res, trim=False)
+        raise ex
 
-    if context:
-        context.response = res
-        logging.debug("Last response stored in context.response")
-
+    log_response(res)
     return res
 
 
