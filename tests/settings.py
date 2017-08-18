97425e2f1a93705600acd755cc79863f74574b01
kowalcj0 <kowalczykjanusz@gmail.com>
2017-08-18 20:08:53 +0100

    add Subject of the Email sent to Supplier from FAS

--- tests/settings.py
+++ tests/settings.py
@@ -41,6 +41,8 @@ MAILGUN_SECRET_API_KEY = os.environ["MAILGUN_SECRET_API_KEY"]
 # Static data used across the project
 EMAIL_VERIFICATION_MSG_SUBJECT = ("Your great.gov.uk account: Please Confirm "
                                   "Your E-mail Address")
+FAS_MESSAGE_FROM_BUYER_SUBJECT = ("Someone is interested in your Find a Buyer "
+                                  "profile")
 NO_OF_EMPLOYEES = ["1-10", "11-50", "51-200", "201-500", "501-1000",
                    "1001-10000", "10001+"]
 SECTORS = [
