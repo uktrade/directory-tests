1a0a6ecdfde39f84b0a3c1beec84ddad5ae474cf
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-18 20:27:27 +0100

    step: classify as new, occasional & regular exporter

--- tests/exred/steps/when_impl.py
+++ tests/exred/steps/when_impl.py
@@ -8,6 +8,7 @@ from pages import (
     home,
     triage_1st_question,
     triage_2nd_question,
+    triage_3rd_question,
     triage_4th_question,
     triage_result
 )
@@ -25,10 +26,31 @@ def triage_select_sector(context: Context, *, sector: str = None):
     triage_2nd_question.should_be_here(driver)
 
 
+def triage_say_you_exported_before(context: Context):
+    driver = context.driver
+    triage_2nd_question.select_yes(driver)
+    triage_2nd_question.submit(driver)
+    triage_3rd_question.should_be_here(driver)
+
+
 def triage_say_you_never_exported_before(context: Context):
     driver = context.driver
     triage_2nd_question.select_no(driver)
     triage_2nd_question.submit(driver)
+    triage_4th_question.should_be_here(driver)
+
+
+def triage_say_you_export_regularly(context: Context):
+    driver = context.driver
+    triage_3rd_question.select_yes(driver)
+    triage_3rd_question.submit(driver)
+    triage_4th_question.should_be_here(driver)
+
+
+def triage_say_you_do_not_export_regularly(context: Context):
+    driver = context.driver
+    triage_3rd_question.select_no(driver)
+    triage_3rd_question.submit(driver)
     triage_4th_question.should_be_here(driver)
 
 
@@ -41,6 +63,14 @@ def triage_enter_company_name(context: Context):
 
 def triage_should_be_classified_as_new(context: Context):
     triage_result.should_be_classified_as_new(context.driver)
+
+
+def triage_should_be_classified_as_occasional(context: Context):
+    triage_result.should_be_classified_as_occasional(context.driver)
+
+
+def triage_should_be_classified_as_regular(context: Context):
+    triage_result.should_be_classified_as_regular(context.driver)
 
 
 def triage_create_exporting_journey(context: Context):
