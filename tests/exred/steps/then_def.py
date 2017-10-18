025ec30eae02fa1843c86ac55bc74b33046254d8
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-18 11:23:20 +0100

    step: then actor should be on specific page

--- tests/exred/steps/then_def.py
+++ tests/exred/steps/then_def.py
@@ -2,9 +2,14 @@
 """Then step definitions."""
 from behave import then
 
-from tests.exred.steps.then_impl import should_see_sections
+from tests.exred.steps.then_impl import should_be_on_page, should_see_sections
 
 
 @then('"{actor_name}" should see the "{sections}" sections')
 def then_actor_should_see_sections(context, actor_name, sections):
     should_see_sections(context, actor_name, sections)
+
+
+@then('"{actor_alias}" should be on the "{page_name}" page')
+def then_actor_should_be_on_page(context, actor_alias, page_name):
+    should_be_on_page(context, actor_alias, page_name)
--- tests/exred/steps/then_impl.py
+++ tests/exred/steps/then_impl.py
@@ -4,6 +4,8 @@ import logging
 
 from behave.runner import Context
 
+from tests.exred.registry import get_page_object
+
 
 def should_see_sections(context: Context, actor_name: str, sections: str):
     section_names = sections.lower().split(", ")
@@ -12,3 +14,9 @@ def should_see_sections(context: Context, actor_name: str, sections: str):
     logging.debug(
         "%s saw all expected sections on '%s' page", actor_name,
         context.current_page.NAME)
+
+
+def should_be_on_page(context: Context, actor_alias: str, page_name: str):
+    page = get_page_object(page_name)
+    page.should_be_here(context.driver)
+    logging.debug("%s is on %s page", actor_alias, page_name)
