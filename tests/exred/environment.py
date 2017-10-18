f6a4f0bac15fade00a356cc40d442e7848b0707a
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-18 11:12:43 +0100

    add doctrings and pep8

--- tests/exred/environment.py
+++ tests/exred/environment.py
@@ -27,11 +27,10 @@ Actor = namedtuple(
 Actor.__new__.__defaults__ = (None,) * len(Actor._fields)
 
 
-def initialize_scenario_data():
+def initialize_scenario_data() -> ScenarioData:
     """Will initialize the Scenario Data.
 
     :return an empty ScenarioData named tuple
-    :rtype ScenarioData
     """
     actors = {}
     scenario_data = ScenarioData(actors)
@@ -39,10 +38,20 @@ def initialize_scenario_data():
 
 
 def before_step(context: Context, step: Step):
+    """Place here code which that has to be executed before every step.
+
+    :param context: Behave Context object
+    :param step: Behave Step object
+    """
     logging.debug('Step: %s %s', step.step_type, str(repr(step.name)))
 
 
 def before_scenario(context: Context, scenario: Scenario):
+    """Place here code which has to be executed before every Scenario.
+
+    :param context: Behave Context object
+    :param scenario: Behave Scenario object
+    """
     logging.debug('Starting scenario: %s', scenario.name)
     context.scenario_data = initialize_scenario_data()
     logging.debug('Starting Selenium Driver for scenario: %s', scenario.name)
@@ -52,14 +61,23 @@ def before_scenario(context: Context, scenario: Scenario):
 
 
 def after_scenario(context: Context, scenario: Scenario):
+    """Place here code which has to be executed after every scenario.
+
+    :param context: Behave Context object
+    :param scenario: Behave Scenario object
+    """
     logging.debug('Quitting Selenium Driver after scenario: %s', scenario.name)
     context.driver.close()
     context.driver.quit()
 
 
 def before_all(context: Context):
+    """Place here code which has to be executed before all scenarios.
+
+    :param context: Behave Context object
+    """
     init_loggers(context, log_file=os.path.join(
-            ".", "tests", "exred", "reports", "behave.log"))
+        ".", "tests", "exred", "reports", "behave.log"))
     context.browser_name = os.environ.get("BROWSER", "chrome")
     context.viewport_width = os.environ.get("WIDTH", 1600)
     context.viewport_height = os.environ.get("HEIGHT", 1200)
