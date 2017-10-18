0e33cdac11968059c7bd08000fc84c7e9618a0e6
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-18 19:58:37 +0100

    PO: triage result page

---
+++ tests/exred/pages/triage_result.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""ExRed Triage 4th Question Page Object."""
+import logging
+
+from settings import DRIVERS
+from utils import assertion_msg, get_absolute_url, take_screenshot
+
+NAME = "ExRed Triage - result"
+URL = get_absolute_url(NAME)
+
+CLASSIFICATION = ".question > h2"
+ANSWERS = "div.answers"
+CONTINUE_BUTTON = "a.next"
+CHANGE_ANSWERS_LINK = "a.update"
+BACK_TO_HOME_LINK = "#content > .questions .home-link > a"
+
+EXPECTED_ELEMENTS = {
+    "classification": CLASSIFICATION,
+    "answers section": ANSWERS,
+    "continue button": CONTINUE_BUTTON,
+    "change answers link": CHANGE_ANSWERS_LINK,
+    "back to home link": BACK_TO_HOME_LINK
+}
+
+
+def should_be_here(driver: DRIVERS):
+    for element_name, element_selector in EXPECTED_ELEMENTS.items():
+        element = driver.find_element_by_css_selector(element_selector)
+        with assertion_msg(
+                "It looks like '%s' element is not visible on %s",
+                element_name, NAME):
+            assert element.is_displayed()
+    take_screenshot(driver, NAME)
+    logging.debug("All expected elements are visible on '%s' page", NAME)
+
+
+def get_classification(driver: DRIVERS) -> str:
+    element = driver.find_element_by_css_selector(CLASSIFICATION)
+    return element.text
+
+
+def should_be_classified_as_new(driver: DRIVERS):
+    classified = get_classification(driver)
+    expected = "New to exporting"
+    with assertion_msg(
+            "Expected to be classified as '%s' but was classified as: '%s'",
+            expected, classified):
+        assert classified == expected
+
+
+def create_exporting_journey(driver: DRIVERS):
+    button = driver.find_element_by_css_selector(CONTINUE_BUTTON)
+    button.click()
+    take_screenshot(driver, NAME + " after submitting")
