7b1fc5bcc008f4b723132948a0fbfd7c1bef8218
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-18 11:22:14 +0100

    add option to simulate first time visit

--- tests/exred/steps/given_def.py
+++ tests/exred/steps/given_def.py
@@ -8,3 +8,8 @@ from tests.exred.steps.given_impl import visit_page
 @given('"{actor_name}" visits the "{page_name}" page')
 def given_actor_visits_page(context, actor_name, page_name):
     visit_page(context, actor_name, page_name)
+
+
+@given('"{actor_name}" visits the "{page_name}" page for the first time')
+def given_actor_visits_page_for_the_first_time(context, actor_name, page_name):
+    visit_page(context, actor_name, page_name, first_time=True)
--- tests/exred/steps/given_impl.py
+++ tests/exred/steps/given_impl.py
@@ -7,7 +7,9 @@ from behave.runner import Context
 from tests.exred.registry import get_page_object
 
 
-def visit_page(context: Context, actor_name: str, page_name: str):
-    logging.debug("%s will visit '%s' page", page_name)
+def visit_page(
+        context: Context, actor_name: str, page_name: str, *,
+        first_time: bool = False):
+    logging.debug("%s will visit '%s' page", actor_name, page_name)
     context.current_page = get_page_object(page_name)
-    context.current_page.visit(context.driver)
+    context.current_page.visit(context.driver, first_time=first_time)
