68c96bb74b0fcbb000c8458d9e7473fc5409fa9f
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-18 20:02:12 +0100

    step: classify as new to exporting

--- tests/exred/steps/given_def.py
+++ tests/exred/steps/given_def.py
@@ -2,7 +2,7 @@
 """Given step definitions."""
 from behave import given
 
-from steps.given_impl import visit_page
+from steps.given_impl import classify_as, visit_page
 
 
 @given('"{actor_name}" visits the "{page_name}" page')
@@ -13,3 +13,9 @@ def given_actor_visits_page(context, actor_name, page_name):
 @given('"{actor_name}" visits the "{page_name}" page for the first time')
 def given_actor_visits_page_for_the_first_time(context, actor_name, page_name):
     visit_page(context, actor_name, page_name, first_time=True)
+
+
+@given('"{actor_alias}" classifies herself as "{exporter_status}" exporter')
+@given('"{actor_alias}" classifies himself as "{exporter_status}" exporter')
+def given_actor_classifies_as(context, actor_alias, exporter_status):
+    classify_as(context, actor_alias, exporter_status)
--- tests/exred/steps/given_impl.py
+++ tests/exred/steps/given_impl.py
@@ -5,6 +5,14 @@ import logging
 from behave.runner import Context
 
 from registry import get_page_object
+from steps.when_impl import (
+    start_triage,
+    triage_create_exporting_journey,
+    triage_enter_company_name,
+    triage_say_you_never_exported_before,
+    triage_select_sector,
+    triage_should_be_classified_as_new
+)
 
 
 def visit_page(
@@ -13,3 +21,34 @@ def visit_page(
     logging.debug("%s will visit '%s' page", actor_name, page_name)
     context.current_page = get_page_object(page_name)
     context.current_page.visit(context.driver, first_time=first_time)
+
+
+def classify_as_new(context: Context, actor_alias: str):
+    start_triage(context, actor_alias)
+    triage_select_sector(context)
+    triage_say_you_never_exported_before(context)
+    triage_enter_company_name(context)
+    triage_should_be_classified_as_new(context)
+    triage_create_exporting_journey(context)
+
+
+def classify_as_occasional(context: Context, actor_alias: str):
+    start_triage(context, actor_alias)
+
+
+def classify_as_regular(context: Context, actor_alias: str):
+    start_triage(context, actor_alias)
+
+
+def classify_as(context: Context, actor_alias: str, exporter_status: str):
+    classifications = {
+        "new": classify_as_new,
+        "occasional": classify_as_occasional,
+        "regular": classify_as_regular
+    }
+    visit_page(context, actor_alias, "home")
+    step = classifications[exporter_status.lower()]
+    logging.debug(
+        "%s decided to classify himself/herself as %s Exporter", actor_alias,
+        exporter_status)
+    step(context, actor_alias)
