49507b0cd0865b1d215bf04876275f4696dde6c9
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-18 11:22:56 +0100

    step: when actor starts triage

--- tests/exred/steps/when_def.py
+++ tests/exred/steps/when_def.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 """When step definitions."""
 from behave import when
+
+from tests.exred.steps.when_impl import start_triage
+
+
+@when('"{actor_alias}" decides to get started in Exporting journey section')
+def when_actor_starts_triage(context, actor_alias):
+    start_triage(context, actor_alias)
--- tests/exred/steps/when_impl.py
+++ tests/exred/steps/when_impl.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 """When step implementations."""
+import logging
+
+from behave.runner import Context
+
+from tests.exred.pages import exred_home
+
+
+def start_triage(context: Context, actor_alias: str):
+    exred_home.start_exporting_journey(context.driver)
+    logging.debug("%s started triage process", actor_alias)
