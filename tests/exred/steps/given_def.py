5027445043489e1c5b360f5e0959767938107b81
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-19 15:03:01 +0100

    step: actor answers triage questions

--- tests/exred/steps/given_def.py
+++ tests/exred/steps/given_def.py
@@ -2,7 +2,7 @@
 """Given step definitions."""
 from behave import given
 
-from steps.given_impl import classify_as, visit_page
+from steps.given_imp import classify_as, finish_triage, visit_page
 
 
 @given('"{actor_name}" goes to the "{page_name}" page')
@@ -20,3 +20,8 @@ def given_actor_visits_page_for_the_first_time(context, actor_name, page_name):
 @given('"{actor_alias}" classifies himself as "{exporter_status}" exporter')
 def given_actor_classifies_as(context, actor_alias, exporter_status):
     classify_as(context, actor_alias, exporter_status)
+
+
+@given('"{actor_alias}" has answered triage questions')
+def given_actor_answered_triage_questions(context, actor_alias):
+    finish_triage(context, actor_alias)
