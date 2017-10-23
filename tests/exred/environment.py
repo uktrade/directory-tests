896abbd903c1e9916f80b45d993c977716c0b73a
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-23 16:05:05 +0100

    use BrowserStack

--- tests/exred/environment.py
+++ tests/exred/environment.py
@@ -5,10 +5,14 @@ from collections import namedtuple
 
 from behave.model import Scenario, Step
 from behave.runner import Context
+from selenium import webdriver
 
-import drivers
-from settings import BROWSER, HEIGHT, WIDTH
-from utils import init_loggers, take_screenshot
+from settings import (
+    BROWSER_STACK_CONFIG,
+    BROWSER_STACK_EXECUTOR_URL,
+    BROWSER_STACK_TASK_ID
+)
+from utils import init_loggers
 
 ScenarioData = namedtuple(
     "ScenarioData",
@@ -53,16 +57,6 @@ def after_step(context: Context, step: Step):
     :param step: Behave Step object
     """
     logging.debug('Step: %s %s', step.step_type, str(repr(step.name)))
-    if step.status == "failed":
-        logging.debug(
-            'Step "%s %s" failed. Reason: "%s"', step.step_type, step.name,
-            step.exception)
-        if hasattr(context, "driver"):
-            if hasattr(context, "current_page"):
-                page_name = "{}-{}_failure".format(step.name, context.current_page.NAME)
-            else:
-                page_name = "{}-last_page_failure".format(step.name)
-            take_screenshot(context.driver, page_name)
 
 
 def before_scenario(context: Context, scenario: Scenario):
@@ -73,10 +67,13 @@ def before_scenario(context: Context, scenario: Scenario):
     """
     logging.debug('Starting scenario: %s', scenario.name)
     context.scenario_data = initialize_scenario_data()
-    logging.debug('Starting Selenium Driver for scenario: %s', scenario.name)
-    context.driver = drivers.get(
-        context.browser_name, width=context.viewport_width,
-        height=context.viewport_height)
+    desired_capabilities = context.desired_capabilities
+    desired_capabilities["name"] = scenario.name
+    context.driver = webdriver.Remote(
+        desired_capabilities=desired_capabilities,
+        command_executor=BROWSER_STACK_EXECUTOR_URL
+    )
+    context.driver.maximize_window()
 
 
 def after_scenario(context: Context, scenario: Scenario):
@@ -89,7 +86,7 @@ def after_scenario(context: Context, scenario: Scenario):
         "Deleting all cookies and closing Selenium Driver after scenario: %s",
         scenario.name)
     context.driver.delete_all_cookies()
-    context.driver.close()
+    context.driver.quit()
 
 
 def before_all(context: Context):
@@ -97,17 +94,15 @@ def before_all(context: Context):
 
     :param context: Behave Context object
     """
-    init_loggers(context)
-    context.browser_name = BROWSER
-    context.viewport_width = WIDTH
-    context.viewport_height = HEIGHT
+    desired_capabilities = BROWSER_STACK_CONFIG["environments"][BROWSER_STACK_TASK_ID]
+    browser_name = desired_capabilities["browser"]
 
+    for key in BROWSER_STACK_CONFIG["capabilities"]:
+        if key not in desired_capabilities:
+            desired_capabilities[key] = BROWSER_STACK_CONFIG["capabilities"][key]
 
-def after_all(context: Context):
-    """Place here code which has to be executed after all scenarios.
+    task_id = "{}-{}".format(BROWSER_STACK_TASK_ID, browser_name)
+    init_loggers(context, task_id=task_id)
 
-    :param context: Behave Context object
-    """
-    if hasattr(context, "driver"):
-        logging.debug('Quitting Selenium Driver')
-        context.driver.quit()
+    context.desired_capabilities = desired_capabilities
+    context.browser_name = browser_name
