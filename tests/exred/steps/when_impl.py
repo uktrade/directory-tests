d57bd0f426ba396b29da77395a4c60d9e10229c8
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-24 19:35:45 +0100

    use new PO names

--- tests/exred/steps/when_impl.py
+++ tests/exred/steps/when_impl.py
@@ -6,11 +6,12 @@ from behave.runner import Context
 
 from pages import (
     home,
-    triage_1st_question,
-    triage_2nd_question,
-    triage_3rd_question,
-    triage_4th_question,
-    triage_result
+    triage_are_you_regular_exporter,
+    triage_company_name_or_sole_trader,
+    triage_do_you_use_online_marketplaces,
+    triage_have_you_exported,
+    triage_result,
+    triage_what_is_your_sector
 )
 
 
@@ -21,43 +22,43 @@ def start_triage(context: Context, actor_alias: str):
 
 def triage_select_sector(context: Context, *, sector: str = None):
     driver = context.driver
-    triage_1st_question.select_sector(driver, sector)
-    triage_1st_question.submit(driver)
-    triage_2nd_question.should_be_here(driver)
+    triage_what_is_your_sector.select_sector(driver, sector)
+    triage_what_is_your_sector.submit(driver)
+    triage_have_you_exported.should_be_here(driver)
 
 
 def triage_say_you_exported_before(context: Context):
     driver = context.driver
-    triage_2nd_question.select_yes(driver)
-    triage_2nd_question.submit(driver)
-    triage_3rd_question.should_be_here(driver)
+    triage_have_you_exported.select_yes(driver)
+    triage_have_you_exported.submit(driver)
+    triage_are_you_regular_exporter.should_be_here(driver)
 
 
 def triage_say_you_never_exported_before(context: Context):
     driver = context.driver
-    triage_2nd_question.select_no(driver)
-    triage_2nd_question.submit(driver)
-    triage_4th_question.should_be_here(driver)
+    triage_have_you_exported.select_no(driver)
+    triage_have_you_exported.submit(driver)
+    triage_company_name_or_sole_trader.should_be_here(driver)
 
 
 def triage_say_you_export_regularly(context: Context):
     driver = context.driver
-    triage_3rd_question.select_yes(driver)
-    triage_3rd_question.submit(driver)
-    triage_4th_question.should_be_here(driver)
+    triage_are_you_regular_exporter.select_yes(driver)
+    triage_are_you_regular_exporter.submit(driver)
+    triage_company_name_or_sole_trader.should_be_here(driver)
 
 
 def triage_say_you_do_not_export_regularly(context: Context):
     driver = context.driver
-    triage_3rd_question.select_no(driver)
-    triage_3rd_question.submit(driver)
-    triage_4th_question.should_be_here(driver)
+    triage_are_you_regular_exporter.select_no(driver)
+    triage_are_you_regular_exporter.submit(driver)
+    triage_do_you_use_online_marketplaces.should_be_here(driver)
 
 
 def triage_enter_company_name(context: Context):
     driver = context.driver
-    triage_4th_question.enter_company_name(driver)
-    triage_4th_question.submit(driver)
+    triage_company_name_or_sole_trader.enter_company_name(driver)
+    triage_company_name_or_sole_trader.submit(driver)
     triage_result.should_be_here(driver)
 
 
