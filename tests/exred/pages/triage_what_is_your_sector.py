dced113dcdd59e9c8b3258334dc13ef86052bf16
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-25 19:28:19 +0100

    fix selectors & deal with the new comobobox selector

--- tests/exred/pages/triage_what_is_your_sector.py
+++ tests/exred/pages/triage_what_is_your_sector.py
@@ -5,21 +5,23 @@ import random
 from urllib.parse import urljoin
 
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
 
 from settings import EXRED_UI_URL
-from utils import assertion_msg, take_screenshot
+from utils import assertion_msg, selenium_action, take_screenshot
 
 NAME = "ExRed Triage - what is your sector"
 URL = urljoin(EXRED_UI_URL, "triage")
 
-SECTORS_DROPDOWN = "#id_SECTOR-sector"
-SECTOR_OPTIONS = "#id_SECTOR-sector option"
+SECTORS_COMBOBOX = ".exred-triage-form div[role=combobox]"
+SECTORS_INPUT = "#js-sector-select"
+SECTORS_SELECT_LIST = "js-sector-select-select"
+SECTOR_OPTIONS = "#js-sector-select-select > option"
+AUTOCOMPLETE_1ST_OPTION = "#js-sector-select__option--0"
 CONTINUE_BUTTON = "#content .exred-triage-form button[type=submit]"
 BACK_TO_HOME_LINK = "#content .home-link a"
 EXPECTED_ELEMENTS = {
     "question": "#content .exred-triage-form label",
-    "sectors dropdown": SECTORS_DROPDOWN,
+    "sectors combobox": SECTORS_COMBOBOX,
     "continue button": CONTINUE_BUTTON,
     "back to home link": BACK_TO_HOME_LINK
 }
@@ -55,17 +57,22 @@ def extract_sectors_values(driver: webdriver) -> list:
     options = driver.find_elements_by_css_selector(SECTOR_OPTIONS)
     option_values = []
     for option in options:
-        option_values.append(option.get_attribute("value"))
+        if option.text != "---------":
+            option_values.append(option.text)
     return option_values
 
 
 def select_sector(driver: webdriver, sector: str):
     if not sector:
         sector = random.choice(extract_sectors_values(driver))
-    options = driver.find_element_by_css_selector(SECTORS_DROPDOWN)
-    assert options.is_displayed()
-    select = Select(options)
-    select.select_by_value(sector)
+    with selenium_action(driver, "Can't find Sector selector input box"):
+        input = driver.find_element_by_css_selector(SECTORS_INPUT)
+    input.click()
+    input.clear()
+    input.send_keys(sector)
+    with selenium_action(driver, "Can't find Autocomplete 1st option"):
+        option = driver.find_element_by_css_selector(AUTOCOMPLETE_1ST_OPTION)
+    option.click()
     take_screenshot(driver, NAME)
 
 
