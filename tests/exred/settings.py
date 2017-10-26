4b65833b30bb9eab9faeade8c535f70d7a520c44
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-26 14:53:24 +0100

    simplify build name

--- tests/exred/settings.py
+++ tests/exred/settings.py
@@ -3,8 +3,7 @@ from datetime import datetime
 
 from directory_constants.constants.exred_sector_names import CODES_SECTORS_DICT
 
-BUILD_VERSION = "v0.1"
-BUILD_SUFFIX = os.environ.get("CIRCLE_SHA1", str(datetime.date(datetime.now())))
+BUILD_ID = os.environ.get("CIRCLE_SHA1", str(datetime.date(datetime.now())))
 
 EXRED_UI_URL = os.environ["EXRED_UI_URL"]
 EXRED_SECTORS = CODES_SECTORS_DICT
@@ -15,7 +14,6 @@ BROWSER_STACK_ACCESS_KEY = os.environ.get("BROWSER_STACK_ACCESS_KEY")
 BROWSER_STACK_TASK_ID = int(os.environ.get("TASK_ID", 0))
 BROWSER_STACK_EXECUTOR_URL = ("http://{}:{}@{}/wd/hub".format(
     BROWSER_STACK_USERNAME, BROWSER_STACK_ACCESS_KEY, BROWSER_STACK_SERVER))
-
 BROWSER_STACK_SESSIONS_URL = "https://www.browserstack.com/automate/sessions/{}.json"
 
 BROWSER_STACK_CONFIG = {
@@ -25,7 +23,7 @@ BROWSER_STACK_CONFIG = {
     "capabilities": {
         "browserstack.debug": True,
         "browserstack.selenium_version": "3.5.2",
-        "build": "{}-{}".format(BUILD_VERSION, BUILD_SUFFIX),
+        "build": BUILD_ID,
         "project": "ExRed",
         "resolution": "1600x1200"
     },
