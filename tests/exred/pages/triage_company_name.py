d0bd7497d0e5c314995e0ef5f97b7506c6757389
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-27 19:52:22 +0100

    click on 1st suggestion only if suggestion list is visible

--- tests/exred/pages/triage_company_name.py
+++ tests/exred/pages/triage_company_name.py
@@ -13,6 +13,7 @@ NAME = "ExRed Triage - What is your company name"
 URL = urljoin(EXRED_UI_URL, "triage")
 
 COMPANY_NAME_INPUT = "#js-typeahead-company-name"
+SUGGESTIONS = "ul.SelectiveLookupDisplay"
 FIRST_SUGGESTION = "ul.SelectiveLookupDisplay > li:nth-child(1)"
 CONTINUE_BUTTON = ".exred-triage-form button.button"
 PREVIOUS_STEP_BUTTON = ".exred-triage-form button.previous-step"
@@ -44,8 +45,10 @@ def enter_company_name(driver: webdriver, *, company_name: str = None):
     input_field = driver.find_element_by_css_selector(COMPANY_NAME_INPUT)
     input_field.clear()
     input_field.send_keys(company_name)
-    first_suggestion = driver.find_element_by_css_selector(FIRST_SUGGESTION)
-    first_suggestion.click()
+    suggestions = driver.find_element_by_css_selector(SUGGESTIONS)
+    if suggestions.is_displayed():
+        first_suggestion = driver.find_element_by_css_selector(FIRST_SUGGESTION)
+        first_suggestion.click()
     take_screenshot(driver, NAME + " after typing in company name")
 
 
