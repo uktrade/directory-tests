bcc0493deab752841610ca2dcfa24fb2e2566f7c
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-27 17:15:17 +0100

    load config from file & clean-up variable names

--- tests/exred/settings.py
+++ tests/exred/settings.py
@@ -1,56 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Project Settings."""
 import os
 from datetime import datetime
 
 from directory_constants.constants.exred_sector_names import CODES_SECTORS_DICT
 
-BUILD_ID = os.environ.get("CIRCLE_SHA1", str(datetime.date(datetime.now())))
+import config
 
-EXRED_UI_URL = os.environ["EXRED_UI_URL"]
 EXRED_SECTORS = CODES_SECTORS_DICT
-BROWSER_STACK_SERVER = os.environ.get(
-    "BROWSER_STACK_SERVER", "hub.browserstack.com")
-BROWSER_STACK_USERNAME = os.environ.get("BROWSER_STACK_USERNAME")
-BROWSER_STACK_ACCESS_KEY = os.environ.get("BROWSER_STACK_ACCESS_KEY")
-BROWSER_STACK_TASK_ID = int(os.environ.get("TASK_ID", 0))
-BROWSER_STACK_EXECUTOR_URL = ("http://{}:{}@{}/wd/hub".format(
-    BROWSER_STACK_USERNAME, BROWSER_STACK_ACCESS_KEY, BROWSER_STACK_SERVER))
-BROWSER_STACK_SESSIONS_URL = "https://www.browserstack.com/automate/sessions/{}.json"
 
-BROWSER_STACK_CONFIG = {
-    "server": BROWSER_STACK_SERVER,
-    "user": BROWSER_STACK_USERNAME,
-    "key": BROWSER_STACK_ACCESS_KEY,
-    "capabilities": {
-        "browserstack.debug": True,
-        "browserstack.selenium_version": "3.5.2",
-        "build": BUILD_ID,
-        "project": "ExRed",
-        "resolution": "1600x1200"
-    },
-    "environments": [
-        {
-            "browser": "Chrome",
-            "browser_version": "61.0",
-        },
-        {
-            "browser": "Chrome",
-            "browser_version": "62.0",
-        },
-        {
-            "browser": "Firefox",
-            "browser_version": "56.0",
-        },
-        {
-            "browser": "IE",
-            "browser_version": "11.0",
-        },
-        {
-            "browser": "Edge",
-            "browser_version": "15.0",
-        },
-        {
-            "browser": "Edge",
-            "browser_version": "16.0",
-        },
-    ]
-}
+# variables set in Paver configuration file
+CONFIG_NAME = os.environ.get("CONFIG", "local")
+TASK_ID = int(os.environ.get("TASK_ID", 0))
+
+# optional variables set by user
+BROWSERS = os.environ.get("BROWSERS", "").split()
+HUB_URL = os.environ.get("HUB_URL", None)
+CAPABILITIES = os.environ.get("CAPABILITIES", None)
+BUILD_ID = os.environ.get("CIRCLE_SHA1", str(datetime.date(datetime.now())))
+EXRED_UI_URL = os.environ["EXRED_UI_URL"]
+
+# BrowserStack variables
+BROWSERSTACK_SERVER = os.environ.get(
+    "BROWSERSTACK_SERVER", "hub.browserstack.com")
+BROWSERSTACK_USER = os.environ.get("BROWSERSTACK_USER", "")
+BROWSERSTACK_PASS = os.environ.get("BROWSERSTACK_PASS", "")
+BROWSERSTACK_EXECUTOR_URL = ("http://{}:{}@{}/wd/hub".format(
+    BROWSERSTACK_USER, BROWSERSTACK_PASS, BROWSERSTACK_SERVER))
+BROWSERSTACK_SESSIONS_URL = "https://www.browserstack.com/automate/sessions/{}.json"
+
+
+if (CONFIG_NAME == "browserstack" and
+        (BROWSERSTACK_SERVER and BROWSERSTACK_USER and BROWSERSTACK_PASS)):
+    HUB_URL = BROWSERSTACK_EXECUTOR_URL
+
+CONFIG = config.get(
+    config_file=CONFIG_NAME, hub_url=HUB_URL,
+    capabilities=CAPABILITIES, browsers=BROWSERS, build_id=BUILD_ID)
