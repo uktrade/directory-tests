12103a4e067b59da544ef42b068532a207e74472
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-30 12:39:30 +0000

    step: should see the Guidance Navigation Ribbon

--- tests/exred/steps/then_def.py
+++ tests/exred/steps/then_def.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
-"""Then step definitions."""
+"""Then steps definitions."""
 from behave import then
 
-from steps.then_impl import should_be_on_page, should_see_sections_on_home_page
+from steps.then_impl import (
+    guidance_ribbon_should_be_visible,
+    should_be_on_page,
+    should_see_sections_on_home_page
+)
 
 
 @then('"{actor_name}" should see the "{sections}" sections on home page')
@@ -13,3 +17,8 @@ def then_actor_should_see_sections(context, actor_name, sections):
 @then('"{actor_alias}" should be on the "{page_name}" page')
 def then_actor_should_be_on_page(context, actor_alias, page_name):
     should_be_on_page(context, actor_alias, page_name)
+
+
+@then('"{actor_alias}" should see the Guidance Navigation Ribbon')
+def then_guidance_ribbon_should_be_visible(context, actor_alias):
+    guidance_ribbon_should_be_visible(context, actor_alias)
--- tests/exred/steps/then_impl.py
+++ tests/exred/steps/then_impl.py
@@ -4,7 +4,7 @@ import logging
 
 from behave.runner import Context
 
-from pages import home
+from pages import guidance_common, home
 from registry import get_page_object
 
 
@@ -21,3 +21,10 @@ def should_be_on_page(context: Context, actor_alias: str, page_name: str):
     page = get_page_object(page_name)
     page.should_be_here(context.driver)
     logging.debug("%s is on %s page", actor_alias, page_name)
+
+
+def guidance_ribbon_should_be_visible(context: Context, actor_alias: str):
+    driver = context.driver
+    guidance_common.ribbon_should_be_visible(driver)
+    logging.debug(
+        "%s can see Guidance Ribbon on %s", actor_alias, driver.current_url)
