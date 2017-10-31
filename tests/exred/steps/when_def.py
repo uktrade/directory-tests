5837507a75135ea8ddd9cdfab44ec0daf2c9b72c
kowalcj0 <kowalczykjanusz@gmail.com>
2017-10-31 17:05:46 +0000

    step: a "when" alias for opening a guidance category

--- tests/exred/steps/when_def.py
+++ tests/exred/steps/when_def.py
@@ -2,6 +2,7 @@
 """When step definitions."""
 from behave import when
 
+from steps.given_imp import guidance_open_category
 from steps.when_impl import start_triage
 
 
@@ -9,3 +10,8 @@ from steps.when_impl import start_triage
 @when('"{actor_alias}" decides to get started in Exporting journey section')
 def when_actor_starts_triage(context, actor_alias):
     start_triage(context, actor_alias)
+
+
+@when('"{actor_alias}" goes to the "{category}" Guidance articles via "{location}"')
+def step_impl(context, actor_alias, category, location):
+    guidance_open_category(context, actor_alias, category, location)
