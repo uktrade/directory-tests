39a245b2fa0fe4c32a49615500823d034178344f
kowalcj0 <kowalczykjanusz@gmail.com>
2017-11-02 13:21:37 +0000

    grouped sections into one dict

--- tests/exred/pages/personalised_journey.py
+++ tests/exred/pages/personalised_journey.py
@@ -12,44 +12,56 @@ from utils import assertion_msg, selenium_action, take_screenshot
 NAME = "ExRed Personalised Journey"
 URL = urljoin(EXRED_UI_URL, "custom")
 
-SHOW_MORE_BUTTON = "#persona-overview a.button.more"
-HERO_SECTION = {
-    "hero - title": "section.hero-section h1",
-    "hero - introduction": "section.hero-section p",
-    "hero - exporting is great logo": "section.hero-section img",
-}
-FACTS_SECTION = {
-    "facts - intro": "#content > section.sector-fact p.intro",
-    "facts - figures": "#content > section.sector-fact p.figure",
-}
-ARTICLES_SECTION = {
-    "articles - heading": "#persona-overview h2",
-    "articles - introduction": "#persona-overview p.intro",
-    "articles - article list": "#persona-overview div.section-content-list",
-}
-TOP_10_SECTION = {
-    "top 10 - heading": "#content > section.markets h1",
-    "top 10 - table": "#content > section.markets table",
-}
-SERVICES_SECTION = {
-    "services - heading": "section.service-section h2",
-    "services - intro": "section.service-section .intro",
-    "services - other": "#other-services > div > div",
-}
-GUIDANCE_SECTION = {
-    "guidance - heading": "#resource-guidance h2",
-    "guidance - introduction": "#resource-guidance p.section-intro",
-    "guidance - categories": "#resource-guidance div.group",
-}
-
-EXPECTED_ELEMENTS = {}
-EXPECTED_ELEMENTS.update(HERO_SECTION)
 SHOW_MORE_BUTTON = "#js-paginate-list-more"
 READ_COUNTER = "#articles .scope-indicator .position > span.from"
 
+MARKET_RESEARCH_LINK = "#resource-guidance a[href='/market-research']"
+CUSTOMER_INSIGHT_LINK = "#resource-guidance a[href='/customer-insight']"
+FINANCE_LINK = "#resource-guidance a[href='/finance']"
+BUSINESS_LINK = "#resource-guidance a[href='/business-planning']"
+GETTING_PAID_LINK = "#resource-guidance a[href='/getting-paid']"
+OPERATIONS_AND_COMPLIANCE_LINK = "#resource-guidance a[href='/operations-and-compliance']"
+
+SECTIONS = {
+    "hero": {
+        "hero - title": "section.hero-section h1",
+        "hero - introduction": "section.hero-section p",
+        "hero - exporting is great logo": "section.hero-section img",
+    },
+    "facts": {
+        "facts - intro": "#content > section.sector-fact p.intro",
+        "facts - figures": "#content > section.sector-fact p.figure",
+    },
+    "articles": {
+        "articles - heading": "#persona-overview h2",
+        "articles - introduction": "#persona-overview p.intro",
+        "articles - article list": "#persona-overview div.section-content-list",
+    },
+    "top 10": {
+        "top 10 - heading": "#content > section.markets h1",
+        "top 10 - table": "#content > section.markets table",
+    },
+    "services": {
+        "services - heading": "section.service-section h2",
+        "services - intro": "section.service-section .intro",
+        "services - other": "#other-services > div > div",
+    },
+    "guidance": {
+        "guidance - heading": "#resource-guidance h2",
+        "guidance - introduction": "#resource-guidance p.section-intro",
+        "guidance - categories": "#resource-guidance div.group",
+        "market research": MARKET_RESEARCH_LINK,
+        "customer insight": CUSTOMER_INSIGHT_LINK,
+        "finance": FINANCE_LINK,
+        "business planning": BUSINESS_LINK,
+        "getting paid": GETTING_PAID_LINK,
+        "operations and compliance": OPERATIONS_AND_COMPLIANCE_LINK,
+    }
+}
+
 
 def should_be_here(driver: webdriver):
-    for element_name, element_selector in EXPECTED_ELEMENTS.items():
+    for element_name, element_selector in SECTIONS["hero"].items():
         with selenium_action(
                 driver, "Could not find '%s' using '%s'", element_name,
                 element_selector):
