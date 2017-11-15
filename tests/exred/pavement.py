dc9afbca752e8864b26b5acd72ebf73cd0236e46
kowalcj0 <kowalczykjanusz@gmail.com>
2017-11-15 12:28:12 +0000

    Revert "run only 1 scenario for testing purposes"
    This reverts commit 88d9627f8d908be17450f3a3f0bc504ce3c876cd.

--- tests/exred/pavement.py
+++ tests/exred/pavement.py
@@ -30,7 +30,7 @@ def run_behave_test(
         versions: str = "", tag: str = None):
     extra_tag = "--tags={}".format(tag) if tag else ""
     sh("BROWSERS={} VERSIONS={} CONFIG={} TASK_ID={} behave -k --format "
-       "progress3 --no-logcapture --tags=-wip --tags=-skip --tags=~fixme -t ED-2613 {}"
+       "progress3 --no-logcapture --tags=-wip --tags=-skip --tags=~fixme {}"
         .format(browsers, versions, config_name, task_id, extra_tag))
 
 
