a056c50c0db9901994290477f475c36e055c6539
kowalcj0 <kowalczykjanusz@gmail.com>
2017-11-22 21:21:12 +0000

    add new required env variables

--- tests/exred/settings.py
+++ tests/exred/settings.py
@@ -2,6 +2,7 @@
 """Project Settings."""
 import os
 from datetime import datetime
+from urllib import parse as urlparse
 
 from directory_constants.constants.exred_sector_names import CODES_SECTORS_DICT
 
@@ -21,6 +22,7 @@ CAPABILITIES = os.environ.get("CAPABILITIES", None)
 BUILD_ID = os.environ.get("CIRCLE_SHA1", str(datetime.date(datetime.now())))
 EXRED_UI_URL = os.environ["EXRED_UI_URL"]
 DIRECTORY_UI_BUYER_URL = os.environ["DIRECTORY_UI_BUYER_URL"]
+DIRECTORY_UI_SSO_URL = os.environ["DIRECTORY_UI_SSO_URL"]
 SELLING_ONLINE_OVERSEAS_UI_URL = os.environ["SELLING_ONLINE_OVERSEAS_UI_URL"]
 EXPORT_OPPORTUNITIES_UI_URL = os.environ["EXPORT_OPPORTUNITIES_UI_URL"]
 EVENTS_UI_URL = os.environ["EVENTS_UI_URL"]
@@ -39,6 +41,15 @@ BROWSERSTACK_PASS = os.environ.get("BROWSERSTACK_PASS", "")
 BROWSERSTACK_EXECUTOR_URL = ("http://{}:{}@{}/wd/hub".format(
     BROWSERSTACK_USER, BROWSERSTACK_PASS, BROWSERSTACK_SERVER))
 BROWSERSTACK_SESSIONS_URL = "https://www.browserstack.com/automate/sessions/{}.json"
+
+
+# These DB details are required to do post-test clean-up in SSO DB
+SSO_DB_URL = urlparse.urlparse(os.getenv('SSO_DATABASE_URL'))
+SSO_DB_NAME = SSO_DB_URL.path[1:] if SSO_DB_URL else None
+SSO_DB_USER = SSO_DB_URL.username if SSO_DB_URL else None
+SSO_DB_PASSWORD = SSO_DB_URL.password if SSO_DB_URL else None
+SSO_DB_HOST = SSO_DB_URL.hostname if SSO_DB_URL else None
+SSO_DB_PORT = SSO_DB_URL.port if SSO_DB_URL else None
 
 
 if (CONFIG_NAME.startswith("browserstack") and
