1f5e790508b2a8ccda52938d81278d3344c163c4
kowalcj0 <kowalczykjanusz@gmail.com>
2017-11-27 18:19:22 +0000

    there's no need to refresh the page

--- tests/exred/steps/then_impl.py
+++ tests/exred/steps/then_impl.py
@@ -348,6 +348,5 @@ def articles_read_counter_same_as_before_registration(
         context: Context, actor_alias: str):
     actor = get_actor(context, actor_alias)
     preregistration_value = actor.articles_read_counter
-    context.driver.refresh()
     articles_should_see_read_counter_set_to(
         context, actor_alias, preregistration_value)
