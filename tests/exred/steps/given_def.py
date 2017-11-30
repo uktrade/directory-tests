2ceff1b0fc9d9ec25234a9fb6d1728dc204e1e64
kowalcj0 <kowalczykjanusz@gmail.com>
2017-11-30 17:05:02 +0000

    step: add new aliases

--- tests/exred/steps/given_def.py
+++ tests/exred/steps/given_def.py
@@ -28,6 +28,7 @@ from steps.when_impl import (
 )
 
 
+@given('"{actor_alias}" went to the "{page_name}" page')
 @given('"{actor_alias}" goes to the "{page_name}" page')
 @given('"{actor_alias}" visits the "{page_name}" page')
 def given_actor_visits_page(context, actor_alias, page_name):
