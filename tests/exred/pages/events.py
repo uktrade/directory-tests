efb8bcb04b9d2c0929808b04f0cc997fd311b14c
kowalcj0 <kowalczykjanusz@gmail.com>
2017-12-07 11:31:51 +0000

    improve test stability by waiting for elements on external sites

--- tests/exred/pages/events.py
+++ tests/exred/pages/events.py
@@ -6,14 +6,17 @@ from urllib.parse import urljoin
 from selenium import webdriver
 
 from settings import EVENTS_UI_URL
-from utils import assertion_msg, take_screenshot
+from utils import assertion_msg, take_screenshot, wait_for_visibility
 
 NAME = "Events Home page"
 URL = urljoin(EVENTS_UI_URL, "")
 PAGE_TITLE = "Department for International Trade (DIT): exporting from or investing in the UK"
 
+WELCOME_MESSAGE = "#feature > div.contain.slider > article.tile.initial.visible > div > h2"
+
 
 def should_be_here(driver: webdriver):
+    wait_for_visibility(driver, by_css=WELCOME_MESSAGE, time_to_wait=15)
     with assertion_msg(
             "Expected page title to be: '%s' but got '%s'", PAGE_TITLE,
             driver.title):
--- tests/exred/pages/export_opportunities.py
+++ tests/exred/pages/export_opportunities.py
@@ -6,14 +6,17 @@ from urllib.parse import urljoin
 from selenium import webdriver
 
 from settings import EXPORT_OPPORTUNITIES_UI_URL
-from utils import assertion_msg, take_screenshot
+from utils import assertion_msg, take_screenshot, wait_for_visibility
 
 NAME = "Export Opportunities Home page"
 URL = urljoin(EXPORT_OPPORTUNITIES_UI_URL, "")
 PAGE_TITLE = "Exporting is GREAT"
 
+WELCOME_MESSAGE = "#content-top > div.hero__specialContainer > h1"
+
 
 def should_be_here(driver: webdriver):
+    wait_for_visibility(driver, by_css=WELCOME_MESSAGE, time_to_wait=15)
     with assertion_msg(
             "Expected page title to be: '%s' but got '%s'", PAGE_TITLE,
             driver.title):
