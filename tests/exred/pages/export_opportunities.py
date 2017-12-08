15364899c24f49fdafd42943278d01fab3795b80
kowalcj0 <kowalczykjanusz@gmail.com>
2017-12-08 13:01:19 +0000

    update page title for Export Opportunities

--- tests/exred/pages/export_opportunities.py
+++ tests/exred/pages/export_opportunities.py
@@ -10,7 +10,7 @@ from utils import assertion_msg, take_screenshot, wait_for_visibility
 
 NAME = "Export Opportunities Home page"
 URL = urljoin(EXPORT_OPPORTUNITIES_UI_URL, "")
-PAGE_TITLE = "Exporting is GREAT"
+PAGE_TITLE = "Export opportunities"
 
 WELCOME_MESSAGE = "#content-top > div.hero__specialContainer > h1"
 
