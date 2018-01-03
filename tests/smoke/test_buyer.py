40a4b659b5e20766dd7527e68a2847c1c19106fc
kowalcj0 <kowalczykjanusz@gmail.com>
2018-01-03 12:15:37 +0000

    expect 301 instead of 302 & enable redirect following where needed

--- tests/smoke/test_buyer.py
+++ tests/smoke/test_buyer.py
@@ -83,12 +83,12 @@ def test_profile_redirects_anon_user():
         get_absolute_url('ui-buyer:company-profile'), allow_redirects=False
     )
 
-    assert response.status_code == http.client.FOUND
+    assert response.status_code == http.client.MOVED_PERMANENTLY
 
 
 def test_enrolment_200_company_user(logged_in_session):
     response = logged_in_session.get(
-        get_absolute_url('ui-buyer:company-profile'), allow_redirects=False
+        get_absolute_url('ui-buyer:company-profile'), allow_redirects=True
     )
 
     assert response.status_code == http.client.OK
@@ -99,12 +99,12 @@ def test_profile_logo_edit_redirects_anon_user():
         get_absolute_url('ui-buyer:upload-logo'), allow_redirects=False
     )
 
-    assert response.status_code == http.client.FOUND
+    assert response.status_code == http.client.MOVED_PERMANENTLY
 
 
 def test_profile_logo_edit_200_company_user(logged_in_session):
     response = logged_in_session.get(
-        get_absolute_url('ui-buyer:upload-logo'), allow_redirects=False
+        get_absolute_url('ui-buyer:upload-logo'), allow_redirects=True
     )
 
     assert response.status_code == http.client.OK
@@ -116,13 +116,13 @@ def test_profile_address_edit_redirects_anon_user():
         allow_redirects=False
     )
 
-    assert response.status_code == http.client.FOUND
+    assert response.status_code == http.client.MOVED_PERMANENTLY
 
 
 def test_profile_address_edit_200_company_user(logged_in_session):
     response = logged_in_session.get(
         get_absolute_url('ui-buyer:company-edit-address'),
-        allow_redirects=False
+        allow_redirects=True
     )
 
     assert response.status_code == http.client.OK
@@ -134,13 +134,13 @@ def test_profile_description_edit_redirects_anon_user():
         allow_redirects=False
     )
 
-    assert response.status_code == http.client.FOUND
+    assert response.status_code == http.client.MOVED_PERMANENTLY
 
 
 def test_profile_description_edit_200_company_user(logged_in_session):
     response = logged_in_session.get(
         get_absolute_url('ui-buyer:company-edit-description'),
-        allow_redirects=False
+        allow_redirects=True
     )
 
     assert response.status_code == http.client.OK
@@ -152,13 +152,13 @@ def test_profile_key_facts_edit_redirects_anon_user():
         allow_redirects=False
     )
 
-    assert response.status_code == http.client.FOUND
+    assert response.status_code == http.client.MOVED_PERMANENTLY
 
 
 def test_profile_key_facts_edit_200_company_user(logged_in_session):
     response = logged_in_session.get(
         get_absolute_url('ui-buyer:company-edit-key-facts'),
-        allow_redirects=False
+        allow_redirects=True
     )
 
     assert response.status_code == http.client.OK
@@ -170,13 +170,13 @@ def test_profile_sectors_edit_redirects_anon_user():
         allow_redirects=False
     )
 
-    assert response.status_code == http.client.FOUND
+    assert response.status_code == http.client.MOVED_PERMANENTLY
 
 
 def test_profile_sectors_edit_200_company_user(logged_in_session):
     response = logged_in_session.get(
         get_absolute_url('ui-buyer:company-edit-sectors'),
-        allow_redirects=False
+        allow_redirects=True
     )
 
     assert response.status_code == http.client.OK
@@ -188,13 +188,13 @@ def test_profile_contact_edit_redirects_anon_user():
         allow_redirects=False
     )
 
-    assert response.status_code == http.client.FOUND
+    assert response.status_code == http.client.MOVED_PERMANENTLY
 
 
 def test_profile_contact_edit_200_company_user(logged_in_session):
     response = logged_in_session.get(
         get_absolute_url('ui-buyer:company-edit-contact'),
-        allow_redirects=False
+        allow_redirects=True
     )
 
     assert response.status_code == http.client.OK
@@ -206,13 +206,13 @@ def test_profile_social_media_edit_redirects_anon_user():
         allow_redirects=False
     )
 
-    assert response.status_code == http.client.FOUND
+    assert response.status_code == http.client.MOVED_PERMANENTLY
 
 
 def test_profile_social_media_edit_200_company_user(logged_in_session):
     response = logged_in_session.get(
         get_absolute_url('ui-buyer:company-edit-social-media'),
-        allow_redirects=False
+        allow_redirects=True
     )
 
     assert response.status_code == http.client.OK
