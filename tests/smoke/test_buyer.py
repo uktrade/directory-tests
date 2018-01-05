a50838730aaadf69d5e7acc2a5e9a54f4bb6634e
kowalcj0 <kowalczykjanusz@gmail.com>
2018-01-05 12:30:29 +0000

    replaced FAB tests with parametrized ones to increase coverage

--- tests/smoke/test_buyer.py
+++ tests/smoke/test_buyer.py
@@ -1,31 +1,10 @@
 import http.client
 
+import pytest
 import requests
 
 from tests import get_absolute_url, companies
 from tests.settings import DIRECTORY_API_HEALTH_CHECK_TOKEN as TOKEN
-
-
-def test_healthcheck_api():
-    params = {'token': TOKEN}
-    response = requests.get(
-            get_absolute_url('ui-buyer:healthcheck-api'), params=params)
-    assert response.status_code == http.client.OK
-
-
-def test_healthcheck_sso():
-    params = {'token': TOKEN}
-    response = requests.get(
-            get_absolute_url('ui-buyer:healthcheck-sso'), params=params)
-    assert response.status_code == http.client.OK
-
-
-def test_landing_page_200():
-    response = requests.get(
-        get_absolute_url('ui-buyer:landing'), allow_redirects=False
-    )
-
-    assert response.status_code == http.client.OK
 
 
 def test_landing_page_post_company_not_active():
@@ -58,175 +37,119 @@ def test_landing_page_post_company_happy_path():
         get_absolute_url('ui-buyer:landing'),
         data=data
     )
-
     assert 'Register' in str(response.content)
-
-
-def test_enrolment_instructions_302_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:register'), allow_redirects=False
-    )
-
-    assert response.status_code == http.client.FOUND
-
-
-def test_enrolment_redirects_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:register'), allow_redirects=False
-    )
-
-    assert response.status_code == http.client.FOUND
-
-
-def test_profile_redirects_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:company-profile'), allow_redirects=False
-    )
-
-    assert response.status_code == http.client.FOUND
-
-
-def test_enrolment_200_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:company-profile'), allow_redirects=True
-    )
-
-    assert response.status_code == http.client.OK
-
-
-def test_profile_logo_edit_redirects_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:upload-logo'), allow_redirects=False
-    )
-
-    assert response.status_code == http.client.MOVED_PERMANENTLY
-
-
-def test_profile_logo_edit_200_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:upload-logo'), allow_redirects=True
-    )
-
-    assert response.status_code == http.client.OK
-
-
-def test_profile_address_edit_redirects_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:company-edit-address'),
-        allow_redirects=False
-    )
-
-    assert response.status_code == http.client.MOVED_PERMANENTLY
-
-
-def test_profile_address_edit_200_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:company-edit-address'),
-        allow_redirects=True
-    )
-
-    assert response.status_code == http.client.OK
-
-
-def test_profile_description_edit_redirects_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:company-edit-description'),
-        allow_redirects=False
-    )
-
-    assert response.status_code == http.client.FOUND
-
-
-def test_profile_description_edit_200_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:company-edit-description'),
-        allow_redirects=True
-    )
-
-    assert response.status_code == http.client.OK
-
-
-def test_profile_key_facts_edit_redirects_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:company-edit-key-facts'),
-        allow_redirects=False
-    )
-
-    assert response.status_code == http.client.MOVED_PERMANENTLY
-
-
-def test_profile_key_facts_edit_200_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:company-edit-key-facts'),
-        allow_redirects=True
-    )
-
-    assert response.status_code == http.client.OK
-
-
-def test_profile_sectors_edit_redirects_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:company-edit-sectors'),
-        allow_redirects=False
-    )
-
-    assert response.status_code == http.client.MOVED_PERMANENTLY
-
-
-def test_profile_sectors_edit_200_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:company-edit-sectors'),
-        allow_redirects=True
-    )
-
-    assert response.status_code == http.client.OK
-
-
-def test_profile_contact_edit_redirects_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:company-edit-contact'),
-        allow_redirects=False
-    )
-
-    assert response.status_code == http.client.MOVED_PERMANENTLY
-
-
-def test_profile_contact_edit_200_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:company-edit-contact'),
-        allow_redirects=True
-    )
-
-    assert response.status_code == http.client.OK
-
-
-def test_profile_social_media_edit_redirects_anon_user():
-    response = requests.get(
-        get_absolute_url('ui-buyer:company-edit-social-media'),
-        allow_redirects=False
-    )
-
-    assert response.status_code == http.client.MOVED_PERMANENTLY
-
-
-def test_profile_social_media_edit_200_company_user(logged_in_session):
-    response = logged_in_session.get(
-        get_absolute_url('ui-buyer:company-edit-social-media'),
-        allow_redirects=True
-    )
-
-    assert response.status_code == http.client.OK
 
 
 def test_not_existing_page_return_404_anon_user():
     url = get_absolute_url('ui-buyer:landing') + '/foobar'
     response = requests.get(url, allow_redirects=False)
-
     assert response.status_code == 404
 
 
 def test_not_existing_page_return_404_user(logged_in_session):
     url = get_absolute_url('ui-buyer:landing') + '/foobar'
     response = logged_in_session.get(url, allow_redirects=False)
-
     assert response.status_code == 404
+
+
+@pytest.mark.parametrize("absolute_url", [
+    get_absolute_url('ui-buyer:healthcheck-api'),
+    get_absolute_url('ui-buyer:healthcheck-sso'),
+])
+def test_health_check_endpoints(absolute_url):
+    params = {'token': TOKEN}
+    response = requests.get(absolute_url, params=params)
+    assert response.status_code == http.client.OK
+
+
+@pytest.mark.parametrize("absolute_url", [
+    get_absolute_url('ui-buyer:healthcheck-api'),
+    get_absolute_url('ui-buyer:healthcheck-sso'),
+])
+def test_redirects_for_health_check_endpoints(absolute_url):
+    params = {'token': TOKEN}
+    # get rid of trailing slash
+    absolute_url = absolute_url[:-1]
+    response = requests.get(absolute_url, params=params, allow_redirects=False)
+    assert response.status_code == http.client.MOVED_PERMANENTLY
+
+
+@pytest.mark.parametrize("absolute_url", [
+    get_absolute_url('ui-buyer:register'),
+    get_absolute_url('ui-buyer:register-confirm-export-status'),
+    get_absolute_url('ui-buyer:register-submit-account-details'),
+    get_absolute_url('ui-buyer:case-study-create'),
+    get_absolute_url('ui-buyer:case-study-edit'),
+    get_absolute_url('ui-buyer:confirm-company-address'),
+    get_absolute_url('ui-buyer:confirm-identity'),
+    get_absolute_url('ui-buyer:confirm-identity-letter'),
+    get_absolute_url('ui-buyer:company-profile'),
+    get_absolute_url('ui-buyer:company-edit'),
+    get_absolute_url('ui-buyer:company-edit-description'),
+])
+def test_301_redirects_for_anon_user(absolute_url):
+    response = requests.get(absolute_url, allow_redirects=False)
+    assert response.status_code == http.client.FOUND
+
+
+@pytest.mark.parametrize("absolute_url", [
+    get_absolute_url('ui-buyer:company-edit-key-facts'),
+    get_absolute_url('ui-buyer:company-edit-sectors'),
+    get_absolute_url('ui-buyer:company-edit-contact'),
+    get_absolute_url('ui-buyer:company-edit-social-media'),
+])
+def test_302_redirects_for_anon_user(absolute_url):
+    response = requests.get(absolute_url, allow_redirects=False)
+    assert response.status_code == http.client.MOVED_PERMANENTLY
+
+
+@pytest.mark.parametrize("absolute_url", [
+    get_absolute_url('ui-buyer:healthcheck-api'),
+    get_absolute_url('ui-buyer:healthcheck-sso'),
+    get_absolute_url('ui-buyer:register-confirm-company'),
+    get_absolute_url('ui-buyer:register-confirm-export-status'),
+    get_absolute_url('ui-buyer:register-finish'),
+    get_absolute_url('ui-buyer:register-submit-account-details'),
+    get_absolute_url('ui-buyer:case-study-create'),
+    get_absolute_url('ui-buyer:case-study-edit'),
+    get_absolute_url('ui-buyer:confirm-company-address'),
+    get_absolute_url('ui-buyer:confirm-identity'),
+    get_absolute_url('ui-buyer:confirm-identity-letter'),
+    get_absolute_url('ui-buyer:company-profile'),
+    get_absolute_url('ui-buyer:company-edit'),
+    get_absolute_url('ui-buyer:company-edit-description'),
+])
+def test_302_redirects_after_removing_trailing_slash_for_anon_user(
+        absolute_url):
+    # get rid of trailing slash
+    absolute_url = absolute_url[:-1]
+    response = requests.get(absolute_url, allow_redirects=False)
+    assert response.status_code == http.client.MOVED_PERMANENTLY
+
+
+@pytest.mark.parametrize("absolute_url", [
+    get_absolute_url('ui-buyer:landing'),
+    get_absolute_url('ui-buyer:register'),
+    get_absolute_url('ui-buyer:register-confirm-company'),
+    get_absolute_url('ui-buyer:register-confirm-export-status'),
+    get_absolute_url('ui-buyer:register-finish'),
+    get_absolute_url('ui-buyer:register-submit-account-details'),
+    get_absolute_url('ui-buyer:upload-logo'),
+    get_absolute_url('ui-buyer:case-study-create'),
+    get_absolute_url('ui-buyer:case-study-edit'),
+    get_absolute_url('ui-buyer:confirm-company-address'),
+    get_absolute_url('ui-buyer:confirm-identity'),
+    get_absolute_url('ui-buyer:confirm-identity-letter'),
+    get_absolute_url('ui-buyer:company-profile'),
+    get_absolute_url('ui-buyer:company-edit'),
+    get_absolute_url('ui-buyer:company-edit-address'),
+    get_absolute_url('ui-buyer:company-edit-description'),
+    get_absolute_url('ui-buyer:company-edit-key-facts'),
+    get_absolute_url('ui-buyer:company-edit-sectors'),
+    get_absolute_url('ui-buyer:company-edit-contact'),
+    get_absolute_url('ui-buyer:company-edit-social-media'),
+])
+def test_access_non_health_check_endpoints_as_logged_in_user(
+        logged_in_session, absolute_url):
+    response = logged_in_session.get(absolute_url, allow_redirects=True)
+    assert response.status_code == http.client.OK
