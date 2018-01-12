627e5410fdc98787935879c73f08c7300f73a148
kowalcj0 <kowalczykjanusz@gmail.com>
2018-01-12 15:56:47 +0000

    add a module for storing common PO actions

---
+++ tests/exred/pages/common_actions.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Common PageObject actions
+
+REMEMBER:
+ to overwrite variables from this module in your PageObject.
+"""
+from selenium import webdriver
+
+from utils import clear_driver_cookies, take_screenshot
+
+
+def visit(
+        driver: webdriver, url: str, page_name: str, *,
+        first_time: bool = False):
+    """Open the URL.
+
+    :param driver: Any Selenium Driver (Remote, Chrome, Firefox, PhantomJS etc.
+    :param url: URL to open
+    :param page_name: human friend page name used in screenshot name
+    :param first_time: (optional) will delete all cookies if True
+    """
+    if first_time:
+        clear_driver_cookies(driver)
+    driver.get(url)
+    take_screenshot(driver, page_name)
