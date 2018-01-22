71549a96a0ab4bf9fb1719c069214576e6011c3a
kowalcj0 <kowalczykjanusz@gmail.com>
2018-01-22 11:11:31 +0000

    PO: add action to visit events page

--- tests/exred/pages/events.py
+++ tests/exred/pages/events.py
@@ -5,6 +5,7 @@ from urllib.parse import urljoin
 
 from selenium import webdriver
 
+from pages.common_actions import visit as common_visit
 from settings import EVENTS_UI_URL
 from utils import assertion_msg, take_screenshot, wait_for_visibility
 
@@ -12,6 +13,9 @@ NAME = "Events Home page"
 URL = urljoin(EVENTS_UI_URL, "")
 PAGE_TITLE = "Department for International Trade (DIT): exporting from or investing in the UK"
 
+
+def visit(driver: webdriver, *, first_time: bool = False):
+    common_visit(driver, URL, NAME, first_time=first_time)
 
 
 def should_be_here(driver: webdriver):
