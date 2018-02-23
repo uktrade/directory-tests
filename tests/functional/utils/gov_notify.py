de364c2cedde0d74cb1b1ce6646c13072b6ad08a
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-23 18:32:03 +0000

    add gov notify helpers

---
+++ tests/functional/utils/gov_notify.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""Common operations for Gov Notify service"""
+import logging
+
+from notifications_python_client import NotificationsAPIClient
+from retrying import retry
+
+from tests.functional.utils.generic import assertion_msg
+from tests.settings import (
+    GOV_NOTIFY_API_KEY,
+    SSO_PASSWORD_RESET_MSG_SUBJECT,
+    EMAIL_VERIFICATION_MSG_SUBJECT
+)
+
+GOV_NOTIFY_CLIENT = NotificationsAPIClient(GOV_NOTIFY_API_KEY)
+
+
+def extract_email_confirmation_link(payload: str) -> str:
+    """Find email confirmation link inside the plain text email payload."""
+    start = payload.find("https")
+    activation_link = payload[start:]
+    logging.debug("Found email confirmation link: %s", activation_link)
+    return activation_link
+
+
+def extract_password_reset_link(payload: str) -> str:
+    """Find password reset link inside the plain text email payload."""
+    start = payload.find("https")
+    end = payload.find("\r\n", start)
+    password_reset_link = payload[start:end]
+    with assertion_msg(
+            "Extracted link is not a correct password reset link: %s",
+            password_reset_link):
+        assert "accounts/password/reset/key/" in password_reset_link
+    logging.debug("Found password reset link: %s", password_reset_link)
+    return password_reset_link
+
+
+def filter_by_subject(notifications: list, subject: str) -> list:
+    return list(filter(lambda x: x["subject"] == subject, notifications))
+
+
+def filter_by_recipient(notifications: list, email: str) -> list:
+    return list(filter(lambda x: x["email_address"] == email, notifications))
+
+
+@retry(wait_fixed=5000, stop_max_attempt_number=5)
+def get_email_confirmation_notification(
+        email: str, *, subject: str = EMAIL_VERIFICATION_MSG_SUBJECT) -> dict:
+    notifications = GOV_NOTIFY_CLIENT.get_all_notifications(
+        template_type="email")["notifications"]
+
+    user_notifications = filter_by_recipient(notifications, email)
+    email_confirmations = filter_by_subject(user_notifications, subject)
+
+    assert len(email_confirmations) == 1, (
+        "Expected to find 1 email confirmation notification for {} but found "
+        "{}".format(email, len(email_confirmations)))
+
+    return email_confirmations[0]
+
+
+@retry(wait_fixed=5000, stop_max_attempt_number=5)
+def get_password_reset_notification(
+        email: str, *, subject: str = SSO_PASSWORD_RESET_MSG_SUBJECT) -> dict:
+    notifications = GOV_NOTIFY_CLIENT.get_all_notifications(
+        template_type="email")["notifications"]
+
+    user_notifications = filter_by_recipient(notifications, email)
+    password_reset_notifications = filter_by_subject(
+        user_notifications, subject)
+
+    assert len(password_reset_notifications) == 1, (
+        "Expected to find 1 password reset notification for {} but found "
+        "{}".format(email, len(password_reset_notifications)))
+
+    return password_reset_notifications[0]
+
+
+def get_verification_link(email: str) -> str:
+    logging.debug("Searching for verification email of: {}".format(email))
+    notification = get_email_confirmation_notification(email)
+    body = notification["body"]
+    return extract_email_confirmation_link(body)
+
+
+def get_password_reset_link(email: str) -> str:
+    logging.debug("Searching for password reset email of: {}".format(email))
+    notification = get_password_reset_notification(email)
+    body = notification["body"]
+    return extract_password_reset_link(body)
