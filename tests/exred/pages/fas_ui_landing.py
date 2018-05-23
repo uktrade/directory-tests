bfb4b718a17b22b125a1411d5e4afc758a838f24
kowalcj0 <kowalczykjanusz@gmail.com>
2018-05-23 12:21:19 +0100

    PO: add search action & section checker

--- tests/exred/pages/fas_ui_landing.py
+++ tests/exred/pages/fas_ui_landing.py
@@ -1,29 +1,37 @@
 # -*- coding: utf-8 -*-
-"""Find a Supplier Home Page Object."""
+"""Find a Supplier Landing Page Object."""
 import logging
 from urllib.parse import urljoin
 
 from selenium import webdriver
 
-from pages.common_actions import check_title, check_url, go_to_url, \
-    check_for_expected_sections_elements
+from pages.common_actions import (
+    check_for_expected_sections_elements,
+    check_for_section,
+    check_title,
+    check_url,
+    go_to_url,
+)
 from settings import DIRECTORY_UI_SUPPLIER_URL
-from utils import take_screenshot
+from utils import take_screenshot, find_element
 
 NAME = "Find a Supplier Home page"
 URL = urljoin(DIRECTORY_UI_SUPPLIER_URL, "")
 PAGE_TITLE = \
     "Find trade profiles of reliable UK suppliers - trade.great.gov.uk"
 
+SEARCH_INPUT = "#id_term"
+SEARCH_SECTOR = "#id_sectors"
+SEARCH_BUTTON = "#search-area > form button"
 SECTIONS = {
     "hero": {
         "itself": "section#hero",
     },
     "find uk suppliers": {
         "itself": "#search-area",
-        "search term input": "#id_term",
-        "search selectors dropdown": "#id_sectors",
-        "find suppliers button": "#search-area > form button"
+        "search term input": SEARCH_INPUT,
+        "search selectors dropdown": SEARCH_SECTOR,
+        "find suppliers button": SEARCH_BUTTON
     },
     "contact us": {
         "itself": "#introduction-section",
@@ -57,3 +65,29 @@ def should_be_here(driver: webdriver):
     check_title(driver, PAGE_TITLE, exact_match=True)
     check_for_expected_sections_elements(driver, SECTIONS)
     logging.debug("All expected elements are visible on '%s' page", NAME)
+
+
+def should_see_section(driver: webdriver, name: str):
+    check_for_section(driver, SECTIONS, sought_section=name)
+
+
+def search(driver: webdriver, *, keyword: str = None, sector: str = None):
+    input_field = find_element(
+        driver, by_css=SEARCH_INPUT, element_name="Search input field",
+        wait_for_it=False)
+    input_field.clear()
+    if keyword:
+        input_field.send_keys(keyword)
+    if sector:
+        sector_dropdown = find_element(
+            driver, by_css=SEARCH_SECTOR, element_name="Sector dropdown menu",
+            wait_for_it=False)
+        sector_value = "option[value='{}']".format(sector.upper().replace(" ", "_"))
+        sector_option = sector_dropdown.find_element_by_css_selector(sector_value)
+        sector_option.click()
+    take_screenshot(driver, NAME + " after entering the keyword")
+    button = find_element(
+        driver, by_css=SEARCH_BUTTON, element_name="Search button",
+        wait_for_it=False)
+    button.click()
+    take_screenshot(driver, NAME + " after submitting the search form")
