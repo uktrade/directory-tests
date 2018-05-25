64aa8b5f0d26d89aff868b1b9342150f0ef54591
kowalcj0 <kowalczykjanusz@gmail.com>
2018-05-25 09:39:37 +0100

    PO: add actions to search & go to contact us

--- tests/exred/pages/fas_ui_landing.py
+++ tests/exred/pages/fas_ui_landing.py
@@ -11,6 +11,7 @@ from pages.common_actions import (
     check_title,
     check_url,
     go_to_url,
+    find_and_click_on_page_element
 )
 from settings import DIRECTORY_UI_SUPPLIER_URL
 from utils import take_screenshot, find_element
@@ -23,6 +24,7 @@ PAGE_TITLE = \
 SEARCH_INPUT = "#id_term"
 SEARCH_SECTOR = "#id_sectors"
 SEARCH_BUTTON = "#search-area > form button"
+CONTACT_US_BUTTON = "#introduction-section a"
 SECTIONS = {
     "hero": {
         "itself": "section#hero",
@@ -36,7 +38,7 @@ SECTIONS = {
     "contact us": {
         "itself": "#introduction-section",
         "introduction text": "#introduction-section p",
-        "contact us button": "#introduction-section a"
+        "contact us": CONTACT_US_BUTTON
     },
     "uk industries": {
         "itself": "#industries-section",
@@ -91,3 +93,16 @@ def search(driver: webdriver, *, keyword: str = None, sector: str = None):
         wait_for_it=False)
     button.click()
     take_screenshot(driver, NAME + " after submitting the search form")
+
+
+def click_on_page_element(driver: webdriver, element_name: str):
+    find_and_click_on_page_element(driver, SECTIONS, element_name)
+    take_screenshot(driver, NAME + " after clicking on " + element_name)
+
+
+def open_industry(driver: webdriver, industry_name: str):
+    industry_link = find_element(
+        driver, by_partial_link_text=industry_name,
+        element_name="Industry card", wait_for_it=False)
+    industry_link.click()
+    take_screenshot(driver, NAME + " after opening " + industry_name + " page")
