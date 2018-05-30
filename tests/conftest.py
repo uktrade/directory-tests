68776b4c607d3d3a8f1f76b02b42951804028f96
kowalcj0 <kowalczykjanusz@gmail.com>
2018-05-30 16:35:51 +0100

    add CMS client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 import requests
+from directory_api_client.testapiclient import DirectoryTestAPIClient
 
 from tests import get_absolute_url, users
+from tests.settings import CMS_SIGNATURE_SECRET_API_KEY
 
 
 @pytest.fixture
@@ -14,3 +16,10 @@ def logged_in_session():
     )
     assert 'Sign out' in str(response.content)
     return session
+
+
+@pytest.fixture
+def cms_client():
+    base_url = get_absolute_url('cms-healthcheck:landing')
+    return DirectoryTestAPIClient(
+        base_url=base_url, api_key=CMS_SIGNATURE_SECRET_API_KEY)
