b395247ed1625d690b6320de91572a6c0e803433
kowalcj0 <kowalczykjanusz@gmail.com>
2018-06-04 14:45:04 +0100

    PO: add option to check if expected filters are checked

--- tests/exred/pages/fas_ui_search_results.py
+++ tests/exred/pages/fas_ui_search_results.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 """Find a Supplier Search Results Page Object."""
 import logging
+from typing import List
 from urllib.parse import urljoin
 
 from selenium import webdriver
+from selenium.webdriver.remote import webelement
+from utils import assertion_msg, find_elements, take_screenshot
 
 from pages.common_actions import (
     check_for_expected_sections_elements,
     check_for_section,
     check_title,
-    check_url,
+    check_url
 )
 from settings import DIRECTORY_UI_SUPPLIER_URL
-from utils import take_screenshot
 
 NAME = "Find a Search results page"
 URL = urljoin(DIRECTORY_UI_SUPPLIER_URL, "search/")
 PAGE_TITLE = \
     "Search the database of UK suppliers' trade profiles - trade.great.gov.uk"
 
+SECTOR_FILTERS = "#id_sectors input"
 SECTIONS = {
     "filters": {
         "itself": "#ed-search-filters-container",
@@ -52,3 +55,28 @@ def should_be_here(driver: webdriver):
 
 def should_see_section(driver: webdriver, name: str):
     check_for_section(driver, SECTIONS, sought_section=name)
+
+
+def should_see_filtered_results(driver: webdriver, expected_filters: List[str]):
+
+    def to_filter_format(name):
+        return name.upper().replace(" ", "_").replace("-", "_")
+
+    formatted_expected_filters = list(map(to_filter_format, expected_filters))
+
+    sector_filters = find_elements(driver, by_css=SECTOR_FILTERS)
+    checked_sector_filters = [sector_filter.get_attribute("value")
+                              for sector_filter in sector_filters
+                              if sector_filter.get_attribute("checked")]
+
+    number_expected_filters = len(formatted_expected_filters)
+    number_checked_filters = len(checked_sector_filters)
+    with assertion_msg(
+            "Expected to see %d sector filter(s) to be checked but saw %d",
+            number_expected_filters, number_checked_filters):
+        assert number_checked_filters == number_expected_filters
+
+    diff = list(set(formatted_expected_filters) - set(checked_sector_filters))
+    with assertion_msg("Couldn't find '%s' among checked filters",
+                       diff):
+        assert not diff
