d00763dfe1e8d993f461fc6d6affe7d4df0694d6
kowalcj0 <kowalczykjanusz@gmail.com>
2018-06-14 15:37:01 +0100

    check if published translated pages return 200

--- tests/smoke/test_cms.py
+++ tests/smoke/test_cms.py
@@ -189,3 +189,56 @@ def test_all_published_pages_should_return_200(cms_client, page_type):
         len(non_200), len(results), page_type, pformat(formatted_non_200)
     )
     assert not non_200, error_msg
+
+
+@pytest.mark.parametrize(
+    "page_type",
+    [
+        "export_readiness.GetFinancePage",
+        "export_readiness.PrivacyAndCookiesPage",
+        "export_readiness.TermsAndConditionsPage",
+        "find_a_supplier.IndustryArticlePage",
+        "find_a_supplier.IndustryContactPage",
+        "find_a_supplier.IndustryLandingPage",
+        "find_a_supplier.IndustryPage",
+        "find_a_supplier.LandingPage",
+        "invest.InfoPage",
+        "invest.InvestHomePage",
+        "invest.RegionLandingPage",
+        "invest.SectorLandingPage",
+        "invest.SectorPage",
+        "invest.SetupGuideLandingPage",
+        "invest.SetupGuidePage",
+    ],
+)
+def test_published_translated_pages_should_return_200(cms_client, page_type):
+    results = []
+    page_ids = get_page_ids_by_type(cms_client, page_type)
+    for page_id in page_ids:
+        url = "{}{}/".format(get_relative_url("cms-api:pages"), page_id)
+        try:
+            api_response = cms_client.get(url)
+        except Exception as ex:
+            results.append((page_id, url, str(ex)))
+            continue
+        if api_response.status_code == 200:
+            page = api_response.json()
+            live_url = page["meta"]["url"]
+            lang_codes = [lang[0] for lang in page["meta"]["languages"]]
+            for code in lang_codes:
+                lang_url = "{}?lang={}".format(live_url, code)
+                try:
+                    live_response = requests.get(lang_url)
+                except Exception as ex:
+                    results.append((page_id, lang_url, str(ex)))
+                    continue
+                results.append((page_id, lang_url, live_response.status_code))
+        else:
+            print("{} returned {}".format(url, api_response.status_code))
+    non_200 = [result for result in results if result[2] != 200]
+    template = "Page ID: {} URL: {} Status Code: {}"
+    formatted_non_200 = [template.format(*result) for result in non_200]
+    error_msg = "{} out of {} published pages of type {} are broken {}".format(
+        len(non_200), len(results), page_type, pformat(formatted_non_200)
+    )
+    assert not non_200, error_msg
