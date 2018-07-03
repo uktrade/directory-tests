81351bab6fe9077ed1026a5b1c9506709e6fc0ce
kowalcj0 <kowalczykjanusz@gmail.com>
2018-07-03 16:50:54 +0100

    PO: Invest contact us page

---
+++ tests/browser/pages/invest_contact_us.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Invest in Great - Contact us Page Object."""
+import logging
+from urllib.parse import urljoin
+
+from utils import take_screenshot
+
+from pages import Executor, visit_url
+from pages.common_actions import check_title, check_url
+from settings import INVEST_UI_URL
+
+URL = urljoin(INVEST_UI_URL, "contact/")
+PAGE_TITLE = "Invest in Great Britain - "
+
+
+def visit(executor: Executor, *, first_time: bool = False):
+    visit_url(executor, URL)
+
+
+def should_be_here(executor: Executor):
+    check_title(executor, PAGE_TITLE, exact_match=False)
+    check_url(executor, URL, exact_match=False)
+    take_screenshot(executor, PAGE_TITLE)
+    logging.debug("All expected elements are visible on '%s' page", PAGE_TITLE)
