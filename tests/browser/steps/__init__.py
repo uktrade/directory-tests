c565ec28ee1d88a9af5b6d9126480a9fbe8cbfa1
kowalcj0 <kowalczykjanusz@gmail.com>
2018-07-23 11:40:50 +0100

    fix the var name

--- tests/browser/steps/__init__.py
+++ tests/browser/steps/__init__.py
@@ -3,6 +3,6 @@ from types import ModuleType
 
 def has_action(page: ModuleType, name: str):
     assert hasattr(page, name), (
-        f"{page.SERVICE} - {page.NAME} has no 'should_see_sections' action"
+        f"{page.SERVICE} - {page.NAME} has no '{name}' action"
     )
 
