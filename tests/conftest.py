5593c99a0cb1fe5dc77a79c2c6eb60d3f086cef0
kowalcj0 <kowalczykjanusz@gmail.com>
2018-07-25 15:56:39 +0100

    change env variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,10 @@ import requests
 from directory_api_client.testapiclient import DirectoryTestAPIClient
 
 from tests import get_absolute_url, users
-from tests.settings import CMS_SIGNATURE_SECRET_API_KEY
+from tests.settings import (
+    CMS_SIGNATURE_SECRET_API_KEY,
+    SSO_API_SIGNATURE_SECRET,
+)
 
 
 @pytest.fixture
