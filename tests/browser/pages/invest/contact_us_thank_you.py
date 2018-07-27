d1b2bfd240cbb61abed856d786b82a9d97050599
kowalcj0 <kowalczykjanusz@gmail.com>
2018-07-27 13:34:26 +0100

    PO: thank you for your message

---
+++ tests/browser/pages/invest/contact_us_thank_you.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Invest in Great - Thank you for your message Page Object."""
+import logging
+from urllib.parse import urljoin
+
+from pages.common_actions import (
+    Executor,
+    check_title,
+    check_url,
+    take_screenshot,
+    visit_url,
+)
+from settings import INVEST_UI_URL
+
+NAME = "Thank you for your message"
+SERVICE = "invest"
+TYPE = "contact"
+URL = urljoin(INVEST_UI_URL, "contact/")
+PAGE_TITLE = "Invest in Great Britain -"
+SELECTORS = {}
+
+
+def visit(executor: Executor, *, first_time: bool = False):
+    visit_url(executor, URL)
+
+
+def should_be_here(executor: Executor):
+    check_title(executor, PAGE_TITLE, exact_match=False)
+    check_url(executor, URL, exact_match=False)
+    take_screenshot(executor, PAGE_TITLE)
+    logging.debug("All expected elements are visible on '%s' page", PAGE_TITLE)
