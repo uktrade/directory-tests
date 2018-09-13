0f9c7c2831801713e780471fe149f85b030ef758
kowalcj0 <kowalczykjanusz@gmail.com>
2018-09-13 14:05:59 +0100

    ensure that password always contains digits & letters

--- tests/browser/pages/common_actions.py
+++ tests/browser/pages/common_actions.py
@@ -224,11 +224,9 @@ def unauthenticated_actor(alias: str, *, self_classification: str = None) -> Act
         .replace(" ", "")
         .lower()
     )
-    password_length = 20
-    password = "".join(
-        random.choice(string.ascii_letters + string.digits)
-        for _ in range(password_length)
-    )
+    letters = "".join(random.choice(string.ascii_letters) for _ in range(10))
+    digits = "".join(random.choice(string.ascii_letters) for _ in range(10))
+    password = f"{letters}{digits}"
     return Actor(
         alias=alias,
         email=email,
