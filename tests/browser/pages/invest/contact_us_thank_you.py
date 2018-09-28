0b54c1290915fcc7c9a020f6be7847ec04a878a4
kowalcj0 <kowalczykjanusz@gmail.com>
2018-09-28 14:12:02 +0100

    PO: ensure that user is on the thank you page

--- tests/browser/pages/invest/contact_us_thank_you.py
+++ tests/browser/pages/invest/contact_us_thank_you.py
@@ -3,10 +3,14 @@
 import logging
 from urllib.parse import urljoin
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
 from pages.common_actions import (
-    Executor,
+    check_for_expected_sections_elements,
     check_title,
     check_url,
+    Selector,
     take_screenshot,
     visit_url,
 )
@@ -17,15 +21,23 @@ SERVICE = "invest"
 TYPE = "contact"
 URL = urljoin(INVEST_UI_URL, "contact/success/")
 PAGE_TITLE = ""
-SELECTORS = {}
+SELECTORS = {
+    "hero": {
+        "itself": Selector(By.CSS_SELECTOR, "section.hero"),
+    },
+    "success message": {
+        "itself": Selector(By.CSS_SELECTOR, "section.contact-success")
+    }
+}
 
 
-def visit(executor: Executor, *, first_time: bool = False):
-    visit_url(executor, URL)
+def visit(driver: WebDriver, *, first_time: bool = False):
+    visit_url(driver, URL)
 
 
-def should_be_here(executor: Executor):
-    check_title(executor, PAGE_TITLE, exact_match=False)
-    check_url(executor, URL, exact_match=False)
-    take_screenshot(executor, PAGE_TITLE)
+def should_be_here(driver: WebDriver):
+    check_title(driver, PAGE_TITLE, exact_match=False)
+    check_url(driver, URL, exact_match=True)
+    take_screenshot(driver, PAGE_TITLE)
+    check_for_expected_sections_elements(driver, SELECTORS)
     logging.debug("All expected elements are visible on '%s' page", PAGE_TITLE)
