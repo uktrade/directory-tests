751865331b0c91fd43303951ced63f3b28f11d2a
kowalcj0 <kowalczykjanusz@gmail.com>
2018-10-05 16:11:01 +0100

    fix CMS endpoint selector

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,8 +16,8 @@ join_exred = partial(urljoin, settings.EXRED_UI_URL)
 join_contact_us = partial(urljoin, settings.DIRECTORY_CONTACT_US_UI_URL)
 join_soo = partial(urljoin, settings.SOO_UI_URL)
 join_cms_url = partial(urljoin, settings.DIRECTORY_CMS_API_CLIENT_BASE_URL)
-join_cms_api = partial(urljoin, 'api', settings.DIRECTORY_CMS_API_CLIENT_BASE_URL)
-join_cms_ui = partial(urljoin, 'admin', settings.DIRECTORY_CMS_API_CLIENT_BASE_URL)
+join_cms_api = partial(urljoin, settings.DIRECTORY_CMS_API_CLIENT_BASE_URL)
+join_cms_ui = partial(urljoin, settings.DIRECTORY_CMS_API_CLIENT_BASE_URL)
 
 urls = {
     # SSO
