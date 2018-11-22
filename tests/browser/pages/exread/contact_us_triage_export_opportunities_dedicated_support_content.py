9e6e2667c85b8ba82943c04b1f5dcc9552edd6f5
kowalcj0 <kowalczykjanusz@gmail.com>
2018-11-22 13:45:47 +0000

    PO: dedicated support content for ExOpps & great.gov.uk

---
+++ tests/browser/pages/exread/contact_us_triage_export_opportunities_dedicated_support_content.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""Export Readiness - Domestic Contact us - Great.gov.uk account"""
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages import ElementType
+from pages.common_actions import Selector, check_url, go_to_url, take_screenshot
+from settings import EXRED_UI_URL
+
+NAME = "Export opportunities service"
+NAMES = ["Export opportunities service", "My daily alerts are not relevant to me"]
+SERVICE = "Export Readiness"
+TYPE = "Dedicated Support Content"
+URL = urljoin(EXRED_UI_URL, "contact/triage/export-opportunities/")
+PAGE_TITLE = "Welcome to great.gov.uk"
+
+URLs = {
+    "export opportunities service": URL,
+    "my daily alerts are not relevant to me": urljoin(URL, "alerts-not-relevant/"),
+}
+
+SUBMIT_BUTTON = Selector(
+    By.CSS_SELECTOR, "form button[type=submit]", type=ElementType.BUTTON
+)
+SELECTORS = {
+    "breadcrumbs": {
+        "itself": Selector(By.CSS_SELECTOR, "div.breadcrumbs"),
+        "links": Selector(By.CSS_SELECTOR, "div.breadcrumbs a"),
+    },
+    "support content": {
+        "itself": Selector(By.CSS_SELECTOR, "div.container section"),
+        "heading": Selector(By.CSS_SELECTOR, "section h1"),
+        "content": Selector(By.CSS_SELECTOR, "section p"),
+        "submit an enquiry": Selector(
+            By.CSS_SELECTOR, "#further-help-link > a", type=ElementType.LINK
+        ),
+    },
+    "error reporting": {
+        "itself": Selector(By.CSS_SELECTOR, "section.error-reporting"),
+        "link": Selector(By.ID, "error-reporting-section-contact-us"),
+    },
+}
+
+
+def visit(driver: WebDriver, *, first_time: bool = False):
+    go_to_url(driver, URL, NAME, first_time=first_time)
+
+
+def should_be_here(driver: WebDriver, *, page_name: str = None):
+    take_screenshot(driver, NAME)
+    url = URLs[page_name.lower()] if page_name else URL
+    check_url(driver, url, exact_match=False)
---
+++ tests/browser/pages/exread/contact_us_triage_great_account_dedicated_support_content.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""Export Readiness - Domestic Contact us - Great.gov.uk account"""
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages import ElementType
+from pages.common_actions import Selector, check_url, go_to_url, take_screenshot
+from settings import EXRED_UI_URL
+
+NAME = "Great.gov.uk account"
+NAMES = [
+    "I have not received an email confirmation",
+    "I need to reset my password",
+    "My Companies House login is not working",
+    "I do not know where to enter my verification code",
+    "I have not received my letter containing the verification code",
+]
+SERVICE = "Export Readiness"
+TYPE = "Dedicated Support Content"
+URL = urljoin(EXRED_UI_URL, "contact/triage/great-account/")
+PAGE_TITLE = "Welcome to great.gov.uk"
+
+URLs = {
+    "great.gov.uk account": URL,
+    "i have not received an email confirmation": urljoin(URL, "no-verification-email/"),
+    "i need to reset my password": urljoin(URL, "password-reset/"),
+    "my companies house login is not working": urljoin(URL, "companies-house-login/"),
+    "i do not know where to enter my verification code": urljoin(
+        URL, "verification-letter-code/"
+    ),
+    "i have not received my letter containing the verification code": urljoin(
+        URL, "no-verification-letter/"
+    ),
+}
+
+SUBMIT_BUTTON = Selector(
+    By.CSS_SELECTOR, "form button[type=submit]", type=ElementType.BUTTON
+)
+SELECTORS = {
+    "breadcrumbs": {
+        "itself": Selector(By.CSS_SELECTOR, "div.breadcrumbs"),
+        "links": Selector(By.CSS_SELECTOR, "div.breadcrumbs a"),
+    },
+    "support content": {
+        "itself": Selector(By.CSS_SELECTOR, "div.container section"),
+        "heading": Selector(By.CSS_SELECTOR, "section h1"),
+        "content": Selector(By.CSS_SELECTOR, "section p"),
+        "submit an enquiry": Selector(
+            By.CSS_SELECTOR, "#further-help-link > a", type=ElementType.LINK
+        ),
+    },
+    "error reporting": {
+        "itself": Selector(By.CSS_SELECTOR, "section.error-reporting"),
+        "link": Selector(By.ID, "error-reporting-section-contact-us"),
+    },
+}
+
+
+def visit(driver: WebDriver, *, first_time: bool = False):
+    go_to_url(driver, URL, NAME, first_time=first_time)
+
+
+def should_be_here(driver: WebDriver, *, page_name: str = None):
+    take_screenshot(driver, NAME)
+    url = URLs[page_name.lower()] if page_name else URL
+    check_url(driver, url, exact_match=False)
