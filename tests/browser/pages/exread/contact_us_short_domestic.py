38641711cea299cda7f56401bd0f2975f01c744d
kowalcj0 <kowalczykjanusz@gmail.com>
2018-11-22 16:34:39 +0000

    PO: handle situation when other type of uk organisation is selected
    and add actions to fill out and submit the form

--- tests/browser/pages/exread/contact_us_short_domestic.py
+++ tests/browser/pages/exread/contact_us_short_domestic.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-"""Export Readiness - First page of Long Contact us form"""
+"""Export Readiness - Sort Domestic Contact us form"""
+import logging
+import random
 from types import ModuleType
-
 from urllib.parse import urljoin
 
 from selenium.webdriver.common.by import By
@@ -9,11 +10,17 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 from pages import ElementType
 from pages.common_actions import (
+    Actor,
     Selector,
+    check_radio,
     check_url,
+    fill_out_input_fields,
+    fill_out_textarea_fields,
     find_element,
     go_to_url,
     take_screenshot,
+    tick_captcha_checkbox,
+    tick_checkboxes,
 )
 from pages.exread import contact_us_short_domestic_thank_you
 from settings import EXRED_UI_URL
@@ -31,7 +38,9 @@ TYPE = "Contact us"
 URL = urljoin(EXRED_UI_URL, "contact/domestic/")
 PAGE_TITLE = "Welcome to great.gov.uk"
 
-SUBMIT_BUTTON = Selector(By.CSS_SELECTOR, "form button[type=submit]", type=ElementType.BUTTON)
+SUBMIT_BUTTON = Selector(
+    By.CSS_SELECTOR, "form button[type=submit]", type=ElementType.BUTTON
+)
 SELECTORS = {
     "form": {
         "itself": Selector(By.CSS_SELECTOR, "#lede form"),
@@ -39,26 +48,48 @@ SELECTORS = {
         "first name": Selector(By.ID, "id_given_name", type=ElementType.INPUT),
         "last name": Selector(By.ID, "id_family_name", type=ElementType.INPUT),
         "email": Selector(By.ID, "id_email", type=ElementType.INPUT),
-        "uk private or public limited company": Selector(By.ID, "id_company_type_0", type=ElementType.CHECKBOX),
-        "other type of uk organisation": Selector(By.ID, "id_company_type_1", type=ElementType.CHECKBOX),
-        "organisation name": Selector(By.ID, "id_organisation_name", type=ElementType.INPUT),
+        "uk private or public limited company": Selector(
+            By.ID, "id_company_type_0", type=ElementType.RADIO
+        ),
+        "other type of uk organisation": Selector(
+            By.ID, "id_company_type_1", type=ElementType.RADIO
+        ),
+        "organisation name": Selector(
+            By.ID, "id_organisation_name", type=ElementType.INPUT
+        ),
         "postcode": Selector(By.ID, "id_postcode", type=ElementType.INPUT),
-        "terms and conditions": Selector(By.ID, "id_terms_agreed", type=ElementType.CHECKBOX),
+        "terms and conditions": Selector(
+            By.ID, "id_terms_agreed", type=ElementType.CHECKBOX
+        ),
         "submit": SUBMIT_BUTTON,
     }
 }
+OTHER_SELECTORS = {
+    "charity": Selector(By.ID, "id_company_type_other_0", type=ElementType.RADIO),
+    "government department": Selector(
+        By.ID, "id_company_type_other_1", type=ElementType.RADIO
+    ),
+    "intermediary": Selector(By.ID, "id_company_type_other_2", type=ElementType.RADIO),
+    "limited partnership": Selector(
+        By.ID, "id_company_type_other_3", type=ElementType.RADIO
+    ),
+    "sole trader": Selector(By.ID, "id_company_type_other_4", type=ElementType.RADIO),
+    "uk branch of foreign company (BR)": Selector(
+        By.ID, "id_company_type_other_5", type=ElementType.RADIO
+    ),
+    "other": Selector(By.ID, "id_company_type_other_6", type=ElementType.RADIO),
+}
 
 URLs = {
-    "short contact form (tell us how we can help)":
-        URL,
-    "short contact form (events)":
-        urljoin(URL, "/contact/events/"),
-    "short contact form (defence and security organisation (dso))":
-        urljoin(URL, "/contact/defence-and-security-organisation/"),
-    "short contact form (other)":
-        URL,
-    "short contact form (buying from the uk)":
-        urljoin(URL, "/contact/find-uk-companies/"),
+    "short contact form (tell us how we can help)": URL,
+    "short contact form (events)": urljoin(URL, "/contact/events/"),
+    "short contact form (defence and security organisation (dso))": urljoin(
+        URL, "/contact/defence-and-security-organisation/"
+    ),
+    "short contact form (other)": URL,
+    "short contact form (buying from the uk)": urljoin(
+        URL, "/contact/find-uk-companies/"
+    ),
 }
 
 
@@ -70,6 +101,58 @@ def should_be_here(driver: WebDriver, *, page_name: str = None):
     take_screenshot(driver, NAME)
     url = URLs[page_name.lower()] if page_name else URL
     check_url(driver, url, exact_match=True)
+
+
+def generate_form_details(actor: Actor) -> dict:
+    is_company = random.choice([True, False])
+    result = {
+        "comment": f"Submitted by automated test {actor.alias}",
+        "first name": f"send by {actor.alias} - automated tests",
+        "last name": actor.alias,
+        "email": actor.email,
+        "uk private or public limited company": is_company,
+        "other type of uk organisation": not is_company,
+        "organisation name": "automated tests",
+        "postcode": "SW1H 0TL",
+        "terms and conditions": True,
+    }
+    if not is_company:
+        other_type_of_uk_organisation = [
+            "charity",
+            "government department",
+            "intermediary",
+            "limited partnership",
+            "sole trader",
+            "uk branch of foreign company (BR)",
+            "other",
+        ]
+        other_choices = len(other_type_of_uk_organisation) * [False]
+        other_choices[random.randint(0, len(other_choices) - 1)] = True
+        other = {
+            other_type_of_uk_organisation[i]: other_choices[i]
+            for i in range(len(other_type_of_uk_organisation))
+        }
+        result.update(other)
+        SELECTORS["form"].update(OTHER_SELECTORS)
+    else:
+        # In order to avoid situation when previous scenario example modified
+        # SELECTORS we have to remove OTHER_SELECTORS that were then added
+        SELECTORS["form"] = dict(
+            set(SELECTORS["form"].items()) - set(OTHER_SELECTORS.items())
+        )
+
+    logging.debug(f"Generated form details: {result}")
+    return result
+
+
+def fill_out(driver: WebDriver, details: dict):
+    form_selectors = SELECTORS["form"]
+    fill_out_textarea_fields(driver, form_selectors, details)
+    fill_out_input_fields(driver, form_selectors, details)
+    check_radio(driver, form_selectors, details)
+    tick_checkboxes(driver, form_selectors, details)
+    tick_captcha_checkbox(driver)
+    take_screenshot(driver, "After filling out the form")
 
 
 def submit(driver: WebDriver) -> ModuleType:
