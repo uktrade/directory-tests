5dbee8db4daaea71a135958e40168cfe5e87f98c
kowalcj0 <kowalczykjanusz@gmail.com>
2018-11-23 18:28:56 +0000

    print module path instead of less helpfull names

--- tests/browser/steps/__init__.py
+++ tests/browser/steps/__init__.py
@@ -2,7 +2,4 @@ from types import ModuleType
 
 
 def has_action(page: ModuleType, name: str):
-    assert hasattr(page, name), (
-        f"{page.SERVICE} - {page.NAME} has no '{name}' action"
-    )
-
+    assert hasattr(page, name), f"{page.__name__} has no '{name}' action"
