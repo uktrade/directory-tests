178512bc346a1b914949edd8866febd5f9873fcd
kowalcj0 <kowalczykjanusz@gmail.com>
2018-11-23 21:48:48 +0000

    PO: add form actions

--- tests/browser/pages/exread/contact_us_long_export_advice_comment.py
+++ tests/browser/pages/exread/contact_us_long_export_advice_comment.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """Export Readiness - First page of Long Contact us form"""
 from types import ModuleType
-
 from urllib.parse import urljoin
 
 from selenium.webdriver.common.by import By
@@ -9,13 +8,15 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 from pages import ElementType
 from pages.common_actions import (
+    Actor,
     Selector,
     check_url,
+    fill_out_textarea_fields,
     find_element,
     go_to_url,
     take_screenshot,
 )
-from pages.exread import contact_us_triage_domestic
+from pages.exread import contact_us_long_export_advice_personal
 from settings import EXRED_UI_URL
 
 NAME = "Long (Export Advice Comment)"
@@ -24,7 +25,9 @@ TYPE = "Contact us"
 URL = urljoin(EXRED_UI_URL, "contact/export-advice/comment/")
 PAGE_TITLE = "Welcome to great.gov.uk"
 
-SUBMIT_BUTTON = Selector(By.CSS_SELECTOR, "form button[type=submit]", type=ElementType.BUTTON)
+SUBMIT_BUTTON = Selector(
+    By.CSS_SELECTOR, "form button[type=submit]", type=ElementType.BUTTON
+)
 SELECTORS = {
     "form": {
         "itself": Selector(By.CSS_SELECTOR, "#lede form"),
@@ -43,6 +46,17 @@ def should_be_here(driver: WebDriver):
     check_url(driver, URL)
 
 
+def generate_form_details(actor: Actor) -> dict:
+    result = {"comment": f"Submitted by automated test {actor.alias}"}
+    return result
+
+
+def fill_out(driver: WebDriver, details: dict):
+    form_selectors = SELECTORS["form"]
+    fill_out_textarea_fields(driver, form_selectors, details)
+    take_screenshot(driver, "After filling out the form")
+
+
 def submit(driver: WebDriver) -> ModuleType:
     take_screenshot(driver, "Before submitting the form")
     button = find_element(
@@ -50,4 +64,4 @@ def submit(driver: WebDriver) -> ModuleType:
     )
     button.click()
     take_screenshot(driver, "After submitting the form")
-    return contact_us_triage_domestic
+    return contact_us_long_export_advice_personal
