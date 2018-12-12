b3e71979b373d017b7d03a885e7bfa6751a5b96e
kowalcj0 <kowalczykjanusz@gmail.com>
2018-12-12 19:46:59 +0000

    provide hawk cookie since ip restrictor is now on on DEV

--- tests/smoke/test_supplier_redirects.py
+++ tests/smoke/test_supplier_redirects.py
@@ -10,8 +10,11 @@ from tests import join_ui_supplier
     ('/industries/technology/', join_ui_supplier('industries/tech/')),
     ('/industries/creative-services/', join_ui_supplier('industries/creative/')),
 ])
-def test_ed_4152_redirect_on_dev_from_old_industry_page(new_url, old_url):
-    response = requests.get(old_url, allow_redirects=False)
+def test_ed_4152_redirect_on_dev_from_old_industry_page(
+        new_url, old_url, fas_hawk_cookie):
+    response = requests.get(
+        old_url, allow_redirects=False, cookies=fas_hawk_cookie
+    )
 
     error_msg = (f"Expected request to '{old_url}' to be redirected to "
                  f"'{new_url}' but instead it was redirected to "
@@ -25,8 +28,11 @@ def test_ed_4152_redirect_on_dev_from_old_industry_page(new_url, old_url):
     ('/trade/industries/technology/', join_ui_supplier('industries/tech/')),
     ('/trade/industries/creative-services/', join_ui_supplier('industries/creative/')),
 ])
-def test_ed_4152_redirect_on_stage_from_old_industry_page(new_url, old_url):
-    response = requests.get(old_url, allow_redirects=False)
+def test_ed_4152_redirect_on_stage_from_old_industry_page(
+        new_url, old_url, fas_hawk_cookie):
+    response = requests.get(
+        old_url, allow_redirects=False, cookies=fas_hawk_cookie
+    )
 
     error_msg = (f"Expected request to '{old_url}' to be redirected to "
                  f"'{new_url}' but instead it was redirected to "
