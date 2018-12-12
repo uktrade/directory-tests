0e2574d861c94cfcb3532d4b250ed3bf7a5732f9
kowalcj0 <kowalczykjanusz@gmail.com>
2018-12-12 15:52:52 +0000

    pytest fixtures to generate hawk cookie

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 
 from directory_constants.constants import cms as SERVICE_NAMES
+from mohawk import Sender
 from retrying import retry
 
 from tests import get_absolute_url, users
@@ -15,6 +16,23 @@ from tests.settings import (
     DIRECTORY_SSO_API_CLIENT_DEFAULT_TIMEOUT,
     DIRECTORY_SSO_API_CLIENT_SENDER_ID,
     DIRECTORY_CMS_API_CLIENT_CACHE_EXPIRE_SECONDS,
+    IP_RESTRICTOR_SKIP_CHECK_SENDER_ID,
+    IP_RESTRICTOR_SKIP_CHECK_SECRET_CMS,
+    IP_RESTRICTOR_SKIP_CHECK_SECRET_EXRED,
+    IP_RESTRICTOR_SKIP_CHECK_SECRET_FAB,
+    IP_RESTRICTOR_SKIP_CHECK_SECRET_FAS,
+    IP_RESTRICTOR_SKIP_CHECK_SECRET_FORMS,
+    IP_RESTRICTOR_SKIP_CHECK_SECRET_INVEST,
+    IP_RESTRICTOR_SKIP_CHECK_SECRET_SSO,
+    IP_RESTRICTOR_SKIP_CHECK_SECRET_SUD,
+    DIRECTORY_SSO_URL,
+    DIRECTORY_PROFILE_URL,
+    DIRECTORY_UI_BUYER_URL,
+    DIRECTORY_UI_SUPPLIER_URL,
+    EXRED_UI_URL,
+    INVEST_UI_URL,
+    OLD_DIRECTORY_UI_SUPPLIER_URL,
+    OLD_EXRED_UI_URL,
 )
 
 
@@ -52,6 +70,55 @@ def cms_client():
         timeout=DIRECTORY_CMS_API_CLIENT_DEFAULT_TIMEOUT,
         default_service_name="change-me",
     )
+
+
+def hawk_cookie(key):
+    sender = Sender(
+        credentials={
+            'id': IP_RESTRICTOR_SKIP_CHECK_SENDER_ID,
+            'key': key,
+            'algorithm': 'sha256'
+        },
+        url='/',
+        method='',
+        always_hash_content=False
+    )
+    return {"ip-restrict-signature": sender.request_header}
+
+
+@pytest.fixture
+def cms_hawk_cookie():
+    return hawk_cookie(IP_RESTRICTOR_SKIP_CHECK_SECRET_CMS)
+
+
+@pytest.fixture
+def fab_hawk_cookie():
+    return hawk_cookie(IP_RESTRICTOR_SKIP_CHECK_SECRET_FAB)
+
+
+@pytest.fixture
+def fas_hawk_cookie():
+    return hawk_cookie(IP_RESTRICTOR_SKIP_CHECK_SECRET_FAS)
+
+
+@pytest.fixture
+def exred_hawk_cookie():
+    return hawk_cookie(IP_RESTRICTOR_SKIP_CHECK_SECRET_EXRED)
+
+
+@pytest.fixture
+def invest_hawk_cookie():
+    return hawk_cookie(IP_RESTRICTOR_SKIP_CHECK_SECRET_INVEST)
+
+
+@pytest.fixture
+def sso_hawk_cookie():
+    return hawk_cookie(IP_RESTRICTOR_SKIP_CHECK_SECRET_SSO)
+
+
+@pytest.fixture
+def sud_hawk_cookie():
+    return hawk_cookie(IP_RESTRICTOR_SKIP_CHECK_SECRET_SUD)
 
 
 @pytest.fixture
