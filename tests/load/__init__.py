ba7f684963739a9bd6d5e37a9eb545cf9c5a0f5d
kowalcj0 <kowalczykjanusz@gmail.com>
2018-12-12 19:21:53 +0000

    add helper to generate hawk cookie for invest

--- tests/load/__init__.py
+++ tests/load/__init__.py
@@ -1,1 +1,26 @@
+import os
+from mohawk import Sender
+
+
 USER_AGENT = {"User-Agent": "locust - load tests"}
+
+IP_RESTRICTOR_SKIP_CHECK_SENDER_ID = os.environ["IP_RESTRICTOR_SKIP_CHECK_SENDER_ID"]
+IP_RESTRICTOR_SKIP_CHECK_SECRET_INVEST = os.environ["IP_RESTRICTOR_SKIP_CHECK_SECRET_INVEST"]
+
+
+def hawk_cookie(key):
+    sender = Sender(
+        credentials={
+            'id': IP_RESTRICTOR_SKIP_CHECK_SENDER_ID,
+            'key': key,
+            'algorithm': 'sha256'
+        },
+        url='/',
+        method='',
+        always_hash_content=False
+    )
+    return {"ip-restrict-signature": sender.request_header}
+
+
+def invest_hawk_cookie():
+    return hawk_cookie(IP_RESTRICTOR_SKIP_CHECK_SECRET_INVEST)
