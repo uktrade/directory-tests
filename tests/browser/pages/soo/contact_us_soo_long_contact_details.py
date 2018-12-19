cbd023db232553e5e9b7b5813c5490d6a1dbbd1c
kowalcj0 <kowalczykjanusz@gmail.com>
2018-12-19 14:14:11 +0000

    PO: TT-833 new SOO contact-us form

---
+++ tests/browser/pages/soo/contact_us_soo_long_contact_details.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""SOO - First page of Long SOO Contact us form"""
+import logging
+import random
+from types import ModuleType
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages import ElementType
+from pages.common_actions import (
+    Actor,
+    Selector,
+    check_radio,
+    check_url,
+    fill_out_input_fields,
+    find_element,
+    go_to_url,
+    pick_option,
+    take_screenshot,
+    tick_captcha_checkbox,
+    tick_checkboxes,
+)
+from pages.soo import contact_us_soo_long_thank_you
+from settings import SELLING_ONLINE_OVERSEAS_UI_URL
+
+NAME = "Long Domestic (Contact details)"
+SERVICE = "Selling Online Overseas"
+TYPE = "Contact us"
+URL = urljoin(SELLING_ONLINE_OVERSEAS_UI_URL, "contact/selling-online-overseas/contact-details/")
+PAGE_TITLE = "Welcome to great.gov.uk"
+
+SUBMIT_BUTTON = Selector(
+    By.CSS_SELECTOR, "form button[type=submit]", type=ElementType.BUTTON
+)
+SELECTORS = {
+    "form": {
+        "itself": Selector(By.CSS_SELECTOR, "#content form"),
+        "contact name": Selector(
+            By.ID, "id_contact-details-contact_name", type=ElementType.INPUT
+        ),
+        "email address": Selector(
+            By.ID, "id_contact-details-contact_email", type=ElementType.INPUT
+        ),
+        "telephone number": Selector(
+            By.ID, "id_contact-details-phone", type=ElementType.INPUT
+        ),
+        "i prefer to be contacted by email": Selector(
+            By.ID, "id_contact-details-email_pref", type=ElementType.CHECKBOX
+        ),
+        "t & c": Selector(
+            By.ID, "id_contact-details-terms_agreed", type=ElementType.CHECKBOX
+        ),
+    }
+}
+
+
+def visit(driver: WebDriver, *, first_time: bool = False):
+    go_to_url(driver, URL, NAME, first_time=first_time)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL)
+
+
+def generate_form_details(actor: Actor) -> dict:
+    prefer_email = random.choice([True, False])
+    result = {
+        "contact name": "automated tests",
+        "email address": actor.email,
+        "telephone number": "0700 100 2000",
+        "i prefer to be contacted by email": prefer_email,
+        "t & c": True,
+    }
+    logging.debug(f"Generated form details: {result}")
+    return result
+
+
+def fill_out(driver: WebDriver, details: dict):
+    form_selectors = SELECTORS["form"]
+    fill_out_input_fields(driver, form_selectors, details)
+    tick_checkboxes(driver, form_selectors, details)
+    tick_captcha_checkbox(driver)
+    take_screenshot(driver, "After filling out the form")
+
+
+def submit(driver: WebDriver) -> ModuleType:
+    take_screenshot(driver, "Before submitting the form")
+    button = find_element(
+        driver, SUBMIT_BUTTON, element_name="Submit button", wait_for_it=False
+    )
+    button.click()
+    take_screenshot(driver, "After submitting the form")
+    return contact_us_soo_long_thank_you
---
+++ tests/browser/pages/soo/contact_us_soo_long_your_experience.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""Export Readiness - First page of Long SOO Contact us form"""
+import logging
+import random
+from types import ModuleType
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages import ElementType
+from pages.common_actions import (
+    Actor,
+    Selector,
+    check_random_radio,
+    check_url,
+    fill_out_input_fields,
+    find_element,
+    go_to_url,
+    pick_option,
+    take_screenshot,
+    tick_captcha_checkbox,
+    tick_checkboxes,
+)
+from pages.soo import contact_us_soo_long_contact_details
+from settings import SELLING_ONLINE_OVERSEAS_UI_URL
+
+NAME = "Long Domestic (Your experience)"
+SERVICE = "Selling Online Overseas"
+TYPE = "Contact us"
+URL = urljoin(SELLING_ONLINE_OVERSEAS_UI_URL, "contact/selling-online-overseas/your-experience/")
+PAGE_TITLE = "Welcome to great.gov.uk"
+
+SUBMIT_BUTTON = Selector(
+    By.CSS_SELECTOR, "form button[type=submit]", type=ElementType.BUTTON
+)
+SELECTORS = {
+    "form": {
+        "itself": Selector(By.CSS_SELECTOR, "#content form"),
+        "not yet": Selector(
+            By.ID, "id_your-experience-experience_0", type=ElementType.RADIO
+        ),
+        "yes, sometimes": Selector(
+            By.ID, "id_your-experience-experience_1", type=ElementType.RADIO
+        ),
+        "yes, regularly": Selector(
+            By.ID, "id_your-experience-experience_2", type=ElementType.RADIO
+        ),
+        "description": Selector(
+            By.ID, "id_your-experience-description", type=ElementType.TEXTAREA
+        ),
+    }
+}
+
+
+def visit(driver: WebDriver, *, first_time: bool = False):
+    go_to_url(driver, URL, NAME, first_time=first_time)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL)
+
+
+def generate_form_details(actor: Actor) -> dict:
+    return {"description": "automated tests"}
+
+
+def fill_out(driver: WebDriver, details: dict):
+    form_selectors = SELECTORS["form"]
+    check_random_radio(driver, form_selectors)
+    fill_out_input_fields(driver, form_selectors, details)
+    take_screenshot(driver, "After filling out the form")
+
+
+def submit(driver: WebDriver) -> ModuleType:
+    take_screenshot(driver, "Before submitting the form")
+    button = find_element(
+        driver, SUBMIT_BUTTON, element_name="Submit button", wait_for_it=False
+    )
+    button.click()
+    take_screenshot(driver, "After submitting the form")
+    return contact_us_soo_long_contact_details
