91c81b70a01d1f9201cba02ff59f730b7689b23b
kowalcj0 <kowalczykjanusz@gmail.com>
2019-01-04 15:53:20 +0000

    step: remove irrelevant option
    as dedicated registation links is not present any longer on article page

--- tests/browser/steps/when_impl.py
+++ tests/browser/steps/when_impl.py
@@ -256,9 +256,7 @@ def registration_go_to(context: Context, actor_alias: str, location: str):
     logging.debug(
         "%s decided to go to registration via %s link", actor_alias, location
     )
-    if location.lower() == "article":
-        exread.article_common.go_to_registration(context.driver)
-    elif location.lower() == "article list":
+    if location.lower() == "article list":
         exread.article_list.go_to_registration(context.driver)
     elif location.lower() == "top bar":
         exread.header.go_to_registration(context.driver)
