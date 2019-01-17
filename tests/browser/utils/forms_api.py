fd1cabfa2e43c2cb5c88056be08ba0a144c745b3
kowalcj0 <kowalczykjanusz@gmail.com>
2019-01-17 10:28:02 +0000

    add simple test client for Forms API

---
+++ tests/browser/utils/forms_api.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from directory_client_core.base import AbstractAPIClient
+
+import settings
+
+
+class FormsClient(AbstractAPIClient):
+    version = "1"
+
+    def __init__(
+            self, base_url, api_key, sender_id, timeout, default_service_name,
+    ):
+        super().__init__(base_url, api_key, sender_id, timeout)
+        self.default_service_name = default_service_name
+
+
+client = FormsClient(
+    base_url=settings.FORMS_API_URL,
+    api_key=settings.FORMS_API_KEY,
+    sender_id=settings.FORMS_API_SENDER_ID,
+    timeout=30,
+    default_service_name="testapi"
+)
+
+
+def find_form_submissions(email: str) -> dict:
+    json = client.get(f"testapi/submissions-by-email/{email}/").json()
+    logging.debug(f"Submissions for {email}: {json}")
+    return json
