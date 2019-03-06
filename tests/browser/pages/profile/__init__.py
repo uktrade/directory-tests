da3baa70a14363822f0b8159d1cf1c99204e52ba
kowalcj0 <kowalczykjanusz@gmail.com>
2019-03-06 12:49:49 +0000

    update list of modules for easier importing

--- tests/browser/pages/profile/__init__.py
+++ tests/browser/pages/profile/__init__.py
@@ -1,4 +1,11 @@
 __all__ = [
+    "about",
+    "enrol_account_created",
     "enrol_create_account",
+    "enrol_enter_your_business_details",
+    "enrol_enter_your_business_details_step_2",
+    "enrol_enter_your_business_email_and_set_password",
+    "enrol_enter_your_confirmation_code",
+    "enrol_enter_your_details",
     "enrol_select_business_type",
 ]
