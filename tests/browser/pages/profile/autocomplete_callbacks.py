6fc7583d8e199531cf446fc6d6f3fe919bca397b
kowalcj0 <kowalczykjanusz@gmail.com>
2019-03-11 16:34:44 +0000

    util: autocomplete callback for enrolment company name

---
+++ tests/browser/pages/profile/autocomplete_callbacks.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Various form autocomplete callbacks"""
+import logging
+import random
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages.common_actions import Selector, find_element, find_elements
+
+
+def enrol_autocomplete_company_name(driver: WebDriver):
+    autocomplete = Selector(
+        By.CSS_SELECTOR, "ul.SelectiveLookupDisplay", is_visible=True
+    )
+    find_element(
+        driver, autocomplete, element_name="Autocomplete", wait_for_it=True
+    )
+    options = find_elements(
+        driver, Selector(By.CSS_SELECTOR, "li[role='option']")
+    )
+    option = random.choice(options)
+    logging.debug(
+        f"Selected company: {option.get_attribute('data-value')} {option.text}"
+    )
+    option.click()
