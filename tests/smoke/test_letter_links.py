95728822ce5e2ce0b7f3745390817d01f94f438a
kowalcj0 <kowalczykjanusz@gmail.com>
2019-03-22 14:48:09 +0000

    use common helpers & reformat with black

--- tests/smoke/test_letter_links.py
+++ tests/smoke/test_letter_links.py
@@ -7,106 +7,118 @@ from rest_framework.status import (
 )
 
 from tests import get_absolute_url
-from tests.smoke.cms_api_helpers import status_error
+from tests.smoke.cms_api_helpers import get_and_assert, status_error
 
 
 @pytest.mark.stage
 @pytest.mark.parametrize(
-    "absolute_url",
+    "url",
     [
         get_absolute_url("legacy-ui-contact-us:help"),
         get_absolute_url("legacy-ui-contact-us:feedback-form"),
     ],
 )
-def test_access_as_anon_user(absolute_url, basic_auth):
-    response = requests.get(absolute_url, allow_redirects=False)
+def test_access_as_anon_user(url, basic_auth):
+    response = requests.get(url, allow_redirects=False)
     redirect = response.headers["location"]
-    response = requests.get(redirect, allow_redirects=True, auth=basic_auth)
-    assert response.status_code == HTTP_200_OK, status_error(
-        HTTP_200_OK, response
+    get_and_assert(
+        url=redirect,
+        allow_redirects=True,
+        status_code=HTTP_200_OK,
+        auth=basic_auth,
     )
 
 
 @pytest.mark.stage
 @pytest.mark.parametrize(
-    "absolute_url",
+    "url",
     [
         get_absolute_url("legacy-ui-contact-us:help"),
         get_absolute_url("legacy-ui-contact-us:feedback-form"),
     ],
 )
 def test_access_contact_us_as_anon_user_after_removing_trailing_slash(
-    absolute_url, basic_auth
+    url, basic_auth
 ):
     # get rid of trailing slash
-    absolute_url = absolute_url[:-1]
-    response = requests.get(absolute_url, allow_redirects=False)
+    if url[-1] == "/":
+        url = url[:-1]
+    response = requests.get(url, allow_redirects=False)
     redirect = response.headers["location"]
-    response = requests.get(redirect, allow_redirects=True, auth=basic_auth)
-    assert response.status_code == HTTP_200_OK, status_error(
-        HTTP_200_OK, response
+    get_and_assert(
+        url=redirect,
+        allow_redirects=True,
+        status_code=HTTP_200_OK,
+        auth=basic_auth,
     )
 
 
 @pytest.mark.stage
 @pytest.mark.parametrize(
-    "absolute_url", [get_absolute_url("ui-buyer:confirm-identity")]
+    "url", [get_absolute_url("ui-buyer:confirm-identity")]
 )
-def test_302_redirects_for_anon_user(absolute_url, basic_auth):
-    response = requests.get(
-        absolute_url, allow_redirects=False, auth=basic_auth
+def test_302_redirects_for_anon_user(url, basic_auth):
+    get_and_assert(
+        url=url,
+        allow_redirects=False,
+        status_code=HTTP_302_FOUND,
+        auth=basic_auth,
     )
-    assert response.status_code == HTTP_302_FOUND
 
 
 @pytest.mark.parametrize(
-    "absolute_url", [get_absolute_url("ui-buyer:confirm-identity")]
+    "url", [get_absolute_url("ui-buyer:confirm-identity")]
 )
 def test_301_redirects_after_removing_trailing_slash_for_anon_user(
-    absolute_url, basic_auth
+    url, basic_auth
 ):
     # get rid of trailing slash
-    absolute_url = absolute_url[:-1]
-    response = requests.get(
-        absolute_url, allow_redirects=False, auth=basic_auth
+    if url[-1] == "/":
+        url = url[:-1]
+    response = get_and_assert(
+        url=url,
+        allow_redirects=False,
+        status_code=HTTP_301_MOVED_PERMANENTLY,
+        auth=basic_auth,
     )
-    assert response.status_code == HTTP_301_MOVED_PERMANENTLY
     assert response.headers["location"] == "/find-a-buyer/verify/"
 
 
 @pytest.mark.session_auth
 @pytest.mark.stage
 @pytest.mark.parametrize(
-    "absolute_url", [get_absolute_url("ui-buyer:confirm-identity")]
+    "url", [get_absolute_url("ui-buyer:confirm-identity")]
 )
 def test_access_endpoints_as_logged_in_user(
-    logged_in_session, absolute_url, basic_auth
+    logged_in_session, url, basic_auth
 ):
     response = logged_in_session.get(
-        absolute_url, allow_redirects=True, auth=basic_auth
+        url, allow_redirects=True, auth=basic_auth
     )
-    assert response.status_code == HTTP_200_OK
+    assert response.status_code == HTTP_200_OK, status_error(
+        HTTP_200_OK, response
+    )
 
 
 @pytest.mark.session_auth
 @pytest.mark.stage
 @pytest.mark.parametrize(
-    "absolute_url", [get_absolute_url("ui-buyer:confirm-identity")]
+    "url", [get_absolute_url("ui-buyer:confirm-identity")]
 )
 def test_check_if_verify_endpoint_redirects_to_correct_page(
-    logged_in_session, absolute_url, basic_auth
+    logged_in_session, url, basic_auth
 ):
     response = logged_in_session.get(
-        absolute_url, allow_redirects=True, auth=basic_auth
+        url, allow_redirects=True, auth=basic_auth
     )
-    assert response.status_code == HTTP_200_OK
+    assert response.status_code == HTTP_200_OK, status_error(
+        HTTP_200_OK, response
+    )
     # depends on the test account status/configuration user should either get
     # to the letter verification page or to their profile page if they already
     # went through verification
     got_to_letter_confirmation = response.url == get_absolute_url(
         "ui-buyer:confirm-company-address"
     )
-    got_to_profile = response.url == get_absolute_url(
-        "profile:fab"
-    )
+    got_to_profile = response.url == get_absolute_url("profile:fab")
     assert got_to_letter_confirmation or got_to_profile
