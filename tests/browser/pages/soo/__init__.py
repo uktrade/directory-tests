9440b55ba5e0477ced5bbad5b6ad43ef40be77c5
kowalcj0 <kowalczykjanusz@gmail.com>
2019-03-26 11:52:19 +0000

    fix module names for easier importing

--- tests/browser/pages/soo/__init__.py
+++ tests/browser/pages/soo/__init__.py
@@ -1,8 +1,6 @@
 __all__ = [
-    "contact_us_soo_long_contact_details",
-    "contact_us_soo_long_organisation_details",
-    "contact_us_soo_long_thank_you",
-    "contact_us_soo_long_your_business",
-    "contact_us_soo_long_your_experience",
+    "autocomplete_callbacks",
     "home",
+    "marketplace",
+    "search_results",
 ]
