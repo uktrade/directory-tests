bc7b771751426f77c9bed01c5eba1788a9e06bad
kowalcj0 <kowalczykjanusz@gmail.com>
2019-04-02 13:11:48 +0100

    PO: fix URL

--- tests/browser/pages/profile/edit_company_profile.py
+++ tests/browser/pages/profile/edit_company_profile.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-"""Find a Buyer - Edit Company Profile"""
+"""Profile - Edit Company Profile"""
 import logging
 from urllib.parse import urljoin
 
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from pages.common_actions import check_url, go_to_url, take_screenshot
-from settings import DIRECTORY_UI_BUYER_URL
+from settings import DIRECTORY_UI_PROFILE_URL
 
 NAME = "Edit Company Profile"
-SERVICE = "Find a Buyer"
+SERVICE = "Profile"
 TYPE = "profile"
-URL = urljoin(DIRECTORY_UI_BUYER_URL, "company-profile/")
+URL = urljoin(DIRECTORY_UI_PROFILE_URL, "find-a-buyer/")
 PAGE_TITLE = "Business profile - great.gov.uk"
 
 SELECTORS = {}
