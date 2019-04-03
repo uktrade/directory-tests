309744c52a72aa59c53ead99536b34a362626d97
kowalcj0 <kowalczykjanusz@gmail.com>
2019-04-03 10:50:15 +0100

    PO: add page for get EORI number

---
+++ tests/browser/pages/external/eori_home.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""EORI Home Page Object."""
+import logging
+
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages.common_actions import (
+    check_url,
+    go_to_url,
+    take_screenshot,
+)
+
+NAME = "Home"
+SERVICE = "EORI"
+TYPE = "home"
+URL = "https://www.gov.uk/eori"
+SELECTORS = {}
+
+
+def visit(driver: WebDriver):
+    go_to_url(driver, URL, NAME)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL)
+    logging.debug("All expected elements are visible on '%s' page", NAME)
