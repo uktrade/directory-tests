00301b9fd833a68da0927d960f38c9f24eb77be0
kowalcj0 <kowalczykjanusz@gmail.com>
2019-04-11 15:08:30 +0100

    PO: international industries page

---
+++ tests/functional/pages/international_industries.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""International Site - Industries page"""
+import logging
+
+from requests import Response, Session
+from tests import get_absolute_url
+from tests.functional.utils.request import Method, check_response, make_request
+
+URL = get_absolute_url("ui-international:industries")
+EXPECTED_STRINGS = [
+    "Industries",
+    "Great.gov.uk International"
+]
+
+
+def go_to(session: Session) -> Response:
+    return make_request(Method.GET, URL, session=session)
+
+
+def should_be_here(response: Response):
+    check_response(response, 200, body_contains=EXPECTED_STRINGS)
+    logging.debug("Successfully got to the International - Industries page")
