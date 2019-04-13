2b18483262fe11c718410f845be1b156c27253a6
kowalcj0 <kowalczykjanusz@gmail.com>
2019-04-13 12:04:36 +0100

    list public POs in every module

---
+++ tests/functional/pages/international/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    'international_industries',
+    'international_industry',
+    'international_landing',
+]
---
+++ tests/functional/pages/sso/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'sso_ui_change_password',
+    'sso_ui_confim_your_email',
+    'sso_ui_invalid_password_reset_link',
+    'sso_ui_login',
+    'sso_ui_logout',
+    'sso_ui_password_reset',
+    'sso_ui_register',
+    'sso_ui_verify_your_email',
+]
