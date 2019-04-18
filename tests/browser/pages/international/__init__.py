4415d447c40738d2a3a1d60261e01aa1631df958
kowalcj0 <kowalczykjanusz@gmail.com>
2019-04-18 19:42:55 +0100

    add PO listing for international pages

---
+++ tests/browser/pages/international/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+    "international",
+    "international_contact_us",
+    "international_eu_exit_contact_us",
+    "international_eu_exit_contact_us_thank_you",
+    "news_international",
+    "news_international_article",
+]
