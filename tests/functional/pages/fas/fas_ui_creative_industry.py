67f7a0ccee865883222552b749aee51374ae1fa6
kowalcj0 <kowalczykjanusz@gmail.com>
2019-04-25 15:27:47 +0100

    PO: check page URL when asserting current location

--- tests/functional/pages/fas/fas_ui_creative_industry.py
+++ tests/functional/pages/fas/fas_ui_creative_industry.py
@@ -6,7 +6,12 @@ from requests import Response, Session
 
 from tests import get_absolute_url
 from tests.functional.pages import Services
-from tests.functional.utils.request import Method, check_response, make_request
+from tests.functional.utils.request import (
+    Method,
+    check_response,
+    check_url,
+    make_request,
+)
 
 SERVICE = Services.FAS
 NAME = "Creative"
@@ -27,5 +32,6 @@ def go_to(session: Session) -> Response:
 
 
 def should_be_here(response: Response):
+    check_url(response, URL)
     check_response(response, 200, body_contains=EXPECTED_STRINGS)
     logging.debug("Supplier is on FAS Creative Industry page")
