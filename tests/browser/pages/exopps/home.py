8570d65fc6fe79d5eebb773e7591fec7cd601cce
kowalcj0 <kowalczykjanusz@gmail.com>
2019-06-05 16:40:41 +0100

    PO: add header & footer selectors to ExOpps home page

--- tests/browser/pages/exopps/home.py
+++ tests/browser/pages/exopps/home.py
@@ -5,7 +5,13 @@ from urllib.parse import urljoin
 
 from selenium.webdriver.remote.webdriver import WebDriver
 
-from pages.common_actions import check_url, go_to_url, take_screenshot
+from pages import common_selectors
+from pages.common_actions import (
+    check_url,
+    find_and_click_on_page_element,
+    go_to_url,
+    take_screenshot,
+)
 from settings import EXPORT_OPPORTUNITIES_UI_URL
 
 NAME = "Home"
@@ -14,6 +20,8 @@ TYPE = "home"
 URL = urljoin(EXPORT_OPPORTUNITIES_UI_URL, "")
 PAGE_TITLE = "Export opportunities"
 SELECTORS = {}
+SELECTORS.update(common_selectors.HEADER)
+SELECTORS.update(common_selectors.FOOTER)
 
 
 def visit(driver: WebDriver):
@@ -24,3 +32,8 @@ def should_be_here(driver: WebDriver):
     take_screenshot(driver, NAME)
     check_url(driver, URL, exact_match=True)
     logging.debug("All expected elements are visible on '%s' page", NAME)
+
+
+def click_on_page_element(driver: WebDriver, element_name: str):
+    find_and_click_on_page_element(driver, SELECTORS, element_name)
+    take_screenshot(driver, NAME + " after clicking on " + element_name)
