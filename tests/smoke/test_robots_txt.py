ca21177a5cdda25d5a48b2a846af4f1e7d04b9e5
kowalcj0 <kowalczykjanusz@gmail.com>
2019-06-12 16:30:10 +0100

    replace dict based URL search with Enum

--- tests/smoke/test_robots_txt.py
+++ tests/smoke/test_robots_txt.py
@@ -3,22 +3,22 @@ from urllib.parse import urljoin
 import pytest
 from rest_framework.status import HTTP_200_OK
 
-from tests import get_absolute_url
+from tests import URLs
 from tests.smoke.cms_api_helpers import get_and_assert
 
 
 @pytest.mark.parametrize(
     "url",
     [
-        # urljoin(get_absolute_url("profile:landing"), "robots.txt"),see TT-1499
-        urljoin(get_absolute_url("sso:landing"), "robots.txt"),
-        urljoin(get_absolute_url("ui-buyer:landing"), "robots.txt"),
-        urljoin(get_absolute_url("ui-exopps:landing"), "robots.txt"),
-        urljoin(get_absolute_url("ui-exred:landing"), "robots.txt"),
-        urljoin(get_absolute_url("ui-international:landing"), "robots.txt"),
-        urljoin(get_absolute_url("ui-invest:landing"), "robots.txt"),
-        urljoin(get_absolute_url("ui-soo:landing"), "robots.txt"),
-        urljoin(get_absolute_url("ui-supplier:landing"), "robots.txt"),
+        urljoin(URLs.DOMESTIC_LANDING.absolute, "robots.txt"),
+        urljoin(URLs.EXOPPS_LANDING.absolute, "robots.txt"),
+        urljoin(URLs.FAB_LANDING.absolute, "robots.txt"),
+        urljoin(URLs.FAS_LANDING.absolute, "robots.txt"),
+        urljoin(URLs.INTERNATIONAL_LANDING.absolute, "robots.txt"),
+        urljoin(URLs.INVEST_LANDING.absolute, "robots.txt"),
+        # urljoin(URLs.PROFILE_LANDING.absolute, "robots.txt"), see TT-1499
+        urljoin(URLs.SOO_LANDING.absolute, "robots.txt"),
+        urljoin(URLs.SSO_LANDING.absolute, "robots.txt"),
     ],
 )
 def test_robots_txt(url, basic_auth):
