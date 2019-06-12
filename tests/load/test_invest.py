73c2aef797e7e77f7cf14607f78a1c47b8eb1e07
kowalcj0 <kowalczykjanusz@gmail.com>
2019-06-12 18:45:06 +0100

    use URLs Enum in Invest load tests & remove tests
    which cause redirects to International site

--- tests/load/test_invest.py
+++ tests/load/test_invest.py
@@ -1,14 +1,15 @@
 from random import choice
 
 from locust import HttpLocust, TaskSet, task
-from tests import get_relative_url, settings
+from tests import settings, URLs
 from tests.load import USER_AGENT, basic_auth
 
 
 class InvestTasks(TaskSet):
+
     @task
     def home_page(self):
-        url = get_relative_url("ui-invest:landing")
+        url = URLs.INVEST_LANDING.relative
         self.client.get(
             url,
             headers=USER_AGENT,
@@ -16,59 +17,40 @@ class InvestTasks(TaskSet):
         )
 
     @task
-    def uk_setup_guides_pages(self):
-        endpoints = [
-            "uk-setup-guide/",
-            "uk-setup-guide/apply-uk-visa/",
-            "uk-setup-guide/establish-base-business-uk/",
-            "uk-setup-guide/hire-skilled-workers-your-uk-operations/",
-            "uk-setup-guide/open-uk-business-bank-account/",
-            "uk-setup-guide/setup-your-business-uk/",
-            "uk-setup-guide/understand-uk-tax-and-incentives/",
-        ]
-        self.client.get(
-            choice(endpoints),
-            headers=USER_AGENT,
-            name="/uk-setup-guide/[guide]/",
-            auth=basic_auth(),
-        )
-
-
-    @task
     def industry_pages(self):
         urls = [
-            "industries/",
-            "industries/advanced-manufacturing/",
-            "industries/aerospace/",
-            "industries/agri-tech/",
-            "industries/asset-management/",
-            "industries/automotive/",
-            "industries/automotive-research-and-development/",
-            "industries/automotive-supply-chain/",
-            "industries/capital-investment/",
-            "industries/chemicals/",
-            "industries/creative-content-and-production/",
-            "industries/creative-industries/",
-            "industries/data-analytics/",
-            "industries/digital-media/",
-            "industries/electrical-networks/",
-            "industries/energy/",
-            "industries/energy-waste/",
-            "industries/financial-services/",
-            "industries/financial-technology/",
-            "industries/food-and-drink/",
-            "industries/food-service-and-catering/",
-            "industries/free-foods/",
-            "industries/health-and-life-sciences/",
-            "industries/meat-poultry-and-dairy/",
-            "industries/medical-technology/",
-            "industries/motorsport/",
-            "industries/nuclear-energy/",
-            "industries/offshore-wind-energy/",
-            "industries/oil-and-gas/",
-            "industries/pharmaceutical-manufacturing/",
-            "industries/retail/",
-            "industries/technology/",
+            URLs.INVEST_INDUSTRIES.relative,
+            URLs.INVEST_INDUSTRIES_ADVANCED_MANUFACTURING.relative,
+            URLs.INVEST_INDUSTRIES_AEROSPACE.relative,
+            URLs.INVEST_INDUSTRIES_AGRI_TECH.relative,
+            URLs.INVEST_INDUSTRIES_ASSET_MANAGEMENT.relative,
+            URLs.INVEST_INDUSTRIES_AUTOMOTIVE.relative,
+            URLs.INVEST_INDUSTRIES_AUTOMOTIVE_RESEARCH_AND_DEVELOPMENT.relative,
+            URLs.INVEST_INDUSTRIES_AUTOMOTIVE_SUPPLY_CHAIN.relative,
+            URLs.INVEST_INDUSTRIES_CAPITAL_INVESTMENT.relative,
+            URLs.INVEST_INDUSTRIES_CHEMICALS.relative,
+            URLs.INVEST_INDUSTRIES_CREATIVE_CONTENT_AND_PRODUCTION.relative,
+            URLs.INVEST_INDUSTRIES_CREATIVE_INDUSTRIES.relative,
+            URLs.INVEST_INDUSTRIES_DATA_ANALYTICS.relative,
+            URLs.INVEST_INDUSTRIES_DIGITAL_MEDIA.relative,
+            URLs.INVEST_INDUSTRIES_ELECTRICAL_NETWORKS.relative,
+            URLs.INVEST_INDUSTRIES_ENERGY.relative,
+            URLs.INVEST_INDUSTRIES_ENERGY_WASTE.relative,
+            URLs.INVEST_INDUSTRIES_FINANCIAL_SERVICES.relative,
+            URLs.INVEST_INDUSTRIES_FINANCIAL_TECHNOLOGY.relative,
+            URLs.INVEST_INDUSTRIES_FOOD_AND_DRINK.relative,
+            URLs.INVEST_INDUSTRIES_FOOD_SERVICE_AND_CATERING.relative,
+            URLs.INVEST_INDUSTRIES_FREE_FOODS.relative,
+            URLs.INVEST_INDUSTRIES_HEALTH_AND_LIFE_SCIENCES.relative,
+            URLs.INVEST_INDUSTRIES_MEAT_POULTRY_AND_DAIRY.relative,
+            URLs.INVEST_INDUSTRIES_MEDICAL_TECHNOLOGY.relative,
+            URLs.INVEST_INDUSTRIES_MOTORSPORT.relative,
+            URLs.INVEST_INDUSTRIES_NUCLEAR_ENERGY.relative,
+            URLs.INVEST_INDUSTRIES_OFFSHORE_WIND_ENERGY.relative,
+            URLs.INVEST_INDUSTRIES_OIL_AND_GAS.relative,
+            URLs.INVEST_INDUSTRIES_PHARMACEUTICAL_MANUFACTURING.relative,
+            URLs.INVEST_INDUSTRIES_RETAIL.relative,
+            URLs.INVEST_INDUSTRIES_TECHNOLOGY.relative,
         ]
         self.client.get(
             choice(urls),
