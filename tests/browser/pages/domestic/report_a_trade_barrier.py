d7aac96a9bdd2fae3d6de90429d55c0eb7837007
kowalcj0 <kowalczykjanusz@gmail.com>
2019-06-21 19:11:11 +0100

    PO: report a trade barrier

---
+++ tests/browser/pages/domestic/report_a_trade_barrier.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""Domestic - Report a trade barrier page"""
+from typing import List
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages import ElementType, Services, common_selectors
+from pages.common_actions import (
+    Selector,
+    check_for_sections,
+    check_url,
+    go_to_url,
+    take_screenshot,
+)
+from pages.domestic import actions as domestic_actions
+from settings import EXRED_UI_URL
+
+NAME = "Report a trade barrier"
+SERVICE = Services.DOMESTIC
+TYPE = "article"
+URL = urljoin(EXRED_UI_URL, "report-trade-barrier/")
+
+SELECTORS = {
+    "description": {
+        "intro sections": Selector(By.CSS_SELECTOR, "p.lede"),
+        "report a barrier": Selector(
+            By.CSS_SELECTOR, "a.button", type=ElementType.LINK
+        ),
+        "contact the relevant british embassy": Selector(
+            By.CSS_SELECTOR, "section div.grid-row p > a", type=ElementType.LINK
+        ),
+        "report steps": Selector(By.CSS_SELECTOR, "ol.list.big-number-list"),
+    }
+}
+SELECTORS.update(common_selectors.HEADER)
+SELECTORS.update(common_selectors.BREADCRUMBS)
+SELECTORS.update(common_selectors.ERROR_REPORTING)
+SELECTORS.update(common_selectors.FOOTER)
+
+
+def visit(driver: WebDriver):
+    go_to_url(driver, URL, NAME)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL)
+
+
+def should_see_sections(driver: WebDriver, names: List[str]):
+    check_for_sections(driver, all_sections=SELECTORS, sought_sections=names)
+
+
+def search(driver: WebDriver, phrase: str):
+    domestic_actions.search(driver, phrase)
