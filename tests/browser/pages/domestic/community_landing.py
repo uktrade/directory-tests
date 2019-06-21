408ff675d8e7afdd731e47fc013d4a0c059cfbcc
kowalcj0 <kowalczykjanusz@gmail.com>
2019-06-21 19:11:48 +0100

    PO: Join export community pages

---
+++ tests/browser/pages/domestic/community_landing.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Domestic - Join our export community - landing page"""
+from typing import List
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages import ElementType, Services, common_selectors
+from pages.common_actions import (
+    Selector,
+    check_for_sections,
+    check_url,
+    go_to_url,
+    take_screenshot,
+)
+from pages.domestic import actions as domestic_actions
+from settings import EXRED_UI_URL
+
+NAME = "Join our export community"
+SERVICE = Services.DOMESTIC
+TYPE = "landing"
+URL = urljoin(EXRED_UI_URL, "community/")
+
+SELECTORS = {
+    "share on social media": {"itself": Selector(By.CSS_SELECTOR, "ul.sharing-links")},
+    "description": {
+        "header": Selector(By.CSS_SELECTOR, "article header"),
+        "heading": Selector(By.CSS_SELECTOR, "h1.heading-xlarge"),
+        "sign up": Selector(
+            By.CSS_SELECTOR, "p.body-text a.link", type=ElementType.LINK
+        ),
+    },
+}
+SELECTORS.update(common_selectors.HEADER)
+SELECTORS.update(common_selectors.BREADCRUMBS)
+SELECTORS.update(common_selectors.ERROR_REPORTING)
+SELECTORS.update(common_selectors.FOOTER)
+
+
+def visit(driver: WebDriver):
+    go_to_url(driver, URL, NAME)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL)
+
+
+def should_see_sections(driver: WebDriver, names: List[str]):
+    check_for_sections(driver, all_sections=SELECTORS, sought_sections=names)
+
+
+def search(driver: WebDriver, phrase: str):
+    domestic_actions.search(driver, phrase)
