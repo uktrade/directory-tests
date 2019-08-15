1a3670a3bdb6b58977f16486bb0a6f114429a345
kowalcj0 <kowalczykjanusz@gmail.com>
2019-08-15 17:02:11 +0100

    ignore additional problematic page type

--- tests/smoke/test_cms.py
+++ tests/smoke/test_cms.py
@@ -20,6 +20,7 @@ from tests.smoke.cms_api_helpers import (
 SKIPPED_PAGE_TYPES = [
     "great_international.internationalcapitalinvestlandingpage",
     "great_international.baseinternationalsectorpage",
+    "great_international.capitalinvestopportunitypage",
 ]
 
 ALL_PAGE_TYPES = get_pages_types(skip=SKIPPED_PAGE_TYPES)
