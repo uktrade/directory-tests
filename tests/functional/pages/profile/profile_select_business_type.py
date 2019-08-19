b5b9c78c04386313f0e5031c15df19b3171a2a3f
kowalcj0 <kowalczykjanusz@gmail.com>
2019-08-19 15:20:02 +0100

    PO: select business type

---
+++ tests/functional/pages/profile/profile_select_business_type.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""Profile - Select Business Type"""
+from requests import Response, Session
+
+from tests import URLs
+from tests.functional.pages import Services
+from tests.functional.utils.context_utils import Actor, Company
+from tests.functional.utils.request import Method, check_response, make_request
+
+SERVICE = Services.PROFILE
+NAME = "Select business type"
+TYPE = "form"
+URL = URLs.PROFILE_ENROL_SELECT_BUSINESS_TYPE.absolute
+EXPECTED_STRINGS = [
+    "Select your business type",
+    "My business is registered with Companies House",
+    "I'm a sole trader or I represent another type of UK ",
+    "I'm a UK taxpayer but do not represent a business",
+    "My business or organisation is not registered in the UK",
+]
+
+
+def go_to(session: Session) -> Response:
+    return make_request(Method.GET, URL, session=session)
+
+
+def should_be_here(response: Response):
+    check_response(response, 200, body_contains=EXPECTED_STRINGS)
+
+
+def submit(actor: Actor, company: Company) -> Response:
+    session = actor.session
+    headers = {"Referer": URL}
+    assert company.business_type
+    data = {
+        "csrfmiddlewaretoken": actor.csrfmiddlewaretoken,
+        "choice": company.business_type,
+    }
+
+    return make_request(
+        Method.POST, URL, session=session, headers=headers, data=data
+    )
