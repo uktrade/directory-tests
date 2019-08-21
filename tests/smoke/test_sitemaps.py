ef41a69f43e7f4e03507c9751959eb20e736c22c
kowalcj0 <kowalczykjanusz@gmail.com>
2019-08-21 18:13:41 +0100

    don't scan FAS sitemap.xml as it doesn't exist

--- tests/smoke/test_sitemaps.py
+++ tests/smoke/test_sitemaps.py
@@ -21,7 +21,6 @@ def get_urls_from_sitemap(sitemap_url: str) -> list:
     [
         URLs.DOMESTIC_SITEMAP.absolute,
         URLs.FAB_SITEMAP.absolute,
-        URLs.FAS_SITEMAP.absolute,
         URLs.INTERNATIONAL_SITEMAP.absolute,
         # URLs.EXOPPS_SITEMAP.absolute,  # missing sitemap.xml. See TT-1581
         # URLs.PROFILE_SITEMAP.absolute,  # missing sitemap.xml. See TT-1581
@@ -37,7 +36,6 @@ def test_if_sitemap_exist(url, basic_auth):
     [
         URLs.DOMESTIC_SITEMAP.absolute,
         URLs.FAB_SITEMAP.absolute,
-        URLs.FAS_SITEMAP.absolute,
         # URLs.INTERNATIONAL_SITEMAP.absolute,  # empty sitemap.xml. See CI-293
     ],
 )
@@ -61,7 +59,6 @@ def test_check_sitemap_schema_and_if_it_contains_urls(url, basic_auth):
     [
         # URLs.DOMESTIC_SITEMAP.absolute, # contains self-reference. See bug CMS-1685
         URLs.FAB_SITEMAP.absolute,
-        URLs.FAS_SITEMAP.absolute,
     ],
 )
 def test_sitemap_should_not_contain_reference_to_itself(url):
@@ -81,8 +78,7 @@ def test_sitemap_should_not_contain_reference_to_itself(url):
     # get_urls_from_sitemap(URLs.SOO_SITEMAP.absolute) +
     set(
         get_urls_from_sitemap(URLs.DOMESTIC_SITEMAP.absolute) +
-        get_urls_from_sitemap(URLs.FAB_SITEMAP.absolute) +
-        get_urls_from_sitemap(URLs.FAS_SITEMAP.absolute)
+        get_urls_from_sitemap(URLs.FAB_SITEMAP.absolute)
     ) - {
         URLs.DOMESTIC_API_COMPANY_HOUSE_SEARCH.absolute,  # see CMS-1699
         URLs.TRADE_BARRIERS_REPORT_FORM_SUCCESS.absolute,  # see CMS-1811
