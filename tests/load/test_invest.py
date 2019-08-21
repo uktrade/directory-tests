b74f2f7a1f3c3b824eaf58c7ef05872eb6898ffe
kowalcj0 <kowalczykjanusz@gmail.com>
2019-08-21 18:06:39 +0100

    fix load tests for Invest
    for HPO pages we have to use absolute URLs

--- tests/load/test_invest.py
+++ tests/load/test_invest.py
@@ -13,49 +13,31 @@ class InvestTasks(TaskSet):
         self.client.get(
             url,
             headers=USER_AGENT,
+            name="/",
             auth=basic_auth(),
         )
 
     @task
-    def industry_pages(self):
-        urls = [
-            URLs.INVEST_INDUSTRIES.relative,
-            URLs.INVEST_INDUSTRIES_ADVANCED_MANUFACTURING.relative,
-            URLs.INVEST_INDUSTRIES_AEROSPACE.relative,
-            URLs.INVEST_INDUSTRIES_AGRI_TECH.relative,
-            URLs.INVEST_INDUSTRIES_ASSET_MANAGEMENT.relative,
-            URLs.INVEST_INDUSTRIES_AUTOMOTIVE.relative,
-            URLs.INVEST_INDUSTRIES_AUTOMOTIVE_RESEARCH_AND_DEVELOPMENT.relative,
-            URLs.INVEST_INDUSTRIES_AUTOMOTIVE_SUPPLY_CHAIN.relative,
-            URLs.INVEST_INDUSTRIES_CAPITAL_INVESTMENT.relative,
-            URLs.INVEST_INDUSTRIES_CHEMICALS.relative,
-            URLs.INVEST_INDUSTRIES_CREATIVE_CONTENT_AND_PRODUCTION.relative,
-            URLs.INVEST_INDUSTRIES_CREATIVE_INDUSTRIES.relative,
-            URLs.INVEST_INDUSTRIES_DATA_ANALYTICS.relative,
-            URLs.INVEST_INDUSTRIES_DIGITAL_MEDIA.relative,
-            URLs.INVEST_INDUSTRIES_ELECTRICAL_NETWORKS.relative,
-            URLs.INVEST_INDUSTRIES_ENERGY.relative,
-            URLs.INVEST_INDUSTRIES_ENERGY_WASTE.relative,
-            URLs.INVEST_INDUSTRIES_FINANCIAL_SERVICES.relative,
-            URLs.INVEST_INDUSTRIES_FINANCIAL_TECHNOLOGY.relative,
-            URLs.INVEST_INDUSTRIES_FOOD_AND_DRINK.relative,
-            URLs.INVEST_INDUSTRIES_FOOD_SERVICE_AND_CATERING.relative,
-            URLs.INVEST_INDUSTRIES_FREE_FOODS.relative,
-            URLs.INVEST_INDUSTRIES_HEALTH_AND_LIFE_SCIENCES.relative,
-            URLs.INVEST_INDUSTRIES_MEAT_POULTRY_AND_DAIRY.relative,
-            URLs.INVEST_INDUSTRIES_MEDICAL_TECHNOLOGY.relative,
-            URLs.INVEST_INDUSTRIES_MOTORSPORT.relative,
-            URLs.INVEST_INDUSTRIES_NUCLEAR_ENERGY.relative,
-            URLs.INVEST_INDUSTRIES_OFFSHORE_WIND_ENERGY.relative,
-            URLs.INVEST_INDUSTRIES_OIL_AND_GAS.relative,
-            URLs.INVEST_INDUSTRIES_PHARMACEUTICAL_MANUFACTURING.relative,
-            URLs.INVEST_INDUSTRIES_RETAIL.relative,
-            URLs.INVEST_INDUSTRIES_TECHNOLOGY.relative,
+    def contact(self):
+        self.client.get(
+            URLs.INVEST_CONTACT.absolute,
+            headers=USER_AGENT,
+            name="/contact/",
+            auth=basic_auth(),
+        )
+
+    @task
+    def hpo(self):
+        hpo_urls = [
+            URLs.INVEST_HPO_FOOD.absolute,
+            URLs.INVEST_HPO_LIGHTWEIGHT.absolute,
+            URLs.INVEST_HPO_RAIL.absolute,
+            URLs.INVEST_HPO_CONTACT.absolute,
         ]
         self.client.get(
-            choice(urls),
+            choice(hpo_urls),
             headers=USER_AGENT,
-            name="/industries/[industry]/",
+            name="/high-potential-opportunities/[hpo]/",
             auth=basic_auth(),
         )
 
