2c8f27c419b379d4b19dc8a4b3d7f6ac1146ce81
kowalcj0 <kowalczykjanusz@gmail.com>
2019-08-22 11:45:18 +0100

    fix test examples & re-enable those affected by bugs

--- tests/smoke/test_sitemaps.py
+++ tests/smoke/test_sitemaps.py
@@ -31,9 +31,9 @@ def get_urls_from_sitemap(sitemap_url: str, *, ignore_404: bool = False) -> list
         URLs.DOMESTIC_SITEMAP.absolute,
         URLs.FAB_SITEMAP.absolute,
         URLs.INTERNATIONAL_SITEMAP.absolute,
-        # URLs.EXOPPS_SITEMAP.absolute,  # missing sitemap.xml. See TT-1581
-        # URLs.PROFILE_SITEMAP.absolute,  # missing sitemap.xml. See TT-1581
-        # URLs.SOO_SITEMAP.absolute,  # missing sitemap.xml. See TT-1581
+        URLs.EXOPPS_SITEMAP.absolute,  # missing sitemap.xml. See TT-1581
+        URLs.PROFILE_SITEMAP.absolute,  # missing sitemap.xml. See TT-1581
+        URLs.SOO_SITEMAP.absolute,  # missing sitemap.xml. See TT-1581
     ],
 )
 def test_if_sitemap_exist(url, basic_auth):
@@ -45,7 +45,7 @@ def test_if_sitemap_exist(url, basic_auth):
     [
         URLs.DOMESTIC_SITEMAP.absolute,
         URLs.FAB_SITEMAP.absolute,
-        # URLs.INTERNATIONAL_SITEMAP.absolute,  # empty sitemap.xml. See CI-293
+        URLs.INTERNATIONAL_SITEMAP.absolute,  # empty sitemap.xml. See CI-293
     ],
 )
 def test_check_sitemap_schema_and_if_it_contains_urls(url, basic_auth):
@@ -66,7 +66,7 @@ def test_check_sitemap_schema_and_if_it_contains_urls(url, basic_auth):
 @pytest.mark.parametrize(
     "url",
     [
-        # URLs.DOMESTIC_SITEMAP.absolute, # contains self-reference. See bug CMS-1685
+        URLs.DOMESTIC_SITEMAP.absolute,  # contains a self-reference. See bug CMS-1699
         URLs.FAB_SITEMAP.absolute,
     ],
 )
@@ -78,15 +78,29 @@ def test_sitemap_should_not_contain_reference_to_itself(url):
 
 @pytest.mark.parametrize(
     "url",
-    # empty sitemap.xml. See CI-293
-    # get_urls_from_sitemap(URLs.INTERNATIONAL_SITEMAP.absolute) +
-    # get_urls_from_sitemap(URLs.INVEST_SITEMAP.absolute) +
-    # missing sitemap.xml. See TT-1581
-    # get_urls_from_sitemap(URLs.EXOPPS_SITEMAP.absolute) +
-    # get_urls_from_sitemap(URLs.PROFILE_SITEMAP.absolute) +
-    # get_urls_from_sitemap(URLs.SOO_SITEMAP.absolute) +
+    [
+        URLs.DOMESTIC_SITEMAP.absolute,  # contains a dupe. See bug CMS-1699
+        URLs.FAB_SITEMAP.absolute,
+    ],
+)
+def test_sitemap_should_not_contain_duplicated_links(url):
+    sitemap_urls = get_urls_from_sitemap(url)
+    dupes = set([url for url in sitemap_urls if sitemap_urls.count(url) > 1])
+    error = f"{url} contains duplicated URLs: {', '.join(dupes)}"
+    assert not dupes, error
+
+
+@pytest.mark.parametrize(
+    "url",
     set(
-        get_urls_from_sitemap(URLs.DOMESTIC_SITEMAP.absolute) +
+        # empty sitemap.xml. See CI-293
+        get_urls_from_sitemap(URLs.INTERNATIONAL_SITEMAP.absolute) +
+        # missing sitemap.xml. See TT-1581
+        get_urls_from_sitemap(URLs.EXOPPS_SITEMAP.absolute) +
+        get_urls_from_sitemap(URLs.PROFILE_SITEMAP.absolute) +
+        get_urls_from_sitemap(URLs.SOO_SITEMAP.absolute) +
+
+        get_urls_from_sitemap(URLs.DOMESTIC_SITEMAP.absolute, ignore_404=True) +
         get_urls_from_sitemap(URLs.FAB_SITEMAP.absolute)
     ) - {
         URLs.DOMESTIC_API_COMPANY_HOUSE_SEARCH.absolute,  # see CMS-1699
