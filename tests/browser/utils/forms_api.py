93f7e3ef3724867502e39e1bad4d61f33363becc
kowalcj0 <kowalczykjanusz@gmail.com>
2019-09-02 10:30:09 +0100

    util:add more filtering helpers for forms-api

--- tests/browser/utils/forms_api.py
+++ tests/browser/utils/forms_api.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+from typing import List
 
 from directory_client_core.base import AbstractAPIClient
 
@@ -23,7 +24,36 @@ client = FormsClient(
 )
 
 
-def find_form_submissions(email: str) -> dict:
+def filter_by_action(submissions: List[dict], action: str) -> list:
+    return list(filter(lambda x: x["meta"]["action_name"] == action, submissions))
+
+
+def filter_by_sender_email(submissions: List[dict], email: str) -> list:
+    return list(
+        filter(lambda x:
+               "sender" in x["meta"] and x["meta"]["sender"]["email_address"] == email,
+               submissions)
+    )
+
+
+def filter_by_subject(submissions: List[dict], action: str) -> list:
+    return list(filter(lambda x: x["meta"]["subject"] == action, submissions))
+
+
+def find_form_submissions(email: str) -> List[dict]:
     json = client.get(f"testapi/submissions-by-email/{email}/").json()
     logging.debug(f"Submissions for {email}: {json}")
     return json
+
+
+def find_form_submissions_by_subject_and_action(
+        email: str, subject: str, action: str
+) -> List[dict]:
+    submissions = find_form_submissions(email)
+    by_subject = filter_by_subject(submissions, subject)
+    return filter_by_action(by_subject, action)
+
+
+def find_form_submissions_for_dit_office(mailbox: str, sender: str) -> List[dict]:
+    submissions = find_form_submissions(mailbox)
+    return filter_by_sender_email(submissions, sender)
