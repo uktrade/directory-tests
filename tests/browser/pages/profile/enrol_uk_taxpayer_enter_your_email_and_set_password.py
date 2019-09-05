317779a0ba4b1716fdaf5b58e6c3b8991cfd07ae
kowalcj0 <kowalczykjanusz@gmail.com>
2019-09-05 18:41:48 +0100

    PO: add new enrolment pages

---
+++ tests/browser/pages/profile/enrol_uk_taxpayer_enter_your_email_and_set_password.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""Profile - Enrol - UK taxpayer - Enter your email address and set a password"""
+import logging
+from types import ModuleType
+from typing import List
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages import ElementType, Services
+from pages.common_actions import (
+    Actor,
+    Selector,
+    check_for_sections,
+    check_url,
+    fill_out_input_fields,
+    find_and_click_on_page_element,
+    go_to_url,
+    take_screenshot,
+    tick_captcha_checkbox,
+    tick_checkboxes,
+)
+from pages.profile import enrol_enter_your_confirmation_code
+from settings import DIRECTORY_UI_PROFILE_URL
+
+NAME = "Enter your email address and set a password (UK taxpayer)"
+SERVICE = Services.PROFILE
+TYPE = "Enrol"
+URL = urljoin(DIRECTORY_UI_PROFILE_URL, "enrol/business-type/individual/user-account/")
+PAGE_TITLE = ""
+
+SELECTORS = {
+    "breadcrumbs": {"itself": Selector(By.CSS_SELECTOR, "nav.breadcrumbs")},
+    "enrolment progress bar": {"itself": Selector(By.ID, "progress-column")},
+    "registration form": {
+        "itself": Selector(By.CSS_SELECTOR, "section form"),
+        "email": Selector(
+            By.ID, "id_user-account-email", type=ElementType.INPUT, is_visible=False
+        ),
+        "password": Selector(
+            By.ID, "id_user-account-password", type=ElementType.INPUT, is_visible=False
+        ),
+        "confirm password": Selector(
+            By.ID,
+            "id_user-account-password_confirmed",
+            type=ElementType.INPUT,
+            is_visible=False,
+        ),
+        "t & c": Selector(
+            By.ID,
+            "id_user-account-terms_agreed-label",
+            type=ElementType.CHECKBOX,
+            is_visible=False,
+        ),
+        "submit": Selector(
+            By.CSS_SELECTOR, "form button.button", type=ElementType.BUTTON
+        ),
+    },
+}
+
+
+def visit(driver: WebDriver):
+    go_to_url(driver, URL, NAME)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL, exact_match=False)
+    msg = f"Got 404 on {driver.current_url}"
+    assert "This page cannot be found" not in driver.page_source, msg
+
+
+def should_see_sections(driver: WebDriver, names: List[str]):
+    check_for_sections(driver, all_sections=SELECTORS, sought_sections=names)
+
+
+def generate_form_details(actor: Actor) -> dict:
+    result = {
+        "email": actor.email,
+        "password": actor.password,
+        "confirm password": actor.password,
+        "t & c": True,
+    }
+    logging.debug(f"Generated form details: {result}")
+    return result
+
+
+def fill_out(driver: WebDriver, details: dict):
+    form_selectors = SELECTORS["registration form"]
+    fill_out_input_fields(driver, form_selectors, details)
+    tick_checkboxes(driver, form_selectors, details)
+    tick_captcha_checkbox(driver)
+    take_screenshot(driver, "After filling out the form")
+
+
+def submit(driver: WebDriver) -> ModuleType:
+    take_screenshot(driver, "Before submitting the form")
+    find_and_click_on_page_element(driver, SELECTORS, "submit", wait_for_it=False)
+    take_screenshot(driver, "After submitting the form")
+    return enrol_enter_your_confirmation_code
---
+++ tests/browser/pages/profile/enrol_you_cannot_create_account.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""Profile - Enrol - You cannot create an account page"""
+import logging
+from types import ModuleType
+from typing import List
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages import Services, common_selectors
+from pages.common_actions import (
+    Actor,
+    Selector,
+    check_for_sections,
+    check_url,
+    fill_out_input_fields,
+    find_and_click_on_page_element,
+    go_to_url,
+    take_screenshot,
+    tick_captcha_checkbox,
+    tick_checkboxes,
+)
+from pages.profile import enrol_enter_your_confirmation_code
+from settings import DIRECTORY_UI_PROFILE_URL
+
+NAME = "You cannot create an account"
+SERVICE = Services.PROFILE
+TYPE = "Enrol"
+URL = urljoin(DIRECTORY_UI_PROFILE_URL, "enrol/business-type/overseas-business/")
+PAGE_TITLE = ""
+
+SELECTORS = {
+    "explanation": {
+        "heading": Selector(By.CSS_SELECTOR, "#content div > h1"),
+        "back": Selector(By.PARTIAL_LINK_TEXT, "Back"),
+        "view our site for international businesses": Selector(
+            By.PARTIAL_LINK_TEXT, "View our site for international businesses"
+        ),
+    },
+}
+SELECTORS.update(common_selectors.HEADER)
+SELECTORS.update(common_selectors.BREADCRUMBS)
+SELECTORS.update(common_selectors.FOOTER)
+
+
+def visit(driver: WebDriver):
+    go_to_url(driver, URL, NAME)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL)
+    msg = f"Got 404 on {driver.current_url}"
+    assert "This page cannot be found" not in driver.page_source, msg
+
+
+def should_see_sections(driver: WebDriver, names: List[str]):
+    check_for_sections(driver, all_sections=SELECTORS, sought_sections=names)
+
+
+def generate_form_details(actor: Actor) -> dict:
+    result = {
+        "email": actor.email,
+        "password": actor.password,
+        "confirm password": actor.password,
+        "t & c": True,
+    }
+    logging.debug(f"Generated form details: {result}")
+    return result
+
+
+def fill_out(driver: WebDriver, details: dict):
+    form_selectors = SELECTORS["registration form"]
+    fill_out_input_fields(driver, form_selectors, details)
+    tick_checkboxes(driver, form_selectors, details)
+    tick_captcha_checkbox(driver)
+    take_screenshot(driver, "After filling out the form")
+
+
+def submit(driver: WebDriver) -> ModuleType:
+    take_screenshot(driver, "Before submitting the form")
+    find_and_click_on_page_element(driver, SELECTORS, "submit", wait_for_it=False)
+    take_screenshot(driver, "After submitting the form")
+    return enrol_enter_your_confirmation_code
