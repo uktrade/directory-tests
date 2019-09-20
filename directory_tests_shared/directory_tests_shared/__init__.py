72d28f9ca26ee9509eb3bf35be93c6805d2e3407
kowalcj0 <kowalczykjanusz@gmail.com>
2019-09-20 21:07:39 +0100

    organise module listing

--- directory_tests_shared/directory_tests_shared/__init__.py
+++ directory_tests_shared/directory_tests_shared/__init__.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
-from . import settings, utils
+from . import constants, settings, utils
 from .enums import BusinessType, PageType, Service
 from .urls import URLs
-__all__ = ("settings", "utils", "BusinessType", "PageType", "Service", "URLs")
+__all__ = (
+    "BusinessType",
+    "constants",
+    "PageType",
+    "Service",
+    "settings",
+    "URLs",
+    "utils",
+)
