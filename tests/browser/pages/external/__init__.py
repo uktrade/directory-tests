1bb173eb4f746eee4edda852535b84e28b0de12a
kowalcj0 <kowalczykjanusz@gmail.com>
2019-09-23 18:20:48 +0100

    PO: remove two unused pages

--- tests/browser/pages/external/__init__.py
+++ tests/browser/pages/external/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 __all__ = [
     "british_council_home",
+    "eori_home",
+    "events_event",
     "events_home",
-    "legal_services_home",
     "visit_britain_home",
 ]
