b0bb5757b8c8e0fa71b79edb428502bbb19f6c95
kowalcj0 <kowalczykjanusz@gmail.com>
2019-09-25 16:13:15 +0100

    PO: use shared URL definition

--- tests/browser/pages/profile/edit_company_profile.py
+++ tests/browser/pages/profile/edit_company_profile.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """Profile - Edit Company Profile"""
 import logging
-from urllib.parse import urljoin
 
+from directory_tests_shared import URLs
 from directory_tests_shared.enums import Service
-from directory_tests_shared.settings import PROFILE_URL
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from pages.common_actions import check_url, go_to_url, take_screenshot
@@ -12,7 +11,7 @@ from pages.common_actions import check_url, go_to_url, take_screenshot
 NAME = "Edit Company Profile"
 SERVICE = Service.PROFILE
 TYPE = "profile"
-URL = urljoin(PROFILE_URL, "find-a-buyer/")
+URL = URLs.PROFILE_FAB.absolute
 PAGE_TITLE = "Business profile - great.gov.uk"
 
 SELECTORS = {}
