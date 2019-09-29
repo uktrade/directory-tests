a47dc63cf78e9f1c3353ce37a4e0c1bc5c6b8916
kowalcj0 <kowalczykjanusz@gmail.com>
2019-09-29 19:14:38 +0100

    PO:add enrolment pages for an individual

---
+++ tests/functional/pages/profile/individual_enter_email_verification_code.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Profile - Individual enter your email verification code"""
+
+from requests import Response, Session
+
+from directory_tests_shared import PageType, Service, URLs
+from tests.functional.utils.context_utils import Actor
+from tests.functional.utils.request import Method, check_response, make_request
+
+SERVICE = Service.PROFILE
+NAME = "Individual enter email verification code"
+TYPE = PageType.FORM
+URL = URLs.PROFILE_ENROL_INDIVIDUAL_EMAIL_VERIFICATION.absolute
+EXPECTED_STRINGS = ["Enter your confirmation code"]
+
+
+def go_to(session: Session) -> Response:
+    return make_request(Method.GET, URL, session=session)
+
+
+def should_be_here(response: Response):
+    check_response(response, 200, body_contains=EXPECTED_STRINGS)
+
+
+def submit(actor: Actor) -> Response:
+    session = actor.session
+    assert actor.email_confirmation_code
+    headers = {"Referer": URL}
+    data = {
+        "csrfmiddlewaretoken": actor.csrfmiddlewaretoken,
+        "individual_user_enrolment_view-current_step": "verification",
+        "verification-code": actor.email_confirmation_code,
+    }
+
+    return make_request(Method.POST, URL, session=session, headers=headers, data=data)
