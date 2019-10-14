45340bb7228e4f9cea96f4c43e8a9567f4d7072f
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-14 19:46:52 +0100

    PO: remove unused go_to_sign_in action

--- tests/browser/pages/sso/registration_confirmation.py
+++ tests/browser/pages/sso/registration_confirmation.py
@@ -5,13 +5,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 from directory_tests_shared import URLs
 from directory_tests_shared.enums import Service
-from pages.common_actions import (
-    Selector,
-    check_url,
-    find_element,
-    take_screenshot,
-    wait_for_page_load_after_action,
-)
+from pages.common_actions import Selector, check_url, take_screenshot
 
 NAME = "Registration Confirmation"
 SERVICE = Service.SSO
@@ -32,10 +26,3 @@ SELECTORS = {
 def should_be_here(driver: WebDriver):
     take_screenshot(driver, NAME)
     check_url(driver, URL, exact_match=False)
-
-
-def go_to_sign_in(driver: WebDriver):
-    registration_link = find_element(driver, SIGN_IN_LINK)
-    with wait_for_page_load_after_action(driver):
-        registration_link.click()
-    take_screenshot(driver, NAME + "after signing in")
