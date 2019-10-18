dbf427b7869a2c088a0e52f532ca21a114b9fb82
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-18 14:23:01 +0100

    PO: use CSS selector to find link to company profile
    it should be more reliable than regular text search

--- tests/functional/pages/fas/search.py
+++ tests/functional/pages/fas/search.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """Find a Supplier - Find a Supplier page"""
 import logging
+from typing import List
 
 from requests import Response, Session
 from retrying import retry
+from scrapy import Selector
 
 from directory_tests_shared import PageType, Service, URLs
 from tests.functional.utils.generic import escape_html, extract_page_contents
@@ -32,6 +34,8 @@ def go_to(
     """
     params = {}
     if term is not None:
+        # remove words which make FAS return hundreds of result pages
+        term = term.replace("LTD", "").replace("LIMITED", "")
         params.update({"q": term})
     if page is not None:
         params.update({"page": page})
@@ -49,20 +53,36 @@ def should_be_here(response, *, number=None):
     logging.debug("Buyer is on the FAS Find a Supplier page")
 
 
-def should_see_company(response: Response, company_title: str) -> bool:
-    content = extract_page_contents(response.content.decode("utf-8")).lower()
+def get_profile_links(page_content: str) -> List[tuple]:
+    profile_selector = "#companies-column > ul > li a"
+    profile_links = Selector(text=page_content).css(profile_selector).extract()
+    results = []
+    for link in profile_links:
+        href = Selector(text=link).css("a::attr(href)").extract()[0]
+        company_title = Selector(text=link).css("h3::text").extract()[0]
+        clean_company_title = escape_html(company_title.replace("  ", " ")).lower()
+        results.append((clean_company_title, href))
+    return results
+
+
+def find_profile_link(response: Response, company_title: str) -> str:
+    raw_content = response.content.decode("utf-8")
+    content = extract_page_contents(raw_content).lower()
+
     no_match = NO_UK_BUSINESS_MATCH.lower() in content
-    clean_title = escape_html(company_title.replace("  ", " ")).lower()
-    contains_company_title = clean_title in content
-    if not contains_company_title:
-        logging.debug(f"Try to find company using raw title: '{company_title.lower()}'")
-        contains_company_title = company_title.lower() in content
-    if not contains_company_title:
-        logging.debug(
-            f"Could not find company: '{company_title}' using: '{clean_title}' or "
-            f"'{company_title.lower()}'"
-        )
-    return contains_company_title and not no_match
+    if no_match:
+        logging.warning(NO_UK_BUSINESS_MATCH)
+        return ""
+
+    profile_links = get_profile_links(raw_content)
+    logging.debug(f"List of found profiles: {profile_links}")
+
+    clean_company_title = escape_html(company_title.replace("  ", " ")).lower()
+    for title, href in profile_links:
+        if title == clean_company_title:
+            logging.debug(f"Found link to '{clean_company_title}' profile: {href}")
+            return href
+    return ""
 
 
 def should_not_see_company(response: Response, company_title: str) -> bool:
