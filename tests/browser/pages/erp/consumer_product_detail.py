b4090587f0063087e7970197449f954446e9bd7b
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-22 18:33:28 +0100

    PO: ERP product detail page for UK Customer

---
+++ tests/browser/pages/erp/consumer_product_detail.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""ERP - Product detail - UK consumer"""
+from typing import List
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from directory_tests_shared import URLs
+from directory_tests_shared.enums import PageType, Service
+from pages import ElementType, common_selectors
+from pages.common_actions import (
+    Selector,
+    check_for_sections,
+    check_url,
+    take_screenshot,
+)
+
+NAME = "Product detail (UK consumer)"
+SERVICE = Service.ERP
+TYPE = PageType.FORM
+URL = URLs.ERP_CONSUMER_PRODUCT_DETAIL.absolute
+PAGE_TITLE = ""
+
+SUBMIT_BUTTON = Selector(
+    By.CSS_SELECTOR, "#content > form button.govuk-button", type=ElementType.BUTTON
+)
+SELECTORS = {
+    "form": {
+        "selection form": Selector(By.CSS_SELECTOR, "#content form[method='post']"),
+        "step counter": Selector(
+            By.CSS_SELECTOR, "form[method=post] span.govuk-caption-l"
+        ),
+        "heading": Selector(By.CSS_SELECTOR, "form[method=post] h1"),
+        "change goods": Selector(
+            By.CSS_SELECTOR, "#selected-values-container a", type=ElementType.LINK
+        ),
+        "submit": SUBMIT_BUTTON,
+    }
+}
+SELECTORS.update(common_selectors.ERP_HEADER)
+SELECTORS.update(common_selectors.ERP_BETA)
+SELECTORS.update(common_selectors.ERP_BACK)
+SELECTORS.update(common_selectors.ERP_BREADCRUMBS)
+SELECTORS.update(common_selectors.ERP_SAVE_FOR_LATER)
+SELECTORS.update(common_selectors.ERP_FOOTER)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL, exact_match=False)
+
+
+def should_see_sections(driver: WebDriver, names: List[str]):
+    check_for_sections(driver, all_sections=SELECTORS, sought_sections=names)
