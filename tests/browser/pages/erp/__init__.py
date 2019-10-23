d04fac60ad91bd7370bd8843311f68e2af332915
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-23 16:41:47 +0100

    PO: add all new ERP pages to package listing

--- tests/browser/pages/erp/__init__.py
+++ tests/browser/pages/erp/__init__.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 __all__ = [
+    "autocomplete_callbacks",
+    "consumer_aware_of_changes",
+    "consumer_finished",
+    "consumer_group_details",
+    "consumer_other_changes_after_brexit",
+    "consumer_personal_details",
+    "consumer_product_detail",
     "consumer_product_search",
+    "consumer_summary",
+    "consumer_type",
     "developing_country_select",
     "landing",
     "save_for_later",
