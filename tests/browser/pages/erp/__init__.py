3619641324d796dfba6c23177139d0845aca0099
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-23 12:00:14 +0100

    PO: ERP progress saved (save for later)

--- tests/browser/pages/erp/__init__.py
+++ tests/browser/pages/erp/__init__.py
@@ -4,6 +4,7 @@ __all__ = [
     "developing_country_select",
     "landing",
     "save_for_later",
+    "save_for_later_progress_saved",
     "triage_import_from_overseas",
     "triage_user_type",
 ]
