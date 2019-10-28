5c6ff98f9d2ccb6211d918e7e6eb185fd5ea4fc5
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-28 14:31:47 +0000

    PO: ERP UK Importer - where do you import from?

---
+++ tests/browser/pages/erp/uk_importer_where_do_you_import_from.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""ERP - Country select"""
+from typing import List
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from directory_tests_shared import URLs
+from directory_tests_shared.enums import PageType, Service
+from pages import ElementType, common_selectors
+from pages.common_actions import (
+    Selector,
+    check_for_sections,
+    check_url,
+    take_screenshot,
+)
+
+NAME = "Where do you import from (UK importer)"
+SERVICE = Service.ERP
+TYPE = PageType.FORM
+URL = URLs.ERP_IMPORTER_WHICH_COUNTRIES.absolute
+PAGE_TITLE = ""
+
+SELECTORS = {
+    "form": {
+        "form itself": Selector(By.CSS_SELECTOR, "#content form[method='post']"),
+        "continue": Selector(
+            By.CSS_SELECTOR,
+            "#content > form button.govuk-button",
+            type=ElementType.BUTTON,
+        ),
+    }
+}
+SELECTORS.update(common_selectors.ERP_HEADER)
+SELECTORS.update(common_selectors.ERP_BETA)
+SELECTORS.update(common_selectors.ERP_BACK)
+SELECTORS.update(common_selectors.ERP_SAVE_FOR_LATER)
+SELECTORS.update(common_selectors.ERP_FOOTER)
+
+
+def should_be_here(driver: WebDriver):
+    take_screenshot(driver, NAME)
+    check_url(driver, URL, exact_match=False)
+
+
+def should_see_sections(driver: WebDriver, names: List[str]):
+    check_for_sections(driver, all_sections=SELECTORS, sought_sections=names)
