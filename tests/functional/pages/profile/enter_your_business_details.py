20f2e95bdb3d94e8d040b0b42dc4c2bf4ee1ef03
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-31 14:55:02 +0000

    PO: check if email verification code was accepted

--- tests/functional/pages/profile/enter_your_business_details.py
+++ tests/functional/pages/profile/enter_your_business_details.py
@@ -5,13 +5,19 @@ from requests import Response, Session
 
 from directory_tests_shared import PageType, Service, URLs
 from tests.functional.utils.context_utils import Actor, Company
-from tests.functional.utils.request import Method, check_response, make_request
+from tests.functional.utils.request import (
+    Method,
+    check_response,
+    check_url,
+    make_request,
+)
 
 SERVICE = Service.PROFILE
 NAME = "Enter your business details (CH search)"
 TYPE = PageType.FORM
 URL = URLs.PROFILE_ENROL_COMPANIES_HOUSE_SEARCH.absolute
 EXPECTED_STRINGS = ["Enter your business details"]
+UNEXPECTED_STRINGS = ["Invalid code"]
 
 
 def go_to(session: Session) -> Response:
@@ -19,7 +25,13 @@ def go_to(session: Session) -> Response:
 
 
 def should_be_here(response: Response):
-    check_response(response, 200, body_contains=EXPECTED_STRINGS)
+    check_url(response, URL)
+    check_response(
+        response,
+        200,
+        body_contains=EXPECTED_STRINGS,
+        unexpected_strings=UNEXPECTED_STRINGS,
+    )
 
 
 def submit(actor: Actor, company: Company) -> Response:
