eddfa8d3ead26a35fe2f5c9f3d5f032a926da695
kowalcj0 <kowalczykjanusz@gmail.com>
2019-11-01 18:10:12 +0000

    util: add autocomplete callback for industry & country

--- tests/browser/pages/erp/autocomplete_callbacks.py
+++ tests/browser/pages/erp/autocomplete_callbacks.py
@@ -4,6 +4,7 @@ import logging
 import random
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from pages.common_actions import Selector, assertion_msg, find_element, find_elements
@@ -58,3 +59,103 @@ def autocomplete_uk_region(driver: WebDriver, *, value):
     logging.debug(f"Selecting random element from the autocomplete listbox")
     option = random.choice(autocomplete_list_options)
     option.click()
+
+
+def autocomplete_industry(driver: WebDriver, *, value):
+    if isinstance(value, bool) and not value:
+        logging.debug(f"Won't use autocomplete")
+        return
+
+    if isinstance(value, bool):
+        logging.debug(f"Will select random industry to type")
+        options = find_elements(
+            driver,
+            Selector(
+                By.CSS_SELECTOR,
+                "#id_imported-products-usage-imported_good_sector-select option",
+                is_visible=False,
+            ),
+        )
+        options_texts = [
+            option.get_attribute("text")
+            for option in options
+            if option.get_attribute("text")
+        ]
+        logging.debug(f"Available region options: {options_texts}")
+        with assertion_msg(f"Expected to find at least 1 region option to choose from"):
+            assert options_texts
+
+        value = random.choice(options_texts)
+
+    logging.debug(f"Will select '{value}' from Region Autocomplete list")
+
+    # enter text value into the input field with autocomplete
+    input_selector = Selector(
+        By.ID, "id_imported-products-usage-imported_good_sector", is_visible=True
+    )
+    input = find_element(
+        driver, input_selector, element_name="industry input", wait_for_it=True
+    )
+    input.click()
+    input.send_keys(value)
+
+    logging.debug(f"Get list of options from autocomplete listbox")
+    autocomplete_list = find_element(
+        driver,
+        Selector(
+            By.ID,
+            "id_imported-products-usage-imported_good_sector__listbox",
+            is_visible=False,
+        ),
+        wait_for_it=True,
+    )
+    autocomplete_list_options = autocomplete_list.find_elements(By.TAG_NAME, "li")
+    with assertion_msg(f"Expected to find at least 1 region suggestion but got 0"):
+        assert autocomplete_list_options
+
+    logging.debug(f"Selecting random element from the autocomplete listbox")
+    option = random.choice(autocomplete_list_options)
+    option.click()
+
+
+def autocomplete_country(driver: WebDriver, *, value):
+    if isinstance(value, bool) and not value:
+        logging.debug(f"Won't use autocomplete")
+        return
+
+    if isinstance(value, bool):
+        logging.debug(f"Will select random country to type")
+        options = find_elements(
+            driver, Selector(By.CSS_SELECTOR, "select[id^=id] option", is_visible=False)
+        )
+        options_texts = [
+            option.get_attribute("text")
+            for option in options
+            if option.get_attribute("text")
+        ]
+        logging.debug(f"Available region options: {options_texts}")
+        with assertion_msg(
+            f"Expected to find at least 1 country option to choose from"
+        ):
+            assert options_texts
+
+        value = random.choice(options_texts)
+
+    logging.debug(f"Will select '{value}' from country autocomplete list")
+
+    # enter text value into the input field with autocomplete
+    input_selector = Selector(
+        By.ID, "id_which-countries-import_countries_autocomplete", is_visible=True
+    )
+    input = find_element(
+        driver, input_selector, element_name="country input", wait_for_it=False
+    )
+    input.click()
+    input.send_keys(value)
+
+    logging.debug(f"Get list of options from autocomplete listbox")
+    autocomplete_list = find_elements(
+        driver, Selector(By.CSS_SELECTOR, "ul[role=listbox] li", is_visible=True)
+    )
+    input.send_keys(Keys.DOWN)
+    random.choice(autocomplete_list).send_keys(Keys.SPACE)
