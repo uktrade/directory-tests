77a6b8ec71c31b3a6ce59fd3635a4a89b0812ae0
kowalcj0 <kowalczykjanusz@gmail.com>
2019-11-08 15:37:14 +0000

    PO: update PIR landing page & add missing actions

--- tests/browser/pages/pir/landing.py
+++ tests/browser/pages/pir/landing.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """PIR - Landing Page"""
 import logging
-from typing import List
+from types import ModuleType
+from typing import List, Union
+from uuid import uuid4
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -10,11 +12,17 @@ from directory_tests_shared import URLs
 from directory_tests_shared.enums import PageType, Service
 from pages import ElementType, common_selectors
 from pages.common_actions import (
+    Actor,
     Selector,
     check_for_sections,
     check_url,
+    fill_out_input_fields,
     go_to_url,
+    pick_option,
+    submit_form,
     take_screenshot,
+    tick_captcha_checkbox,
+    tick_checkboxes,
 )
 
 NAME = "Landing"
@@ -22,15 +30,12 @@ SERVICE = Service.PIR
 TYPE = PageType.LANDING
 URL = URLs.INVEST_PIR.absolute
 
-SUBMIT_BUTTON = Selector(
-    By.CSS_SELECTOR, "form input.button[type=submit]", type=ElementType.BUTTON
-)
 SELECTORS = {
     "hero": {
         "self": Selector(By.ID, "hero"),
         "heading": Selector(By.CSS_SELECTOR, "#hero h1"),
     },
-    "contact form": {
+    "form": {
         "itself": Selector(By.CSS_SELECTOR, "#content form"),
         "name": Selector(By.ID, "id_name", type=ElementType.INPUT),
         "company": Selector(By.ID, "id_company", type=ElementType.INPUT),
@@ -41,7 +46,9 @@ SELECTORS = {
         "send updates": Selector(
             By.ID, "id_gdpr_optin", type=ElementType.CHECKBOX, is_visible=False
         ),
-        "submit": SUBMIT_BUTTON,
+        "submit": Selector(
+            By.CSS_SELECTOR, "form input.button[type=submit]", type=ElementType.SUBMIT
+        ),
     },
 }
 SELECTORS.update(common_selectors.INTERNATIONAL_HEADER_WO_LANGUAGE_SELECTOR)
@@ -54,9 +61,36 @@ def visit(driver: WebDriver):
 
 def should_be_here(driver: WebDriver):
     take_screenshot(driver, NAME)
-    check_url(driver, URL, exact_match=False)
+    check_url(driver, URL)
     logging.debug("All expected elements are visible on '%s' page", NAME)
 
 
 def should_see_sections(driver: WebDriver, names: List[str]):
     check_for_sections(driver, all_sections=SELECTORS, sought_sections=names)
+
+
+def generate_form_details(actor: Actor, *, custom_details: dict = None) -> dict:
+    details = {
+        "name": str(uuid4()),
+        "company": actor.company_name or "Automated test - company name",
+        "email": actor.email,
+        "phone": "0123456789",
+        "country": None,
+        "sector": None,
+        "send updates": True,
+    }
+    if custom_details:
+        details.update(custom_details)
+    return details
+
+
+def fill_out(driver: WebDriver, details: dict):
+    form_selectors = SELECTORS["form"]
+    fill_out_input_fields(driver, form_selectors, details)
+    pick_option(driver, form_selectors, details)
+    tick_checkboxes(driver, form_selectors, details)
+    tick_captcha_checkbox(driver)
+
+
+def submit(driver: WebDriver) -> Union[ModuleType, None]:
+    return submit_form(driver, SELECTORS["form"])
