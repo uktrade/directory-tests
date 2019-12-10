a3ee5cb4ba6d26b4b3053ed2bd04e27d1cd9db27
kowalcj0 <kowalczykjanusz@gmail.com>
2019-12-10 15:44:01 +0000

    circle: monitor test queue

--- parallel_browser_tests/run_browser_tests_in_parallel.sh
+++ parallel_browser_tests/run_browser_tests_in_parallel.sh
@@ -12,8 +12,5 @@ PYTHONPATH=. behave --dry-run --no-source --no-summary --no-snippets --tags=~@wi
   echo "Added task for scenario: '${line}'"
 done
 
-#docker-compose exec pbt_worker sh -c "PYTHONPATH=. behave --dry-run --no-source --no-summary --no-snippets --tags=~@wip --tags=~@fixme --tags=~@skip --format=mini features/${FEATURE_DIR}" | grep -v "Supplied path\|Trying base directory" | while IFS=$'\r' read -r line ; do
-#  docker-compose exec -d pbt_worker python3 behave_celery_task.py "chrome" "${line}"
-#  docker-compose exec -d pbt_worker python3 behave_celery_task.py "firefox" "${line}"
-#  echo "Added task for scenario: '${line}'"
-#done
+# print stats until there are tests to execute
+python3 behave_celery_task.py
