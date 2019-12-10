a08821d6315f47145f2bcb4db44ebc71c272b88a
kowalcj0 <kowalczykjanusz@gmail.com>
2019-12-10 11:07:02 +0000

    behave:renamed mini formatter

--- tests/browser/formatter/mini.py
+++ tests/browser/formatter/mini.py
@@ -2,19 +2,22 @@
 from behave.formatter.base import Formatter
 
 
-class MiniFormatter(Formatter):
+class Mini(Formatter):
     """
     Provides a simple plain formatter without coloring/formatting.
     The formatter displays only scenario name
     """
-    name = "plain"
-    description = "Very basic formatter with maximum compatibility"
 
-    SHOW_MULTI_LINE = False
-    SHOW_TAGS = False
-    SHOW_ALIGNED_KEYWORDS = False
-    indent_size = 0
+    description = "Print out only scenario titles"
+
+    DEFAULT_INDENT_SIZE = 0
     RAISE_OUTPUT_ERRORS = True
+    SHOW_ALIGNED_KEYWORDS = False
+    SHOW_BACKGROUNDS = False
+    SHOW_MULTI_LINE = False
+    SHOW_RULES = False
+    SHOW_TAGS = False
+    SHOW_SUMMARY = False
 
     def scenario(self, scenario):
         skip_config_tags = [
@@ -23,12 +26,10 @@ class MiniFormatter(Formatter):
             if tag.startswith("-")
         ]
         has_feature_skip_tags = any(
-            skip_tag in scenario.feature.tags
-            for skip_tag in skip_config_tags
+            skip_tag in scenario.feature.tags for skip_tag in skip_config_tags
         )
         has_scenario_skip_tags = any(
-            skip_tag in scenario.tags
-            for skip_tag in skip_config_tags
+            skip_tag in scenario.tags for skip_tag in skip_config_tags
         )
         if not has_scenario_skip_tags and not has_feature_skip_tags:
             self.stream.write(f"{scenario.name}\n")
