6213cb5a97214621cb474515df9225d7e4c7c1d1
kowalcj0 <kowalczykjanusz@gmail.com>
2019-12-11 22:36:16 +0000

    remove redundant log entry

--- parallel_browser_tests/run_browser_tests_in_parallel.sh
+++ parallel_browser_tests/run_browser_tests_in_parallel.sh
@@ -3,7 +3,6 @@
 PYTHONPATH=. behave --dry-run --no-source --no-summary --no-snippets --tags=~@wip --tags=~@fixme --tags=~@skip --format=mini features/ | grep -v "Supplied path\|Trying base directory" | while IFS=$'\r' read -r line ; do
   python3 behave_celery_task.py --browser="chrome" --scenario="${line}"
   python3 behave_celery_task.py --browser="firefox" --scenario="${line}"
-  echo "Added task for scenario: '${line}'"
 done
 
 # print stats until there are tests to execute
