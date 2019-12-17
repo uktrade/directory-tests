6cebe41212c65d7fb4af99217cc1573de9e85a66
kowalcj0 <kowalczykjanusz@gmail.com>
2019-12-17 21:35:43 +0000

    behave: increase screen size & disable gpu for chrome

--- tests/browser/utils/browser.py
+++ tests/browser/utils/browser.py
@@ -153,7 +153,8 @@ def start_driver_session(session_name: str, capabilities: dict) -> WebDriver:
             options = Options()
             if BROWSER_HEADLESS:
                 options.add_argument("--headless")
-                options.add_argument("--window-size=1600x2200")
+                options.add_argument("--window-size=1920x2200")
+                options.add_argument("--disable-gpu")
             options.add_argument("--start-maximized")
             options.add_argument("--disable-extensions")
             options.add_argument("--no-sandbox")
@@ -163,7 +164,7 @@ def start_driver_session(session_name: str, capabilities: dict) -> WebDriver:
             options = Options()
             if BROWSER_HEADLESS:
                 options.add_argument("--headless")
-                options.add_argument("--window-size=1600x2200")
+                options.add_argument("--window-size=1920x2200")
                 options.add_argument("--safe-mode")
 
         print(
