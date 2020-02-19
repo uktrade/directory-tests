16de5d40a93d29a0209d1d9636b9fcc1b09d04f9
kowalcj0 <kowalczykjanusz@gmail.com>
2020-02-19 16:35:25 +0000

    shared: use existing Forms API Client

--- directory_tests_shared/directory_tests_shared/clients.py
+++ directory_tests_shared/directory_tests_shared/clients.py
@@ -6,18 +6,19 @@ from notifications_python_client import NotificationsAPIClient
 from directory_api_client.testapiclient import url_company_by_ch_id  # noqa
 from directory_api_client.testapiclient import url_published_companies  # noqa
 from directory_api_client.testapiclient import DirectoryTestAPIClient
-from directory_client_core.base import AbstractAPIClient
 from directory_cms_client.client import cms_api_client as CMS_API_CLIENT  # noqa
+from directory_forms_api_client.client import (  # noqa
+    forms_api_client as FORMS_API_CLIENT,
+)
 from directory_sso_api_client.client import sso_api_client as SSO_API_CLIENT  # noqa
 from directory_sso_api_client.testapiclient import DirectorySSOTestAPIClient
 from .settings import (
+    BASICAUTH_PASS,
+    BASICAUTH_USER,
     CMS_API_DEFAULT_TIMEOUT,
     DIRECTORY_API_KEY,
     DIRECTORY_API_SENDER_ID,
     DIRECTORY_API_URL,
-    FORMS_API_KEY,
-    FORMS_API_SENDER_ID,
-    FORMS_API_URL,
     GOV_NOTIFY_API_KEY,
     PIR_GOV_NOTIFY_API_KEY,
     SSO_API_DEFAULT_TIMEOUT,
@@ -33,21 +34,6 @@ DIRECTORY_TEST_API_CLIENT = DirectoryTestAPIClient(
     CMS_API_DEFAULT_TIMEOUT,
 )
 
-
-class FormsAPIClient(AbstractAPIClient):
-    version = "1"
-
-    def __init__(self, base_url, api_key, sender_id, timeout):
-        super().__init__(base_url, api_key, sender_id, timeout)
-
-
-FORMS_API_CLIENT = FormsAPIClient(
-    base_url=FORMS_API_URL,
-    api_key=FORMS_API_KEY,
-    sender_id=FORMS_API_SENDER_ID,
-    timeout=30,
-)
-
 GOV_NOTIFY_CLIENT = NotificationsAPIClient(GOV_NOTIFY_API_KEY)
 PIR_GOV_NOTIFY_CLIENT = NotificationsAPIClient(PIR_GOV_NOTIFY_API_KEY)
 
@@ -61,3 +47,6 @@ class BasicAuthenticator:
         credentials = f"{username}:{password}"
         encoded_credentials = base64.b64encode(credentials.encode("ascii"))
         self.headers = {"Authorization": f"Basic {encoded_credentials.decode('ascii')}"}
+
+
+BASIC_AUTHENTICATOR = BasicAuthenticator(BASICAUTH_USER, BASICAUTH_PASS)
